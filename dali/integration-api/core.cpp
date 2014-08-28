tizenorg/platform.core.uifw.dali-core

Changed Core::Update to add current time and vsync times as parameters. Moved FrameTime class to Adaptor.
[Problem]  Dali core is trying to second guess what the Vsync monitor is doing
in adaptor.
[Solution] Remove FrameTime from dali-core, and instead pass in the correct
elapsed time and last/next vsync times to Update().

Change-Id: I002dea0c3e126fdb982990776c4ea7966b72758f
Signed-off-by: David Steele <david.steele@partner.samsung.com>

dali/integration-api/core.cpp
@@ -85,11 +85,6 @@ void Core::SetDpi(unsigned int dpiHorizontal, unsigned int dpiVertical)
   mImpl->SetDpi(dpiHorizontal, dpiVertical);
 }
 
-void Core::SetMinimumFrameTimeInterval(unsigned int interval)
-{
-  mImpl->SetMinimumFrameTimeInterval(interval);
-}
-
 void Core::Suspend()
 {
   mImpl->Suspend();
@@ -120,29 +115,14 @@ unsigned int Core::GetMaximumUpdateCount() const
   return mImpl->GetMaximumUpdateCount();
 }
 
-void Core::Update( UpdateStatus& status )
+void Core::Update( float elapsedSeconds, unsigned int lastVSyncTime, unsigned int nextVSyncTime, UpdateStatus& status )
 {
-  mImpl->Update( status );
+  mImpl->Update( elapsedSeconds, lastVSyncTime, nextVSyncTime, status );
 }
 
 void Core::Render( RenderStatus& status )
 {
   mImpl->Render( status );
-}
-
-void Core::Sleep()
-{
-  mImpl->Sleep();
-}
-
-void Core::WakeUp()
-{
-  mImpl->WakeUp();
-}
-
-void Core::VSync( unsigned int frameNumber, unsigned int seconds, unsigned int microseconds )
-{
-  mImpl->VSync( frameNumber, seconds, microseconds );
 }
 
 SystemOverlay& Core::GetSystemOverlay()

dali/integration-api/core.h
@@ -259,22 +259,6 @@ public:
    */
   void SetDpi(unsigned int dpiHorizontal, unsigned int dpiVertical);
 
-  /**
-   * Sets the expected interval between frames used to predict future intervals and the time when the
-   * next render will take place.
-   *
-   * This is the minimum interval that Core should expect. Core will adapt the predicted interval
-   * accordingly if the expected interval is constantly not met (but will not drop it below this
-   * amount).
-   *
-   * The value provided should be in microseconds.
-   *
-   * @param[in]  interval  The minimum interval between frames (in microseconds).
-   *
-   * Multi-threading note: this method should be called from the render thread
-   */
-  void SetMinimumFrameTimeInterval(unsigned int interval);
-
   // Core Lifecycle
 
   /**
@@ -337,10 +321,13 @@ public:
    * However the update-thread must wait until frame N has been rendered, before processing frame N+2.
    * After this method returns, messages may be queued internally for the main thread.
    * In order to process these messages, a notification is sent via the main thread's event loop.
+   * @param[in] elapsedSeconds Number of seconds since the last call
+   * @param[in] lastVSyncTime The last vsync time in milliseconds
+   * @param[in] nextVSyncTime The time of the next predicted VSync in milliseconds
    * @param[out] status showing whether further updates are required. This also shows
    * whether a Notification event should be sent, regardless of whether the multi-threading is used.
    */
-  void Update( UpdateStatus& status );
+  void Update( float elapsedSeconds, unsigned int lastVSyncTime, unsigned int nextVSyncTime, UpdateStatus& status );
 
   /**
    * Render the next frame. This method should be preceded by a call up Update.
@@ -349,31 +336,6 @@ public:
    * @param[out] status showing whether update is required to run.
    */
   void Render( RenderStatus& status );
-
-  /**
-   * Tells core that it is about to sleep.
-   * Application is running as normal, but no updates are taking place i.e. no ongoing animations.
-   * This should be called when we choose to stop updating and rendering when there are no screen
-   * updates required.
-   * Multi-threading note: this method should be called from the update-thread.
-   */
-  void Sleep();
-
-  /**
-   * Wakes up core from a sleep state.
-   * At the first update the elapsed time passed to the animations is zero.
-   * Multi-threading note: this method should be called from the update-thread.
-   */
-  void WakeUp();
-
-  /**
-   * Notification of a vertical blank sync
-   * @param[in] frameNumber  The frame number of this vsync. This number will not update
-   *                         while paused.
-   * @param[in] seconds      The timestamp seconds
-   * @param[in] microseconds The timestamp microseconds
-   */
-  void VSync( unsigned int frameNumber, unsigned int seconds, unsigned int microseconds );
 
   // System-level overlay
 

dali/internal/common/core-impl.h
@@ -54,7 +54,6 @@ class EventProcessor;
 class GestureEventProcessor;
 class ResourceClient;
 class ResourceManager;
-class FrameTime;
 class FontFactory;
 class ImageFactory;
 class ModelFactory;
@@ -118,7 +117,7 @@ public:
   /**
    * @copydoc Dali::Integration::Core::Update()
    */
-  void Update( Integration::UpdateStatus& status );
+  void Update( float elapsedSeconds, unsigned int lastVSyncTime, unsigned int nextVSyncTime, Integration::UpdateStatus& status );
 
   /**
    * @copydoc Dali::Integration::Core::Render()
@@ -154,21 +153,6 @@ public:
    * @copydoc Dali::Integration::Core::GetMaximumUpdateCount()
    */
   unsigned int GetMaximumUpdateCount() const;
-
-  /**
-   * @copydoc Dali::Integration::Core::Sleep()
-   */
-  void Sleep();
-
-  /**
-   * @copydoc Dali::Integration::Core::Wake()
-   */
-  void WakeUp();
-
-  /**
-   * @copydoc Dali::Integration::Core::VSync()
-   */
-  void VSync( unsigned int frameNumber, unsigned int seconds, unsigned int microseconds );
 
   /**
    * @copydoc Dali::Integration::Core::GetSystemOverlay()
@@ -305,7 +289,6 @@ private:
   NotificationManager*                      mNotificationManager;         ///< Notification manager
   AnimationPlaylistOwner                    mAnimationPlaylist;           ///< For 'Fire and forget' animation support
   OwnerPointer<PropertyNotificationManager> mPropertyNotificationManager; ///< For safe signal emmision of property changed notifications
-  FrameTime*                                mFrameTime;                   ///< Time when we render
   FontFactory*                              mFontFactory;                 ///< font resource factory
   ImageFactory*                             mImageFactory;                ///< Image resource factory
   ModelFactory*                             mModelFactory;                ///< Model resource factory

dalihub/dali-core

dali/integration-api/core.cpp
@@ -85,11 +85,6 @@ void Core::SetDpi(unsigned int dpiHorizontal, unsigned int dpiVertical)
   mImpl->SetDpi(dpiHorizontal, dpiVertical);
 }
 
-void Core::SetMinimumFrameTimeInterval(unsigned int interval)
-{
-  mImpl->SetMinimumFrameTimeInterval(interval);
-}
-
 void Core::Suspend()
 {
   mImpl->Suspend();
@@ -120,29 +115,14 @@ unsigned int Core::GetMaximumUpdateCount() const
   return mImpl->GetMaximumUpdateCount();
 }
 
-void Core::Update( UpdateStatus& status )
+void Core::Update( float elapsedSeconds, unsigned int lastVSyncTimeMilliseconds, unsigned int nextVSyncTimeMilliseconds, UpdateStatus& status )
 {
-  mImpl->Update( status );
+  mImpl->Update( elapsedSeconds, lastVSyncTimeMilliseconds, nextVSyncTimeMilliseconds, status );
 }
 
 void Core::Render( RenderStatus& status )
 {
   mImpl->Render( status );
-}
-
-void Core::Sleep()
-{
-  mImpl->Sleep();
-}
-
-void Core::WakeUp()
-{
-  mImpl->WakeUp();
-}
-
-void Core::VSync( unsigned int frameNumber, unsigned int seconds, unsigned int microseconds )
-{
-  mImpl->VSync( frameNumber, seconds, microseconds );
 }
 
 SystemOverlay& Core::GetSystemOverlay()

dali/integration-api/core.h
@@ -259,22 +259,6 @@ public:
    */
   void SetDpi(unsigned int dpiHorizontal, unsigned int dpiVertical);
 
-  /**
-   * Sets the expected interval between frames used to predict future intervals and the time when the
-   * next render will take place.
-   *
-   * This is the minimum interval that Core should expect. Core will adapt the predicted interval
-   * accordingly if the expected interval is constantly not met (but will not drop it below this
-   * amount).
-   *
-   * The value provided should be in microseconds.
-   *
-   * @param[in]  interval  The minimum interval between frames (in microseconds).
-   *
-   * Multi-threading note: this method should be called from the render thread
-   */
-  void SetMinimumFrameTimeInterval(unsigned int interval);
-
   // Core Lifecycle
 
   /**
@@ -337,10 +321,13 @@ public:
    * However the update-thread must wait until frame N has been rendered, before processing frame N+2.
    * After this method returns, messages may be queued internally for the main thread.
    * In order to process these messages, a notification is sent via the main thread's event loop.
+   * @param[in] elapsedSeconds Number of seconds since the last call
+   * @param[in] lastVSyncTimeMilliseconds The last vsync time in milliseconds
+   * @param[in] nextVSyncTimeMilliseconds The time of the next predicted VSync in milliseconds
    * @param[out] status showing whether further updates are required. This also shows
    * whether a Notification event should be sent, regardless of whether the multi-threading is used.
    */
-  void Update( UpdateStatus& status );
+  void Update( float elapsedSeconds, unsigned int lastVSyncTimeMilliseconds, unsigned int nextVSyncTimeMilliseconds, UpdateStatus& status );
 
   /**
    * Render the next frame. This method should be preceded by a call up Update.
@@ -349,31 +336,6 @@ public:
    * @param[out] status showing whether update is required to run.
    */
   void Render( RenderStatus& status );
-
-  /**
-   * Tells core that it is about to sleep.
-   * Application is running as normal, but no updates are taking place i.e. no ongoing animations.
-   * This should be called when we choose to stop updating and rendering when there are no screen
-   * updates required.
-   * Multi-threading note: this method should be called from the update-thread.
-   */
-  void Sleep();
-
-  /**
-   * Wakes up core from a sleep state.
-   * At the first update the elapsed time passed to the animations is zero.
-   * Multi-threading note: this method should be called from the update-thread.
-   */
-  void WakeUp();
-
-  /**
-   * Notification of a vertical blank sync
-   * @param[in] frameNumber  The frame number of this vsync. This number will not update
-   *                         while paused.
-   * @param[in] seconds      The timestamp seconds
-   * @param[in] microseconds The timestamp microseconds
-   */
-  void VSync( unsigned int frameNumber, unsigned int seconds, unsigned int microseconds );
 
   // System-level overlay
 

dali/internal/common/core-impl.h
@@ -54,7 +54,6 @@ class EventProcessor;
 class GestureEventProcessor;
 class ResourceClient;
 class ResourceManager;
-class FrameTime;
 class FontFactory;
 class ImageFactory;
 class ModelFactory;
@@ -118,7 +117,7 @@ public:
   /**
    * @copydoc Dali::Integration::Core::Update()
    */
-  void Update( Integration::UpdateStatus& status );
+  void Update( float elapsedSeconds, unsigned int lastVSyncTimeMilliseconds, unsigned int nextVSyncTimeMilliseconds, Integration::UpdateStatus& status );
 
   /**
    * @copydoc Dali::Integration::Core::Render()
@@ -154,21 +153,6 @@ public:
    * @copydoc Dali::Integration::Core::GetMaximumUpdateCount()
    */
   unsigned int GetMaximumUpdateCount() const;
-
-  /**
-   * @copydoc Dali::Integration::Core::Sleep()
-   */
-  void Sleep();
-
-  /**
-   * @copydoc Dali::Integration::Core::Wake()
-   */
-  void WakeUp();
-
-  /**
-   * @copydoc Dali::Integration::Core::VSync()
-   */
-  void VSync( unsigned int frameNumber, unsigned int seconds, unsigned int microseconds );
 
   /**
    * @copydoc Dali::Integration::Core::GetSystemOverlay()
@@ -305,7 +289,6 @@ private:
   NotificationManager*                      mNotificationManager;         ///< Notification manager
   AnimationPlaylistOwner                    mAnimationPlaylist;           ///< For 'Fire and forget' animation support
   OwnerPointer<PropertyNotificationManager> mPropertyNotificationManager; ///< For safe signal emmision of property changed notifications
-  FrameTime*                                mFrameTime;                   ///< Time when we render
   FontFactory*                              mFontFactory;                 ///< font resource factory
   ImageFactory*                             mImageFactory;                ///< Image resource factory
   ModelFactory*                             mModelFactory;                ///< Model resource factory