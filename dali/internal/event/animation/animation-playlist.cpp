tizenorg/platform.core.uifw.dali-core

Stop heap allocating messages from UpdateManager for Animation and RenderTask completions. Remove the dependency between update manager and event side rendertasklist, generalize notification mechanism
[Problem] cyclic dependencies, heap allocations potentially on a frame by frame basis
[Cause] update manager needed to call API in event side to get the message to sent
[Solution] pass the interface pointer to notification manager

Change-Id: If29f3fd9fe5c972cbf19fcb34a3adf800374b33a

dali/internal/event/animation/animation-playlist.cpp
@@ -67,7 +67,7 @@ void AnimationPlaylist::OnClear( Animation& animation )
   mPlaylist.erase( Dali::Animation(&animation) );
 }
 
-void AnimationPlaylist::NotifyFinishedAnimations()
+void AnimationPlaylist::UpdateCompletedNotification()
 {
   std::vector< Dali::Animation > finishedAnimations;
 

dali/internal/event/animation/animation-playlist.h
@@ -21,8 +21,7 @@
 // INTERNAL INCLUDES
 #include <dali/public-api/animation/animation.h>
 #include <dali/public-api/common/set-wrapper.h>
-#include <dali/internal/common/message.h>
-#include <dali/internal/event/animation/animation-finished-notifier.h>
+#include <dali/internal/event/common/complete-notification-interface.h>
 
 namespace Dali
 {
@@ -36,7 +35,7 @@ class Animation;
  * AnimationPlaylist provides notifications to applications when animations are finished.
  * It reference-counts playing animations, to allow "fire and forget" behaviour.
  */
-class AnimationPlaylist : public AnimationFinishedNotifier
+class AnimationPlaylist : public CompleteNotificationInterface
 {
 public:
 
@@ -73,13 +72,6 @@ public:
    */
   void OnClear( Animation& animation );
 
-  /**
-   * From AnimationFinishedNotifier; emit "Finished" signal on any animations that have finished.
-   * This method should be called in the event-thread; the update-thread must use AnimationFinishedMessage.
-   * @post The "Finished" animations will no longer be referenced by AnimationPlaylist.
-   */
-  void NotifyFinishedAnimations();
-
 private:
 
   /**
@@ -93,11 +85,18 @@ private:
   // Undefined
   AnimationPlaylist& operator=(const AnimationPlaylist& rhs);
 
+private: // from CompleteNotificationInterface
+
+  /**
+   * @copydoc CompleteNotificationInterface::UpdateCompletedNotification()
+   */
+  virtual void UpdateCompletedNotification();
+
 private:
 
   std::set< Animation* > mAnimations; ///< All existing animations (not referenced)
-
   std::set< Dali::Animation > mPlaylist; ///< The currently playing animations (reference counted)
+
 };
 
 } // namespace Internal

dali/internal/event/common/complete-notification-interface.h
@@ -1,0 +1,63 @@
+#ifndef __DALI_INTERNAL_COMPLETE_NOTIFICATION_INTERFACE_H__
+#define __DALI_INTERNAL_COMPLETE_NOTIFICATION_INTERFACE_H__
+
+/*
+ * Copyright (c) 2014 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+// INTERNAL INCLUDES
+
+namespace Dali
+{
+
+namespace Internal
+{
+
+/**
+ * Provides notifications to the event-thread regarding the changes in previous update(s).
+ * For example after an animation finished, or after resources were loaded.
+ */
+class CompleteNotificationInterface
+{
+protected:
+
+  /**
+   * Constructor, not to be directly instantiated.
+   */
+  CompleteNotificationInterface()
+  {}
+
+  /**
+   * Virtual destructor as this is an interface, no deletion through this interface though.
+   */
+  virtual ~CompleteNotificationInterface()
+  {}
+
+public:
+
+  /**
+   * This method is called by Notification Manager
+   */
+  virtual void UpdateCompletedNotification() = 0;
+
+};
+
+} // namespace Internal
+
+} // namespace Dali
+
+#endif // __DALI_INTERNAL_COMPLETE_NOTIFICATION_INTERFACE_H__
+

dali/internal/event/render-tasks/render-task-list-impl.cpp
@@ -113,7 +113,30 @@ Dali::RenderTask RenderTaskList::GetTask( unsigned int index ) const
   return mTasks[index];
 }
 
-void RenderTaskList::NotifyFinished()
+RenderTaskList::RenderTaskList( EventToUpdate& eventToUpdate, RenderTaskDefaults& defaults, bool systemLevel )
+: mEventToUpdate( eventToUpdate ),
+  mDefaults( defaults ),
+  mIsSystemLevel( systemLevel ),
+  mSceneObject( NULL )
+{
+}
+
+RenderTaskList::~RenderTaskList()
+{
+}
+
+void RenderTaskList::Initialize( UpdateManager& updateManager )
+{
+  // This should only be called once, with no existing scene-object
+  DALI_ASSERT_DEBUG( NULL == mSceneObject );
+
+  // Get raw-pointer to render task list
+  mSceneObject = updateManager.GetRenderTaskList( mIsSystemLevel );
+  // set the callback to call us back when tasks are completed
+  mSceneObject->SetCompleteNotificationInterface( this );
+}
+
+void RenderTaskList::UpdateCompletedNotification()
 {
   DALI_LOG_TRACE_METHOD(gLogRenderList);
 
@@ -138,27 +161,6 @@ void RenderTaskList::NotifyFinished()
 
     GetImplementation(handle).EmitSignalFinish();
   }
-}
-
-RenderTaskList::RenderTaskList( EventToUpdate& eventToUpdate, RenderTaskDefaults& defaults, bool systemLevel )
-: mEventToUpdate( eventToUpdate ),
-  mDefaults( defaults ),
-  mIsSystemLevel( systemLevel ),
-  mSceneObject( NULL )
-{
-}
-
-RenderTaskList::~RenderTaskList()
-{
-}
-
-void RenderTaskList::Initialize( UpdateManager& updateManager )
-{
-  // This should only be called once, with no existing scene-object
-  DALI_ASSERT_DEBUG( NULL == mSceneObject );
-
-  // Get raw-pointer to render task list
-  mSceneObject = updateManager.GetRenderTaskList( mIsSystemLevel );
 }
 
 } // namespace Internal

dali/internal/event/render-tasks/render-task-list-impl.h
@@ -23,7 +23,7 @@
 #include <dali/public-api/object/base-object.h>
 #include <dali/public-api/render-tasks/render-task.h>
 #include <dali/public-api/render-tasks/render-task-list.h>
-#include <dali/internal/common/message.h>
+#include <dali/internal/event/common/complete-notification-interface.h>
 
 namespace Dali
 {
@@ -44,7 +44,7 @@ class UpdateManager;
  * A proxy for the scene-graph RenderTaskList.
  * A separate LayerList is maintained for actors added via the SystemLevel::Add().
  */
-class RenderTaskList : public BaseObject
+class RenderTaskList : public BaseObject, public CompleteNotificationInterface
 {
 public:
 
@@ -92,8 +92,9 @@ public:
    * Provide notification signals for a "Finished" render task.
    * This method should be called in the event-thread
    * Queue NotifyFinishedMessage() from update-thread
+   * @param object pointer to this class instance
    */
-  void NotifyFinished();
+  static void NotifyFinished( void* object );
 
 protected:
 
@@ -115,6 +116,13 @@ protected:
    */
   void Initialize( SceneGraph::UpdateManager& updateManager );
 
+private: // from CompleteNotificationInterface
+
+  /**
+   * @copydoc CompleteNotificationInterface::UpdateCompletedNotification()
+   */
+  virtual void UpdateCompletedNotification();
+
 private:
 
   EventToUpdate& mEventToUpdate;
@@ -126,15 +134,6 @@ private:
 
   RenderTaskContainer mTasks; ///< Reference counted render-tasks
 };
-
-/**
- * Notification message for when 1+ render tasks have finished
- * @param[in] renderTaskList This will provide the notification signals.
- */
-inline MessageBase* NotifyFinishedMessage( RenderTaskList& renderTaskList )
-{
-  return new Message< RenderTaskList >( &renderTaskList, &RenderTaskList::NotifyFinished );
-}
 
 } // namespace Internal
 

dalihub/dali-core

dali/internal/event/animation/animation-playlist.cpp
@@ -67,7 +67,7 @@ void AnimationPlaylist::OnClear( Animation& animation )
   mPlaylist.erase( Dali::Animation(&animation) );
 }
 
-void AnimationPlaylist::NotifyFinishedAnimations()
+void AnimationPlaylist::NotifyCompleted()
 {
   std::vector< Dali::Animation > finishedAnimations;
 

dali/internal/event/animation/animation-playlist.h
@@ -21,8 +21,7 @@
 // INTERNAL INCLUDES
 #include <dali/public-api/animation/animation.h>
 #include <dali/public-api/common/set-wrapper.h>
-#include <dali/internal/common/message.h>
-#include <dali/internal/event/animation/animation-finished-notifier.h>
+#include <dali/internal/event/common/complete-notification-interface.h>
 
 namespace Dali
 {
@@ -36,7 +35,7 @@ class Animation;
  * AnimationPlaylist provides notifications to applications when animations are finished.
  * It reference-counts playing animations, to allow "fire and forget" behaviour.
  */
-class AnimationPlaylist : public AnimationFinishedNotifier
+class AnimationPlaylist : public CompleteNotificationInterface
 {
 public:
 
@@ -73,13 +72,6 @@ public:
    */
   void OnClear( Animation& animation );
 
-  /**
-   * From AnimationFinishedNotifier; emit "Finished" signal on any animations that have finished.
-   * This method should be called in the event-thread; the update-thread must use AnimationFinishedMessage.
-   * @post The "Finished" animations will no longer be referenced by AnimationPlaylist.
-   */
-  void NotifyFinishedAnimations();
-
 private:
 
   /**
@@ -93,11 +85,18 @@ private:
   // Undefined
   AnimationPlaylist& operator=(const AnimationPlaylist& rhs);
 
+private: // from CompleteNotificationInterface
+
+  /**
+   * @copydoc CompleteNotificationInterface::NotifyCompleted()
+   */
+  virtual void NotifyCompleted();
+
 private:
 
   std::set< Animation* > mAnimations; ///< All existing animations (not referenced)
-
   std::set< Dali::Animation > mPlaylist; ///< The currently playing animations (reference counted)
+
 };
 
 } // namespace Internal

dali/internal/event/common/complete-notification-interface.h
@@ -1,0 +1,63 @@
+#ifndef __DALI_INTERNAL_COMPLETE_NOTIFICATION_INTERFACE_H__
+#define __DALI_INTERNAL_COMPLETE_NOTIFICATION_INTERFACE_H__
+
+/*
+ * Copyright (c) 2014 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+// INTERNAL INCLUDES
+
+namespace Dali
+{
+
+namespace Internal
+{
+
+/**
+ * Provides notifications to the event-thread regarding the changes in previous update(s).
+ * For example after an animation finished, or after resources were loaded.
+ */
+class CompleteNotificationInterface
+{
+protected:
+
+  /**
+   * Constructor, not to be directly instantiated.
+   */
+  CompleteNotificationInterface()
+  {}
+
+  /**
+   * Virtual destructor as this is an interface, no deletion through this interface though.
+   */
+  virtual ~CompleteNotificationInterface()
+  {}
+
+public:
+
+  /**
+   * This method is called by Notification Manager
+   */
+  virtual void NotifyCompleted() = 0;
+
+};
+
+} // namespace Internal
+
+} // namespace Dali
+
+#endif // __DALI_INTERNAL_COMPLETE_NOTIFICATION_INTERFACE_H__
+

dali/internal/event/render-tasks/render-task-list-impl.cpp
@@ -113,7 +113,30 @@ Dali::RenderTask RenderTaskList::GetTask( unsigned int index ) const
   return mTasks[index];
 }
 
-void RenderTaskList::NotifyFinished()
+RenderTaskList::RenderTaskList( EventToUpdate& eventToUpdate, RenderTaskDefaults& defaults, bool systemLevel )
+: mEventToUpdate( eventToUpdate ),
+  mDefaults( defaults ),
+  mIsSystemLevel( systemLevel ),
+  mSceneObject( NULL )
+{
+}
+
+RenderTaskList::~RenderTaskList()
+{
+}
+
+void RenderTaskList::Initialize( UpdateManager& updateManager )
+{
+  // This should only be called once, with no existing scene-object
+  DALI_ASSERT_DEBUG( NULL == mSceneObject );
+
+  // Get raw-pointer to render task list
+  mSceneObject = updateManager.GetRenderTaskList( mIsSystemLevel );
+  // set the callback to call us back when tasks are completed
+  mSceneObject->SetCompleteNotificationInterface( this );
+}
+
+void RenderTaskList::NotifyCompleted()
 {
   DALI_LOG_TRACE_METHOD(gLogRenderList);
 
@@ -138,27 +161,6 @@ void RenderTaskList::NotifyFinished()
 
     GetImplementation(handle).EmitSignalFinish();
   }
-}
-
-RenderTaskList::RenderTaskList( EventToUpdate& eventToUpdate, RenderTaskDefaults& defaults, bool systemLevel )
-: mEventToUpdate( eventToUpdate ),
-  mDefaults( defaults ),
-  mIsSystemLevel( systemLevel ),
-  mSceneObject( NULL )
-{
-}
-
-RenderTaskList::~RenderTaskList()
-{
-}
-
-void RenderTaskList::Initialize( UpdateManager& updateManager )
-{
-  // This should only be called once, with no existing scene-object
-  DALI_ASSERT_DEBUG( NULL == mSceneObject );
-
-  // Get raw-pointer to render task list
-  mSceneObject = updateManager.GetRenderTaskList( mIsSystemLevel );
 }
 
 } // namespace Internal

dali/internal/event/render-tasks/render-task-list-impl.h
@@ -23,7 +23,7 @@
 #include <dali/public-api/object/base-object.h>
 #include <dali/public-api/render-tasks/render-task.h>
 #include <dali/public-api/render-tasks/render-task-list.h>
-#include <dali/internal/common/message.h>
+#include <dali/internal/event/common/complete-notification-interface.h>
 
 namespace Dali
 {
@@ -44,7 +44,7 @@ class UpdateManager;
  * A proxy for the scene-graph RenderTaskList.
  * A separate LayerList is maintained for actors added via the SystemLevel::Add().
  */
-class RenderTaskList : public BaseObject
+class RenderTaskList : public BaseObject, public CompleteNotificationInterface
 {
 public:
 
@@ -92,8 +92,9 @@ public:
    * Provide notification signals for a "Finished" render task.
    * This method should be called in the event-thread
    * Queue NotifyFinishedMessage() from update-thread
+   * @param object pointer to this class instance
    */
-  void NotifyFinished();
+  static void NotifyFinished( void* object );
 
 protected:
 
@@ -115,6 +116,13 @@ protected:
    */
   void Initialize( SceneGraph::UpdateManager& updateManager );
 
+private: // from CompleteNotificationInterface
+
+  /**
+   * @copydoc CompleteNotificationInterface::NotifyCompleted()
+   */
+  virtual void NotifyCompleted();
+
 private:
 
   EventToUpdate& mEventToUpdate;
@@ -126,15 +134,6 @@ private:
 
   RenderTaskContainer mTasks; ///< Reference counted render-tasks
 };
-
-/**
- * Notification message for when 1+ render tasks have finished
- * @param[in] renderTaskList This will provide the notification signals.
- */
-inline MessageBase* NotifyFinishedMessage( RenderTaskList& renderTaskList )
-{
-  return new Message< RenderTaskList >( &renderTaskList, &RenderTaskList::NotifyFinished );
-}
 
 } // namespace Internal
 