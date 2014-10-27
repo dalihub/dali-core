df340de004fa6bc02b21f9cb7e7720a654f7c0c9
Kimmo Hoikka <kimmo.hoikka@samsung.com>
2014-10-27 17:39:05 +0000

    remove obsolete functions from public API
    [Problem]
    [Cause]
    [Solution]
    
    Change-Id: I693adda565c19532919d9d4146915add89e41e47

--- dali/public-api/dali-core.h
+++ dali/public-api/dali-core.h
@@ -38,7 +38,6 @@
 #include <dali/public-api/animation/active-constraint.h>
 #include <dali/public-api/animation/alpha-functions.h>
 #include <dali/public-api/animation/animation.h>
-#include <dali/public-api/animation/animator-functions.h>
 #include <dali/public-api/animation/constraint-functions.h>
 #include <dali/public-api/animation/constraint-source.h>
 #include <dali/public-api/animation/constraint.h>
