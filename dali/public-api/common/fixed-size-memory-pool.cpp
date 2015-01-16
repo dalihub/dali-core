d4b4574471b5c027a5a579eba460b5906d5baeb8
Kingsley Stephens <k.stephens@partner.samsung.com>
2015-01-16 16:15:23 +0000

    Add fixed size memory pool class
    Change-Id: Idb8b4ba83d11c293e1a081a2067ba5f0faf0570d

---
+++ dali/public-api/common/fixed-size-memory-pool.cpp
@@ -0,0 +1,171 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+// CLASS HEADER
+#include <dali/public-api/common/fixed-size-memory-pool.h>
+
+// INTERNAL HEADERS
+#include <dali/public-api/dali-core.h>
+
+namespace Dali
+{
+
+/**
+ * @brief Private implementation class
+ */
+struct FixedSizeMemoryPool::Impl
+{
+  /**
+   * @brief Struct to represent a block of memory from which allocations can be made.
+   *
+   * The block forms a linked list.
+   */
+  struct Block
+  {
+    void* blockMemory;      ///< The allocated memory from which allocations can be made
+    Block* nextBlock;       ///< The next block in the linked list
+
+    /**
+     * @brief Construct a new block with given size
+     *
+     * @param size The size of the memory block to allocate in bytes. Must be non-zero.
+     */
+    Block( SizeType size )
+    : nextBlock( NULL )
+    {
+      blockMemory = ::operator new( size );
+      DALI_ASSERT_ALWAYS( blockMemory && "Out of memory" );
+    }
+
+    /**
+     * @brief Destructor
+     */
+    ~Block()
+    {
+      ::operator delete( blockMemory );
+    }
+  };
+
+  /**
+   * @brief Constructor
+   */
+  Impl( SizeType fixedSize, SizeType initialCapacity, SizeType maximumBlockCapacity )
+  :  mFixedSize( fixedSize ),
+     mMemoryBlocks( initialCapacity * mFixedSize ),
+     mMaximumBlockCapacity( maximumBlockCapacity ),
+     mCurrentBlock( &mMemoryBlocks ),
+     mCurrentBlockCapacity( initialCapacity ),
+     mCurrentBlockSize( 0 ),
+     mDeletedObjects( NULL )
+  {
+    // We need enough room to store the deleted list in the data
+    DALI_ASSERT_DEBUG( mFixedSize >= sizeof( void* ) );
+  }
+
+  /**
+   * @brief Destructor
+   */
+  ~Impl()
+  {
+    // Clean up memory block linked list (mMemoryBlocks will be auto-destroyed by its destructor)
+    Block* block = mMemoryBlocks.nextBlock;
+    while( block )
+    {
+      Block* nextBlock = block->nextBlock;
+      delete block;
+      block = nextBlock;
+    }
+  }
+
+  /**
+   * @brief Allocate a new block for allocating memory from
+   */
+  void AllocateNewBlock()
+  {
+    // Double capacity for the new block
+    SizeType size = mCurrentBlockCapacity * 2;
+    if( size > mMaximumBlockCapacity || size < mCurrentBlockCapacity )    // Check for overflow of size type
+    {
+      size = mMaximumBlockCapacity;
+    }
+
+    mCurrentBlockCapacity = size;
+
+    // Allocate
+    Block* block = new Block( mCurrentBlockCapacity * mFixedSize );
+    mCurrentBlock->nextBlock = block;       // Add to end of linked list
+    mCurrentBlock = block;
+
+    mCurrentBlockSize = 0;
+  }
+
+  SizeType mFixedSize;                ///< The size of each allocation in bytes
+
+  Block mMemoryBlocks;                ///< Linked list of allocated memory blocks
+  SizeType mMaximumBlockCapacity;     ///< The maximum allowed capacity of allocations in a new memory block
+
+  Block* mCurrentBlock;               ///< Pointer to the active block
+  SizeType mCurrentBlockCapacity;     ///< The maximum number of allocations that can be allocated for the current block
+  SizeType mCurrentBlockSize;         ///< The number of allocations allocated to the current block
+
+  void* mDeletedObjects;              ///< Pointer to the head of the list of deleted objects. The addresses are stored in the allocated memory blocks.
+};
+
+
+
+FixedSizeMemoryPool::FixedSizeMemoryPool( SizeType fixedSize, SizeType initialCapacity, SizeType maximumBlockCapacity )
+{
+  mImpl = new Impl( fixedSize, initialCapacity, maximumBlockCapacity );
+}
+
+FixedSizeMemoryPool::~FixedSizeMemoryPool()
+{
+  delete mImpl;
+}
+
+void* FixedSizeMemoryPool::Allocate()
+{
+  // First, recycle deleted objects
+  if( mImpl->mDeletedObjects )
+  {
+    void* recycled = mImpl->mDeletedObjects;
+    mImpl->mDeletedObjects = *( reinterpret_cast< void** >( mImpl->mDeletedObjects ) );  // Pop head off front of deleted objects list
+    return recycled;
+  }
+
+  // Check if current block is full
+  if( mImpl->mCurrentBlockSize >= mImpl->mCurrentBlockCapacity )
+  {
+    mImpl->AllocateNewBlock();
+  }
+
+  // Placement new the object in block memory
+  unsigned char* objectAddress = static_cast< unsigned char* >( mImpl->mCurrentBlock->blockMemory );
+  objectAddress += mImpl->mCurrentBlockSize * mImpl->mFixedSize;
+  mImpl->mCurrentBlockSize++;
+
+  return objectAddress;
+}
+
+void FixedSizeMemoryPool::Free( void* memory )
+{
+  // Add memory to head of deleted objects list. Store next address in the same memory space as the old object.
+  *( reinterpret_cast< void** >( memory ) ) = mImpl->mDeletedObjects;
+  mImpl->mDeletedObjects = memory;
+}
+
+} // namespace Dali
---
+++ dali/public-api/common/fixed-size-memory-pool.h
@@ -0,0 +1,97 @@
+#ifndef __DALI_FIXED_SIZE_MEMORY_POOL_H__
+#define __DALI_FIXED_SIZE_MEMORY_POOL_H__
+
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+// EXTERNAL INCLUDES
+#include <stdint.h>
+#include <cstddef>
+
+namespace Dali
+{
+
+/**
+ * @brief Calculate the size of a type taking alignment into account
+ */
+template< typename T >
+struct TypeSizeWithAlignment
+{
+  ///< The size of the type with alignment taken into account
+  static const size_t size = ( ( sizeof( T ) + sizeof( void* ) - 1 ) / sizeof( void* ) ) * sizeof( void* );
+};
+
+/**
+ * @brief Memory pool for a given fixed size of memory.
+ *
+ * The pool will allocate and reclaim blocks of memory without concern for what is
+ * stored in them. This means it is up to the client to construct/destruct objects
+ * and hence determine what data type is stored in the memory block. See FixedSizeObjectAllocator
+ * below for an example client for creating objects of a given type. It is also up to the client
+ * to ensure that the size of the block takes memory alignment into account for the
+ * type of data they wish to store in the block. The TypeSizeWithAlignment<T> template
+ * can be useful for determining the size of memory aligned blocks for a given type.
+ */
+class FixedSizeMemoryPool
+{
+public:
+
+  typedef uint32_t SizeType;
+
+public:
+
+  /**
+   * @brief Constructor.
+   *
+   * @param fixedSize The fixed size of each memory allocation. Use TypeSizeWithAlignment if aligned memory is required.
+   * @param initialCapacity The initial size of the memory pool. Defaults to a small value (32) after
+   *                        which the capacity will double as needed.
+   * @param maximumBlockCapacity The maximum size that a new block of memory can be allocated. Defaults to
+   *                             a large value (1024 * 1024 = 1048576).
+   */
+  explicit FixedSizeMemoryPool( SizeType fixedSize, SizeType initialCapacity = 32, SizeType maximumBlockCapacity = 1048576 );
+
+  /**
+   * @brief Destructor.
+   */
+  ~FixedSizeMemoryPool();
+
+  /**
+   * @brief Allocate a new fixed size block of memory
+   *
+   * @return Return the newly allocated memory
+   */
+  void* Allocate();
+
+  /**
+   * @brief Delete a block of memory for the allocation that has been allocated by this memory pool
+   *
+   * @param memory The memory to be deleted. Must have been allocated by this memory pool
+   */
+  void Free( void* memory );
+
+private:
+
+  struct Impl;
+  Impl* mImpl;
+
+};
+
+
+} // namespace Dali
+
+#endif /* __DALI_FIXED_SIZE_MEMORY_POOL_H__ */
