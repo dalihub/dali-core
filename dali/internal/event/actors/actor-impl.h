tizenorg/platform.core.uifw.dali-core

DALi signals refactor to remove V2 naming
Change-Id: Ia8bd3461d2cd12201192bc388727aef5c871fc69

dali/internal/event/actors/actor-impl.h
@@ -986,32 +986,32 @@ public:
   /**
    * @copydoc Dali::Actor::TouchedSignal()
    */
-  Dali::Actor::TouchSignalV2& TouchedSignal();
+  Dali::Actor::TouchSignalType& TouchedSignal();
 
   /**
    * @copydoc Dali::Actor::HoveredSignal()
    */
-  Dali::Actor::HoverSignalV2& HoveredSignal();
+  Dali::Actor::HoverSignalType& HoveredSignal();
 
   /**
    * @copydoc Dali::Actor::MouseWheelEventSignal()
    */
-  Dali::Actor::MouseWheelEventSignalV2& MouseWheelEventSignal();
+  Dali::Actor::MouseWheelEventSignalType& MouseWheelEventSignal();
 
   /**
    * @copydoc Dali::Actor::SetSizeSignal()
    */
-  Dali::Actor::SetSizeSignalV2& SetSizeSignal();
+  Dali::Actor::SetSizeSignalType& SetSizeSignal();
 
   /**
    * @copydoc Dali::Actor::OnStageSignal()
    */
-  Dali::Actor::OnStageSignalV2& OnStageSignal();
+  Dali::Actor::OnStageSignalType& OnStageSignal();
 
   /**
    * @copydoc Dali::Actor::OffStageSignal()
    */
-  Dali::Actor::OffStageSignalV2& OffStageSignal();
+  Dali::Actor::OffStageSignalType& OffStageSignal();
 
   /**
    * Connects a callback function with the object's signals.
@@ -1356,12 +1356,12 @@ protected:
   ActorAttachmentPtr      mAttachment;   ///< Optional referenced attachment
 
   // Signals
-  Dali::Actor::TouchSignalV2             mTouchedSignalV2;
-  Dali::Actor::HoverSignalV2             mHoveredSignalV2;
-  Dali::Actor::MouseWheelEventSignalV2   mMouseWheelEventSignalV2;
-  Dali::Actor::SetSizeSignalV2           mSetSizeSignalV2;
-  Dali::Actor::OnStageSignalV2           mOnStageSignalV2;
-  Dali::Actor::OffStageSignalV2          mOffStageSignalV2;
+  Dali::Actor::TouchSignalType             mTouchedSignalType;
+  Dali::Actor::HoverSignalType             mHoveredSignalType;
+  Dali::Actor::MouseWheelEventSignalType   mMouseWheelEventSignalType;
+  Dali::Actor::SetSizeSignalType           mSetSizeSignalType;
+  Dali::Actor::OnStageSignalType           mOnStageSignalType;
+  Dali::Actor::OffStageSignalType          mOffStageSignalType;
 
   Vector3         mSize;      ///< Event-side storage for size (not a pointer as most actors will have a size)
 

dali/internal/event/common/stage-impl.h
@@ -365,17 +365,17 @@ public:
   /**
    * @copydoc Dali::Stage::KeyEventSignal()
    */
-  Dali::Stage::KeyEventSignalV2& KeyEventSignal();
+  Dali::Stage::KeyEventSignalType& KeyEventSignal();
 
   /**
    * @copydoc Dali::Stage::SignalEventProcessingFinished()
    */
-  Dali::Stage::EventProcessingFinishedSignalV2& EventProcessingFinishedSignal();
+  Dali::Stage::EventProcessingFinishedSignalType& EventProcessingFinishedSignal();
 
   /**
     * @copydoc Dali::Stage::TouchedSignal()
     */
-  Dali::Stage::TouchedSignalV2& TouchedSignal();
+  Dali::Stage::TouchedSignalType& TouchedSignal();
 
   /**
    * @copydoc Dali::Stage::ContextLostSignal()
@@ -390,7 +390,7 @@ public:
   /**
    * @copydoc Dali::Stage::SceneCreatedSignal()
    */
-  Dali::Stage::SceneCreatedSignalV2& SceneCreatedSignal();
+  Dali::Stage::SceneCreatedSignalType& SceneCreatedSignal();
 
 private: // Implementation of ContextNotificationInterface:
 
@@ -472,18 +472,18 @@ private:
   Integration::SystemOverlay* mSystemOverlay; ///< SystemOverlay stage access
 
   // The key event signal
-  Dali::Stage::KeyEventSignalV2                 mKeyEventSignalV2;
+  Dali::Stage::KeyEventSignalType                 mKeyEventSignalType;
 
   // The event processing finished signal
-  Dali::Stage::EventProcessingFinishedSignalV2  mEventProcessingFinishedSignalV2;
+  Dali::Stage::EventProcessingFinishedSignalType  mEventProcessingFinishedSignalType;
 
   // The touched signal
-  Dali::Stage::TouchedSignalV2                  mTouchedSignalV2;
+  Dali::Stage::TouchedSignalType                  mTouchedSignalType;
 
   Dali::Stage::ContextStatusSignal mContextLostSignal;
   Dali::Stage::ContextStatusSignal mContextRegainedSignal;
 
-  Dali::Stage::SceneCreatedSignalV2 mSceneCreatedSignal;
+  Dali::Stage::SceneCreatedSignalType mSceneCreatedSignal;
 };
 
 } // namespace Internal

dali/internal/event/events/long-press-gesture-detector-impl.cpp
@@ -129,7 +129,7 @@ void LongPressGestureDetector::EmitLongPressGestureSignal(Dali::Actor pressedAct
   // Guard against destruction during signal emission
   Dali::LongPressGestureDetector handle( this );
 
-  mDetectedSignalV2.Emit( pressedActor, longPress );
+  mDetectedSignalType.Emit( pressedActor, longPress );
 }
 
 bool LongPressGestureDetector::DoConnectSignal( BaseObject* object, ConnectionTrackerInterface* tracker, const std::string& signalName, FunctorDelegate* functor )

dali/internal/event/events/tap-gesture-detector-impl.cpp
@@ -116,7 +116,7 @@ void TapGestureDetector::EmitTapGestureSignal(Dali::Actor tappedActor, const Tap
   // Guard against destruction during signal emission
   Dali::TapGestureDetector handle( this );
 
-  mDetectedSignalV2.Emit( tappedActor, tap );
+  mDetectedSignalType.Emit( tappedActor, tap );
 }
 
 bool TapGestureDetector::DoConnectSignal( BaseObject* object, ConnectionTrackerInterface* tracker, const std::string& signalName, FunctorDelegate* functor )

dalihub/dali-core

dali/internal/event/actors/actor-impl.h
@@ -986,32 +986,32 @@ public:
   /**
    * @copydoc Dali::Actor::TouchedSignal()
    */
-  Dali::Actor::TouchSignalV2& TouchedSignal();
+  Dali::Actor::TouchSignalType& TouchedSignal();
 
   /**
    * @copydoc Dali::Actor::HoveredSignal()
    */
-  Dali::Actor::HoverSignalV2& HoveredSignal();
+  Dali::Actor::HoverSignalType& HoveredSignal();
 
   /**
    * @copydoc Dali::Actor::MouseWheelEventSignal()
    */
-  Dali::Actor::MouseWheelEventSignalV2& MouseWheelEventSignal();
+  Dali::Actor::MouseWheelEventSignalType& MouseWheelEventSignal();
 
   /**
    * @copydoc Dali::Actor::SetSizeSignal()
    */
-  Dali::Actor::SetSizeSignalV2& SetSizeSignal();
+  Dali::Actor::SetSizeSignalType& SetSizeSignal();
 
   /**
    * @copydoc Dali::Actor::OnStageSignal()
    */
-  Dali::Actor::OnStageSignalV2& OnStageSignal();
+  Dali::Actor::OnStageSignalType& OnStageSignal();
 
   /**
    * @copydoc Dali::Actor::OffStageSignal()
    */
-  Dali::Actor::OffStageSignalV2& OffStageSignal();
+  Dali::Actor::OffStageSignalType& OffStageSignal();
 
   /**
    * Connects a callback function with the object's signals.
@@ -1356,12 +1356,12 @@ protected:
   ActorAttachmentPtr      mAttachment;   ///< Optional referenced attachment
 
   // Signals
-  Dali::Actor::TouchSignalV2             mTouchedSignalV2;
-  Dali::Actor::HoverSignalV2             mHoveredSignalV2;
-  Dali::Actor::MouseWheelEventSignalV2   mMouseWheelEventSignalV2;
-  Dali::Actor::SetSizeSignalV2           mSetSizeSignalV2;
-  Dali::Actor::OnStageSignalV2           mOnStageSignalV2;
-  Dali::Actor::OffStageSignalV2          mOffStageSignalV2;
+  Dali::Actor::TouchSignalType             mTouchedSignal;
+  Dali::Actor::HoverSignalType             mHoveredSignal;
+  Dali::Actor::MouseWheelEventSignalType   mMouseWheelEventSignal;
+  Dali::Actor::SetSizeSignalType           mSetSizeSignal;
+  Dali::Actor::OnStageSignalType           mOnStageSignal;
+  Dali::Actor::OffStageSignalType          mOffStageSignal;
 
   Vector3         mSize;      ///< Event-side storage for size (not a pointer as most actors will have a size)
 

dali/internal/event/common/stage-impl.h
@@ -365,17 +365,17 @@ public:
   /**
    * @copydoc Dali::Stage::KeyEventSignal()
    */
-  Dali::Stage::KeyEventSignalV2& KeyEventSignal();
+  Dali::Stage::KeyEventSignalType& KeyEventSignal();
 
   /**
    * @copydoc Dali::Stage::SignalEventProcessingFinished()
    */
-  Dali::Stage::EventProcessingFinishedSignalV2& EventProcessingFinishedSignal();
+  Dali::Stage::EventProcessingFinishedSignalType& EventProcessingFinishedSignal();
 
   /**
     * @copydoc Dali::Stage::TouchedSignal()
     */
-  Dali::Stage::TouchedSignalV2& TouchedSignal();
+  Dali::Stage::TouchedSignalType& TouchedSignal();
 
   /**
    * @copydoc Dali::Stage::ContextLostSignal()
@@ -390,7 +390,7 @@ public:
   /**
    * @copydoc Dali::Stage::SceneCreatedSignal()
    */
-  Dali::Stage::SceneCreatedSignalV2& SceneCreatedSignal();
+  Dali::Stage::SceneCreatedSignalType& SceneCreatedSignal();
 
 private: // Implementation of ContextNotificationInterface:
 
@@ -472,18 +472,18 @@ private:
   Integration::SystemOverlay* mSystemOverlay; ///< SystemOverlay stage access
 
   // The key event signal
-  Dali::Stage::KeyEventSignalV2                 mKeyEventSignalV2;
+  Dali::Stage::KeyEventSignalType                 mKeyEventSignal;
 
   // The event processing finished signal
-  Dali::Stage::EventProcessingFinishedSignalV2  mEventProcessingFinishedSignalV2;
+  Dali::Stage::EventProcessingFinishedSignalType  mEventProcessingFinishedSignal;
 
   // The touched signal
-  Dali::Stage::TouchedSignalV2                  mTouchedSignalV2;
+  Dali::Stage::TouchedSignalType                  mTouchedSignal;
 
   Dali::Stage::ContextStatusSignal mContextLostSignal;
   Dali::Stage::ContextStatusSignal mContextRegainedSignal;
 
-  Dali::Stage::SceneCreatedSignalV2 mSceneCreatedSignal;
+  Dali::Stage::SceneCreatedSignalType mSceneCreatedSignal;
 };
 
 } // namespace Internal

dali/internal/event/events/long-press-gesture-detector-impl.cpp
@@ -129,7 +129,7 @@ void LongPressGestureDetector::EmitLongPressGestureSignal(Dali::Actor pressedAct
   // Guard against destruction during signal emission
   Dali::LongPressGestureDetector handle( this );
 
-  mDetectedSignalV2.Emit( pressedActor, longPress );
+  mDetectedSignal.Emit( pressedActor, longPress );
 }
 
 bool LongPressGestureDetector::DoConnectSignal( BaseObject* object, ConnectionTrackerInterface* tracker, const std::string& signalName, FunctorDelegate* functor )

dali/internal/event/events/tap-gesture-detector-impl.cpp
@@ -116,7 +116,7 @@ void TapGestureDetector::EmitTapGestureSignal(Dali::Actor tappedActor, const Tap
   // Guard against destruction during signal emission
   Dali::TapGestureDetector handle( this );
 
-  mDetectedSignalV2.Emit( tappedActor, tap );
+  mDetectedSignal.Emit( tappedActor, tap );
 }
 
 bool TapGestureDetector::DoConnectSignal( BaseObject* object, ConnectionTrackerInterface* tracker, const std::string& signalName, FunctorDelegate* functor )