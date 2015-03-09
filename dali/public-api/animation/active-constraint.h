tizenorg/platform.core.uifw.dali-core

dali-core property type comment to represent type instead of property enum
Change-Id: I3e3ef12b11b6984567c4ec1c54de6e54054ce8f0

dali/public-api/animation/active-constraint.h
@@ -52,7 +52,7 @@ public:
   {
     enum
     {
-      WEIGHT = DEFAULT_DERIVED_HANDLE_PROPERTY_START_INDEX, ///< name "weight", type FLOAT
+      WEIGHT = DEFAULT_DERIVED_HANDLE_PROPERTY_START_INDEX, ///< name "weight", type float
     };
   };
 

dali/public-api/animation/path.h
@@ -46,8 +46,8 @@ public:
   {
     enum
     {
-      POINTS         = DEFAULT_DERIVED_HANDLE_PROPERTY_START_INDEX, ///< name "points",         type VECTOR3
-      CONTROL_POINTS,                                               ///< name "control-points", type VECTOR3
+      POINTS         = DEFAULT_DERIVED_HANDLE_PROPERTY_START_INDEX, ///< name "points",         type Vector3
+      CONTROL_POINTS,                                               ///< name "control-points", type Vector3
     };
   };
 

dali/public-api/geometry/animatable-vertex.h
@@ -58,9 +58,9 @@ public:
   {
     enum
     {
-      POSITION      = DEFAULT_DERIVED_HANDLE_PROPERTY_START_INDEX, ///< name prefix "position-",        type VECTOR3
-      COLOR,                                                       ///< name prefix "color-",           type VECTOR4
-      TEXTURE_COORDS,                                              ///< name prefix "texture-coords-",  type VECTOR2
+      POSITION      = DEFAULT_DERIVED_HANDLE_PROPERTY_START_INDEX, ///< name prefix "position-",        type Vector3
+      COLOR,                                                       ///< name prefix "color-",           type Vector4
+      TEXTURE_COORDS,                                              ///< name prefix "texture-coords-",  type Vector2
     };
   };
 

dali/public-api/object/property-index-ranges.h
@@ -32,19 +32,19 @@ namespace Dali
  */
 enum PropertyRanges
 {
-  DEFAULT_ACTOR_PROPERTY_START_INDEX            = 0,          ///< Start index for Actor (and other base classes).
-  DEFAULT_ACTOR_PROPERTY_MAX_COUNT              = 10000,      ///< Actor range: 0 to 9999
+  DEFAULT_DERIVED_HANDLE_PROPERTY_START_INDEX   = 0,          ///< For all objects deriving from Handle (including Actors).
 
-  DEFAULT_DERIVED_HANDLE_PROPERTY_START_INDEX   = DEFAULT_ACTOR_PROPERTY_START_INDEX,         ///< For non-Actor objects.
+  DEFAULT_ACTOR_PROPERTY_START_INDEX            = DEFAULT_DERIVED_HANDLE_PROPERTY_START_INDEX, ///< Start index for Actor.
+  DEFAULT_ACTOR_PROPERTY_MAX_COUNT              = 10000,      ///< Actor range: 0 to 9999
 
   DEFAULT_DERIVED_ACTOR_PROPERTY_START_INDEX    = DEFAULT_ACTOR_PROPERTY_START_INDEX + DEFAULT_ACTOR_PROPERTY_MAX_COUNT, ///< Property start index for classes deriving directly from Actor.
 
   DEFAULT_PROPERTY_MAX_COUNT_PER_DERIVATION     = 10000,      ///< Second-level and onwards derived objects should increment their start index by this.
 
-  DEFAULT_GESTURE_DETECTOR_PROPERTY_START_INDEX = DEFAULT_DERIVED_ACTOR_PROPERTY_START_INDEX, ///< Used by PanGestureDetector.
+  DEFAULT_GESTURE_DETECTOR_PROPERTY_START_INDEX = DEFAULT_DERIVED_ACTOR_PROPERTY_START_INDEX,  ///< Used by PanGestureDetector.
 
   PROPERTY_REGISTRATION_START_INDEX             = 10000000,   ///< The index when registering a property should start from this number.
-  DEFAULT_PROPERTY_MAX_COUNT                    = PROPERTY_REGISTRATION_START_INDEX,          ///< Default Property Range: 0 to 9999999
+  DEFAULT_PROPERTY_MAX_COUNT                    = PROPERTY_REGISTRATION_START_INDEX,           ///< Default Property Range: 0 to 9999999
 
   PROPERTY_REGISTRATION_MAX_INDEX               = 19999999,   ///< The maximum index supported when registering a property
   PROPERTY_CUSTOM_START_INDEX                   = 50000000,   ///< The index at which custom properties start

dali/public-api/render-tasks/render-task.h
@@ -86,9 +86,9 @@ public:
   {
     enum
     {
-      VIEWPORT_POSITION = DEFAULT_DERIVED_HANDLE_PROPERTY_START_INDEX, ///< name "viewport-position", type VECTOR2
-      VIEWPORT_SIZE,                                                   ///< name "viewport-size",     type VECTOR2
-      CLEAR_COLOR,                                                     ///< name "clear-color",       type VECTOR4
+      VIEWPORT_POSITION = DEFAULT_DERIVED_HANDLE_PROPERTY_START_INDEX, ///< name "viewport-position", type Vector2
+      VIEWPORT_SIZE,                                                   ///< name "viewport-size",     type Vector2
+      CLEAR_COLOR,                                                     ///< name "clear-color",       type Vector4
     };
   };
 

dalihub/dali-core

dali/public-api/animation/active-constraint.h
@@ -52,7 +52,7 @@ public:
   {
     enum
     {
-      WEIGHT = DEFAULT_DERIVED_HANDLE_PROPERTY_START_INDEX, ///< name "weight", type FLOAT
+      WEIGHT = DEFAULT_OBJECT_PROPERTY_START_INDEX, ///< name "weight", type float
     };
   };
 

dali/public-api/animation/path.h
@@ -46,8 +46,8 @@ public:
   {
     enum
     {
-      POINTS         = DEFAULT_DERIVED_HANDLE_PROPERTY_START_INDEX, ///< name "points",         type VECTOR3
-      CONTROL_POINTS,                                               ///< name "control-points", type VECTOR3
+      POINTS         = DEFAULT_OBJECT_PROPERTY_START_INDEX, ///< name "points",         type Vector3
+      CONTROL_POINTS,                                       ///< name "control-points", type Vector3
     };
   };
 

dali/public-api/geometry/animatable-vertex.h
@@ -58,9 +58,9 @@ public:
   {
     enum
     {
-      POSITION      = DEFAULT_DERIVED_HANDLE_PROPERTY_START_INDEX, ///< name prefix "position-",        type VECTOR3
-      COLOR,                                                       ///< name prefix "color-",           type VECTOR4
-      TEXTURE_COORDS,                                              ///< name prefix "texture-coords-",  type VECTOR2
+      POSITION      = DEFAULT_OBJECT_PROPERTY_START_INDEX, ///< name prefix "position-",        type Vector3
+      COLOR,                                               ///< name prefix "color-",           type Vector4
+      TEXTURE_COORDS,                                      ///< name prefix "texture-coords-",  type Vector2
     };
   };
 

dali/public-api/object/property-index-ranges.h
@@ -32,19 +32,19 @@ namespace Dali
  */
 enum PropertyRanges
 {
-  DEFAULT_ACTOR_PROPERTY_START_INDEX            = 0,          ///< Start index for Actor (and other base classes).
-  DEFAULT_ACTOR_PROPERTY_MAX_COUNT              = 10000,      ///< Actor range: 0 to 9999
+  DEFAULT_OBJECT_PROPERTY_START_INDEX           = 0,          ///< For all objects deriving from Handle (including Actors).
 
-  DEFAULT_DERIVED_HANDLE_PROPERTY_START_INDEX   = DEFAULT_ACTOR_PROPERTY_START_INDEX,         ///< For non-Actor objects.
+  DEFAULT_ACTOR_PROPERTY_START_INDEX            = DEFAULT_OBJECT_PROPERTY_START_INDEX, ///< Start index for Actor.
+  DEFAULT_ACTOR_PROPERTY_MAX_COUNT              = 10000,      ///< Actor range: 0 to 9999
 
   DEFAULT_DERIVED_ACTOR_PROPERTY_START_INDEX    = DEFAULT_ACTOR_PROPERTY_START_INDEX + DEFAULT_ACTOR_PROPERTY_MAX_COUNT, ///< Property start index for classes deriving directly from Actor.
 
   DEFAULT_PROPERTY_MAX_COUNT_PER_DERIVATION     = 10000,      ///< Second-level and onwards derived objects should increment their start index by this.
 
-  DEFAULT_GESTURE_DETECTOR_PROPERTY_START_INDEX = DEFAULT_DERIVED_ACTOR_PROPERTY_START_INDEX, ///< Used by PanGestureDetector.
+  DEFAULT_GESTURE_DETECTOR_PROPERTY_START_INDEX = DEFAULT_DERIVED_ACTOR_PROPERTY_START_INDEX,  ///< Used by PanGestureDetector.
 
   PROPERTY_REGISTRATION_START_INDEX             = 10000000,   ///< The index when registering a property should start from this number.
-  DEFAULT_PROPERTY_MAX_COUNT                    = PROPERTY_REGISTRATION_START_INDEX,          ///< Default Property Range: 0 to 9999999
+  DEFAULT_PROPERTY_MAX_COUNT                    = PROPERTY_REGISTRATION_START_INDEX,           ///< Default Property Range: 0 to 9999999
 
   PROPERTY_REGISTRATION_MAX_INDEX               = 19999999,   ///< The maximum index supported when registering a property
   PROPERTY_CUSTOM_START_INDEX                   = 50000000,   ///< The index at which custom properties start

dali/public-api/render-tasks/render-task.h
@@ -86,9 +86,9 @@ public:
   {
     enum
     {
-      VIEWPORT_POSITION = DEFAULT_DERIVED_HANDLE_PROPERTY_START_INDEX, ///< name "viewport-position", type VECTOR2
-      VIEWPORT_SIZE,                                                   ///< name "viewport-size",     type VECTOR2
-      CLEAR_COLOR,                                                     ///< name "clear-color",       type VECTOR4
+      VIEWPORT_POSITION = DEFAULT_OBJECT_PROPERTY_START_INDEX, ///< name "viewport-position", type Vector2
+      VIEWPORT_SIZE,                                           ///< name "viewport-size",     type Vector2
+      CLEAR_COLOR,                                             ///< name "clear-color",       type Vector4
     };
   };
 