tizenorg/platform.core.uifw.dali-core

Changed passing of data providers to renderers
The data providers were previously passed as a tree of pointers - this
was wrong because they weren't double buffered in the data
provider implementations.

Instead of passing as a tree, we now collate all the data providers
into a single structure before passing it to the renderer. This is
an owned object, and is doubled buffered through the message system.

This also has the advantage of being able to pass a pointer to
the DataProviders structure around rather than individual pointers.

Change-Id: I3c61fef2e8b86c8e00a1779766158b1ff2bf153b
Signed-off-by: David Steele <david.steele@partner.samsung.com>

dali/internal/update/effects/scene-graph-material.cpp
@@ -69,7 +69,7 @@ void Material::RemoveSampler( const Sampler* sampler )
   const SamplerDataProvider* samplerDataProvider = sampler;
   bool found = false;
 
-  Samplers::Iterator iter = mSamplers.Begin();
+  DataProviders::Samplers::Iterator iter = mSamplers.Begin();
   for( ; iter != mSamplers.End(); ++iter )
   {
     if( *iter == samplerDataProvider )
@@ -90,7 +90,7 @@ void Material::RemoveSampler( const Sampler* sampler )
   }
 }
 
-const Material::Samplers& Material::GetSamplers() const
+const DataProviders::Samplers& Material::GetSamplers() const
 {
   return mSamplers;
 }

dali/internal/update/effects/scene-graph-material.h
@@ -26,7 +26,8 @@
 #include <dali/internal/update/common/property-owner.h>
 #include <dali/internal/update/common/scene-graph-connection-observers.h>
 #include <dali/internal/update/common/uniform-map.h>
-#include <dali/internal/render/data-providers/material-data-provider.h>
+#include <dali/internal/render/data-providers/data-providers.h>
+#include <dali/internal/render/data-providers/sampler-data-provider.h>
 
 namespace Dali
 {
@@ -106,7 +107,7 @@ public: // MaterialDataProvider implementation
    * Get the samplers this material uses.
    * @return the samplers
    */
-  virtual const Samplers& GetSamplers() const;
+  virtual const DataProviders::Samplers& GetSamplers() const;
 
 public: // UniformMap::Observer
   /**
@@ -127,7 +128,7 @@ public: // Property data
 
 private:
   const Shader* mShader;
-  Samplers mSamplers; // Not owned
+  DataProviders::Samplers mSamplers; // Not owned
   ConnectionObservers mConnectionObservers;
 
   // @todo MESH_REWORK add property values for cull face mode, blending options, blend color

dalihub/dali-core

dali/internal/update/effects/scene-graph-material.cpp
@@ -69,7 +69,7 @@ void Material::RemoveSampler( const Sampler* sampler )
   const SamplerDataProvider* samplerDataProvider = sampler;
   bool found = false;
 
-  Samplers::Iterator iter = mSamplers.Begin();
+  RenderDataProvider::Samplers::Iterator iter = mSamplers.Begin();
   for( ; iter != mSamplers.End(); ++iter )
   {
     if( *iter == samplerDataProvider )
@@ -90,7 +90,7 @@ void Material::RemoveSampler( const Sampler* sampler )
   }
 }
 
-const Material::Samplers& Material::GetSamplers() const
+const RenderDataProvider::Samplers& Material::GetSamplers() const
 {
   return mSamplers;
 }

dali/internal/update/effects/scene-graph-material.h
@@ -26,7 +26,7 @@
 #include <dali/internal/update/common/property-owner.h>
 #include <dali/internal/update/common/scene-graph-connection-observers.h>
 #include <dali/internal/update/common/uniform-map.h>
-#include <dali/internal/render/data-providers/material-data-provider.h>
+#include <dali/internal/render/data-providers/render-data-provider.h>
 
 namespace Dali
 {
@@ -106,7 +106,7 @@ public: // MaterialDataProvider implementation
    * Get the samplers this material uses.
    * @return the samplers
    */
-  virtual const Samplers& GetSamplers() const;
+  virtual const RenderDataProvider::Samplers& GetSamplers() const;
 
 public: // UniformMap::Observer
   /**
@@ -127,7 +127,7 @@ public: // Property data
 
 private:
   const Shader* mShader;
-  Samplers mSamplers; // Not owned
+  RenderDataProvider::Samplers mSamplers; // Not owned
   ConnectionObservers mConnectionObservers;
 
   // @todo MESH_REWORK add property values for cull face mode, blending options, blend color