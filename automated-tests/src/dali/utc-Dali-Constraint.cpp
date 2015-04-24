tizenorg/platform.core.uifw.dali-core

AlphaFunction refactoring
Removed all the alpha functions from public api. Alpha functions can now be
defined using an enum to use one of the predefined alpha functions, a pointer,
to define a custom alpha function, or two Vector2 which are used for cubic bezier
alpha function.

The new API is used as follows:

//Using a predefined alpha function
animation.AnimateTo( property, value, AlphaFunction::EASE_IN_OUT );

//Using a custom alpha function
animation.AnimateTo( property, value, &my_alpha_function );

//Using a bezier alpha function
animation.AnimateTo( property, value, AlphaFunction(Vector2(0.0f,1.0f),Vector2(1.0f,0.0f) ) );

Change-Id: I4051c752d826770447923b6e9a3215f2575d6370

automated-tests/src/dali/utc-Dali-Constraint.cpp
@@ -2425,7 +2425,7 @@ int UtcDaliConstraintSetAlphaFunction(void)
   // Test the alpha-function itself
 
   AlphaFunction func = constraint.GetAlphaFunction();
-  DALI_TEST_EQUALS(func(0.1f), 0.1f, TEST_LOCATION); // Default is Linear
+  DALI_TEST_EQUALS(func.GetPredefinedFunction(), AlphaFunction::DEFAULT, TEST_LOCATION); // Default is Linear
 
   // Test that the alpha-function is used correctly
 
@@ -2486,9 +2486,9 @@ int UtcDaliConstraintSetAlphaFunction(void)
 
   // Change to non-linear alpha and retest
 
-  constraint.SetAlphaFunction(AlphaFunctions::EaseIn);
+  constraint.SetAlphaFunction(AlphaFunction::EASE_IN);
   func = constraint.GetAlphaFunction();
-  DALI_TEST_CHECK(func(0.1f) < 0.09f);
+  DALI_TEST_EQUALS(func.GetPredefinedFunction(), AlphaFunction::EASE_IN, TEST_LOCATION);
 
   actor.ApplyConstraint( constraint );
 
@@ -2522,7 +2522,7 @@ int UtcDaliConstraintGetAlphaFunction(void)
   Constraint constraint = Constraint::New<Vector4>( Actor::Property::COLOR, TestConstraint() );
 
   AlphaFunction func = constraint.GetAlphaFunction();
-  DALI_TEST_EQUALS(func(0.5f), 0.5f, TEST_LOCATION); // Default is Linear
+  DALI_TEST_EQUALS(func.GetPredefinedFunction(), AlphaFunction::DEFAULT, TEST_LOCATION);
   END_TEST;
 }
 

dalihub/dali-core

automated-tests/src/dali/utc-Dali-Constraint.cpp
@@ -2632,7 +2632,7 @@ int UtcDaliConstraintSetAlphaFunction(void)
   // Test the alpha-function itself
 
   AlphaFunction func = constraint.GetAlphaFunction();
-  DALI_TEST_EQUALS(func(0.1f), 0.1f, TEST_LOCATION); // Default is Linear
+  DALI_TEST_EQUALS(func.GetBuiltinFunction(), AlphaFunction::DEFAULT, TEST_LOCATION); // Default is Linear
 
   // Test that the alpha-function is used correctly
 
@@ -2693,9 +2693,9 @@ int UtcDaliConstraintSetAlphaFunction(void)
 
   // Change to non-linear alpha and retest
 
-  constraint.SetAlphaFunction(AlphaFunctions::EaseIn);
+  constraint.SetAlphaFunction(AlphaFunction::EASE_IN);
   func = constraint.GetAlphaFunction();
-  DALI_TEST_CHECK(func(0.1f) < 0.09f);
+  DALI_TEST_EQUALS(func.GetBuiltinFunction(), AlphaFunction::EASE_IN, TEST_LOCATION);
 
   actor.ApplyConstraint( constraint );
 
@@ -2729,7 +2729,7 @@ int UtcDaliConstraintGetAlphaFunction(void)
   Constraint constraint = Constraint::New<Vector4>( Actor::Property::COLOR, TestConstraint() );
 
   AlphaFunction func = constraint.GetAlphaFunction();
-  DALI_TEST_EQUALS(func(0.5f), 0.5f, TEST_LOCATION); // Default is Linear
+  DALI_TEST_EQUALS(func.GetBuiltinFunction(), AlphaFunction::DEFAULT, TEST_LOCATION);
   END_TEST;
 }
 