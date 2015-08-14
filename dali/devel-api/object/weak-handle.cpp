tizenorg/platform.core.uifw.dali-core

Implemented WeakHandle in dali-core
WeakHandle stores a weak pointer to an internal Dali object. The handle to the object
can be accessed if the object exists, and such access is not reference counted. When
the object is deleted, the weak pointer will be set to NULL, and any further attmpt
to access to a deleted object will return an empty handle.

Change-Id: I56c8b0ea398f22c1d735270e6287124bdcea740b

dali/devel-api/object/weak-handle.cpp
@@ -1,0 +1,135 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+// CLASS HEADER
+#include <dali/devel-api/object/weak-handle.h>
+
+// INTERNAL INCLUDES
+#include <dali/internal/event/common/object-impl.h>
+
+namespace Dali
+{
+
+struct WeakHandle::Impl : public Internal::Object::Observer
+{
+  // Construction
+  Impl()
+  : mObject( NULL )
+  {
+  }
+
+  // Construction
+  Impl( Handle& handle )
+  : mObject( NULL )
+  {
+    if(handle)
+    {
+      mObject = dynamic_cast<Internal::Object*>( handle.GetObjectPtr() );
+      if(mObject)
+      {
+        mObject->AddObserver( *this );
+      }
+    }
+  }
+
+  // Destruction
+  ~Impl()
+  {
+    if( mObject )
+    {
+      mObject->RemoveObserver( *this );
+    }
+  }
+
+  /**
+   * From Object::Observer
+   */
+  virtual void SceneObjectAdded( Internal::Object& object )
+  {
+  }
+
+  /**
+   * From Object::Observer
+   */
+  virtual void SceneObjectRemoved( Internal::Object& object )
+  {
+  }
+
+  /**
+   * From Object::Observer
+   */
+  virtual void ObjectDestroyed( Internal::Object& object )
+  {
+    mObject = NULL;
+  }
+
+  // Data
+  Internal::Object* mObject;
+};
+
+WeakHandle::WeakHandle()
+: mImpl( new Impl() )
+{
+}
+
+WeakHandle::WeakHandle( Handle& handle )
+: mImpl( new Impl( handle ) )
+{
+}
+
+WeakHandle::~WeakHandle()
+{
+  delete mImpl;
+  mImpl = NULL;
+}
+
+WeakHandle::WeakHandle(const WeakHandle& handle)
+: mImpl( NULL )
+{
+  Handle object = handle.GetHandle();
+  mImpl = new Impl(object);
+}
+
+WeakHandle& WeakHandle::operator=( const WeakHandle& rhs )
+{
+  if( this != &rhs )
+  {
+    delete mImpl;
+
+    Handle handle = rhs.GetHandle();
+    mImpl = new Impl(handle);
+  }
+
+  return *this;
+}
+
+bool WeakHandle::operator==( const WeakHandle& rhs ) const
+{
+  return this->mImpl->mObject == rhs.mImpl->mObject;
+}
+
+bool WeakHandle::operator!=( const WeakHandle& rhs ) const
+{
+  return !( *this == rhs );
+}
+
+Handle WeakHandle::GetHandle() const
+{
+  return Handle( mImpl->mObject );
+}
+
+} // Dali

dalihub/dali-core

dali/devel-api/object/weak-handle.cpp
@@ -1,0 +1,135 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+// CLASS HEADER
+#include <dali/devel-api/object/weak-handle.h>
+
+// INTERNAL INCLUDES
+#include <dali/internal/event/common/object-impl.h>
+
+namespace Dali
+{
+
+struct WeakHandleBase::Impl : public Internal::Object::Observer
+{
+  // Construction
+  Impl()
+  : mObject( NULL )
+  {
+  }
+
+  // Construction
+  Impl( Handle& handle )
+  : mObject( NULL )
+  {
+    if(handle)
+    {
+      mObject = static_cast<Internal::Object*>( handle.GetObjectPtr() );
+      if(mObject)
+      {
+        mObject->AddObserver( *this );
+      }
+    }
+  }
+
+  // Destruction
+  ~Impl()
+  {
+    if( mObject )
+    {
+      mObject->RemoveObserver( *this );
+    }
+  }
+
+  /**
+   * From Object::Observer
+   */
+  virtual void SceneObjectAdded( Internal::Object& object )
+  {
+  }
+
+  /**
+   * From Object::Observer
+   */
+  virtual void SceneObjectRemoved( Internal::Object& object )
+  {
+  }
+
+  /**
+   * From Object::Observer
+   */
+  virtual void ObjectDestroyed( Internal::Object& object )
+  {
+    mObject = NULL;
+  }
+
+  // Data
+  Internal::Object* mObject;
+};
+
+WeakHandleBase::WeakHandleBase()
+: mImpl( new Impl() )
+{
+}
+
+WeakHandleBase::WeakHandleBase( Handle& handle )
+: mImpl( new Impl( handle ) )
+{
+}
+
+WeakHandleBase::~WeakHandleBase()
+{
+  delete mImpl;
+  mImpl = NULL;
+}
+
+WeakHandleBase::WeakHandleBase(const WeakHandleBase& handle)
+: mImpl( NULL )
+{
+  Handle object = handle.GetBaseHandle();
+  mImpl = new Impl(object);
+}
+
+WeakHandleBase& WeakHandleBase::operator=( const WeakHandleBase& rhs )
+{
+  if( this != &rhs )
+  {
+    delete mImpl;
+
+    Handle handle = rhs.GetBaseHandle();
+    mImpl = new Impl(handle);
+  }
+
+  return *this;
+}
+
+bool WeakHandleBase::operator==( const WeakHandleBase& rhs ) const
+{
+  return this->mImpl->mObject == rhs.mImpl->mObject;
+}
+
+bool WeakHandleBase::operator!=( const WeakHandleBase& rhs ) const
+{
+  return !( *this == rhs );
+}
+
+Handle WeakHandleBase::GetBaseHandle() const
+{
+  return Handle( mImpl->mObject );
+}
+
+} // Dali