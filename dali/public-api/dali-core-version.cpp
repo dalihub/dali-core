783137c838cc467f6b46158bf35deb72d0576bf0
Chu Hoang <c.hoang@samsung.com>
2015-08-20 13:32:56 +0100

    DALi Version 1.1.0
    Change-Id: I5a94629a418273369cd7cf3fa79d421676519175

--- dali/public-api/dali-core-version.cpp
+++ dali/public-api/dali-core-version.cpp
@@ -27,8 +27,8 @@ namespace Dali
 {
 
 const unsigned int CORE_MAJOR_VERSION = 1;
-const unsigned int CORE_MINOR_VERSION = 0;
-const unsigned int CORE_MICRO_VERSION = 53;
+const unsigned int CORE_MINOR_VERSION = 1;
+const unsigned int CORE_MICRO_VERSION = 0;
 const char * const CORE_BUILD_DATE    = __DATE__ " " __TIME__;
 
 #ifdef DEBUG_ENABLED
