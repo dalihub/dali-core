9e9b06befca53bf0fb7320f1db3eb704bae610a7
taeyoon <taeyoon0.lee@samsung.com>
2015-11-19 22:01:56 +0900

    [3.0] NativeImageSource with tbm_surface for tizen 3.0 wayland
    Change-Id: I5c8cadd96f87d4d1db9e30101cd647c697e3c7ea

--- dali/public-api/images/native-image.h
+++ dali/public-api/images/native-image.h
@@ -99,9 +99,23 @@ public:
    * If handle points to a NativeImage object, the downcast produces valid handle.
    * If not, the returned handle is left unintialized.
    * @param[in] handle Handle to an object.
-   * @return handle to a NativeImage or an uninitialized handle.
+   * @return Handle to a NativeImage or an uninitialized handle.
    */
   static NativeImage DownCast( BaseHandle handle );
+
+  /**
+   * @brief Get custom fragment prefix for rendering a native image.
+   *
+   * @return String for custom fragment prefix
+   */
+  const char* GetCustomFragmentPreFix();
+
+  /**
+   * @brief Get custom sampler type name for rendering a native image.
+   *
+   * @return String for custom sampler type name
+   */
+  const char* GetCustomSamplerTypename();
 
 public: // Not intended for application developers
 
