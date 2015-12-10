tizenorg/platform.core.uifw.dali-core

Apply constraints when traversing the scene to update
Apply constraints when traversing the scene to update instead of
in a separate traversal

Change-Id: Idc24668a77a896e1b50eb738811969d7b2778086

dali/internal/update/manager/update-manager.h
@@ -519,12 +519,16 @@ private:
   void Animate( BufferIndex bufferIndex, float elapsedSeconds );
 
   /**
-   * Perform constraint updates.
-   * @note Applies constraints to nodes first (depth first search order).
-   * Then shader constraints second (construction order)
+   * Applies constraints to RenderTasks
    * @param[in] bufferIndex to use
    */
-  void ApplyConstraints( BufferIndex bufferIndex );
+  void ApplyConstraintsToRenderTasks( BufferIndex bufferIndex );
+
+  /**
+   * Applies constraints to Shaders
+   * @param[in] bufferIndex to use
+   */
+  void ApplyConstraintsToShaders( BufferIndex bufferIndex );
 
   /**
    * Perform property notification updates
@@ -535,7 +539,7 @@ private:
   /**
    * Prepare materials for rendering
    */
-  void PrepareMaterials();
+  void PrepareMaterials( BufferIndex bufferIndex );
 
   /**
    * Pass shader binaries queued here on to event thread.

dalihub/dali-core

dali/internal/update/manager/update-manager.h
@@ -519,12 +519,22 @@ private:
   void Animate( BufferIndex bufferIndex, float elapsedSeconds );
 
   /**
-   * Perform constraint updates.
-   * @note Applies constraints to nodes first (depth first search order).
-   * Then shader constraints second (construction order)
+   * Applies constraints to CustomObjects
    * @param[in] bufferIndex to use
    */
-  void ApplyConstraints( BufferIndex bufferIndex );
+  void ConstrainCustomObjects( BufferIndex bufferIndex );
+
+  /**
+   * Applies constraints to RenderTasks
+   * @param[in] bufferIndex to use
+   */
+  void ConstrainRenderTasks( BufferIndex bufferIndex );
+
+  /**
+   * Applies constraints to Shaders
+   * @param[in] bufferIndex to use
+   */
+  void ConstrainShaders( BufferIndex bufferIndex );
 
   /**
    * Perform property notification updates
@@ -535,7 +545,7 @@ private:
   /**
    * Prepare materials for rendering
    */
-  void PrepareMaterials();
+  void PrepareMaterials( BufferIndex bufferIndex );
 
   /**
    * Pass shader binaries queued here on to event thread.