tizenorg/platform.core.uifw.dali-core

Removed sets of connected/disconnected nodes. Brought back a vector of sorted nodes to support better cache usage.
Change-Id: Ifb0aef63759639df77c1c58e5a9b799d7f02f8b0

dali/internal/update/manager/update-manager.cpp
@@ -103,25 +103,6 @@ namespace Internal
 namespace SceneGraph
 {
 
-namespace
-{
-
-void DestroyNodeSet( std::set<Node*>& nodeSet )
-{
-  for( std::set<Node*>::iterator iter = nodeSet.begin(); iter != nodeSet.end(); ++iter )
-  {
-    Node* node( *iter );
-
-    // Call Node::OnDestroy as each node is destroyed
-    node->OnDestroy();
-
-    delete node;
-  }
-  nodeSet.clear();
-}
-
-} //namespace
-
 typedef OwnerContainer< Shader* >              ShaderContainer;
 typedef ShaderContainer::Iterator              ShaderIter;
 typedef ShaderContainer::ConstIterator         ShaderConstIter;
@@ -203,9 +184,13 @@ struct UpdateManager::Impl
     }
 
     // UpdateManager owns the Nodes
-    DestroyNodeSet( activeDisconnectedNodes );
-    DestroyNodeSet( connectedNodes );
-    DestroyNodeSet( disconnectedNodes );
+    Vector<Node*>::Iterator iter = nodeRegistry.Begin();
+    Vector<Node*>::Iterator endIter = nodeRegistry.End();
+    for(;iter!=endIter;++iter)
+    {
+      (*iter)->OnDestroy();
+      delete(*iter);
+    }
 
     // If there is root, reset it, otherwise do nothing as rendering was never started
     if( root )
@@ -250,9 +235,8 @@ struct UpdateManager::Impl
 
   Layer*                              root;                          ///< The root node (root is a layer)
   Layer*                              systemLevelRoot;               ///< A separate root-node for system-level content
-  std::set< Node* >                   activeDisconnectedNodes;       ///< A container of new or modified nodes (without parent) owned by UpdateManager
-  std::set< Node* >                   connectedNodes;                ///< A container of connected (with parent) nodes owned by UpdateManager
-  std::set< Node* >                   disconnectedNodes;             ///< A container of inactive disconnected nodes (without parent) owned by UpdateManager
+
+  Vector<Node*>                       nodeRegistry;                  ///< A container of all instantiated nodes
 
   SortedLayerPointers                 sortedLayers;                  ///< A container of Layer pointers sorted by depth
   SortedLayerPointers                 systemLevelSortedLayers;       ///< A separate container of system-level Layers
@@ -342,7 +326,16 @@ void UpdateManager::AddNode( Node* node )
   DALI_ASSERT_ALWAYS( NULL != node );
   DALI_ASSERT_ALWAYS( NULL == node->GetParent() ); // Should not have a parent yet
 
-  mImpl->activeDisconnectedNodes.insert( node ); // Takes ownership of node
+  Vector<Node*>::Iterator iter = mImpl->nodeRegistry.Begin();
+  Vector<Node*>::Iterator end = mImpl->nodeRegistry.End();
+  Vector<Node*>::Iterator lastIter = iter;
+  for(;iter!=end;++iter)
+  {
+    if(*iter > node)
+      break;
+    lastIter = iter;
+  }
+  mImpl->nodeRegistry.Insert(lastIter, node);
 }
 
 void UpdateManager::ConnectNode( Node* parent, Node* node )
@@ -350,15 +343,6 @@ void UpdateManager::ConnectNode( Node* parent, Node* node )
   DALI_ASSERT_ALWAYS( NULL != parent );
   DALI_ASSERT_ALWAYS( NULL != node );
   DALI_ASSERT_ALWAYS( NULL == node->GetParent() ); // Should not have a parent yet
-
-  // Move from active/disconnectedNodes to connectedNodes
-  std::set<Node*>::size_type removed = mImpl->activeDisconnectedNodes.erase( node );
-  if( !removed )
-  {
-    removed = mImpl->disconnectedNodes.erase( node );
-    DALI_ASSERT_ALWAYS( removed );
-  }
-  mImpl->connectedNodes.insert( node );
 
   node->SetActive( true );
 
@@ -371,19 +355,14 @@ void UpdateManager::DisconnectNode( Node* node )
   DALI_ASSERT_ALWAYS( NULL != parent );
   parent->SetDirtyFlag( ChildDeletedFlag ); // make parent dirty so that render items dont get reused
 
-  // Move from connectedNodes to activeDisconnectedNodes (reset properties next frame)
-  parent->DisconnectChild( mSceneGraphBuffers.GetUpdateBufferIndex(), *node, mImpl->connectedNodes, mImpl->activeDisconnectedNodes );
+  parent->DisconnectChild( mSceneGraphBuffers.GetUpdateBufferIndex(), *node, mImpl->discardQueue );
+  node->SetActive(false);
 }
 
 void UpdateManager::SetNodeActive( Node* node )
 {
   DALI_ASSERT_ALWAYS( NULL != node );
   DALI_ASSERT_ALWAYS( NULL == node->GetParent() ); // Should not have a parent yet
-
-  // Move from disconnectedNodes to activeDisconnectedNodes (reset properties next frame)
-  std::set<Node*>::size_type removed = mImpl->disconnectedNodes.erase( node );
-  DALI_ASSERT_ALWAYS( removed );
-  mImpl->activeDisconnectedNodes.insert( node );
 
   node->SetActive( true );
 }
@@ -393,14 +372,17 @@ void UpdateManager::DestroyNode( Node* node )
   DALI_ASSERT_ALWAYS( NULL != node );
   DALI_ASSERT_ALWAYS( NULL == node->GetParent() ); // Should have been disconnected
 
-  // Transfer ownership from new/disconnectedNodes to the discard queue
-  // This keeps the nodes alive, until the render-thread has finished with them
-  std::set<Node*>::size_type removed = mImpl->activeDisconnectedNodes.erase( node );
-  if( !removed )
+  Vector<Node*>::Iterator iter = mImpl->nodeRegistry.Begin();
+  Vector<Node*>::Iterator endIter = mImpl->nodeRegistry.End();
+  for(;iter!=endIter;++iter)
   {
-    removed = mImpl->disconnectedNodes.erase( node );
-    DALI_ASSERT_ALWAYS( removed );
+    if((*iter) == node)
+    {
+      mImpl->nodeRegistry.Erase(iter);
+      break;
+    }
   }
+
   mImpl->discardQueue.Add( mSceneGraphBuffers.GetUpdateBufferIndex(), node );
 
   // Notify the Node about impending destruction
@@ -680,22 +662,11 @@ void UpdateManager::ResetProperties( BufferIndex bufferIndex )
   }
 
   // Reset the Connected Nodes
-  const std::set<Node*>::iterator endIter = mImpl->connectedNodes.end();
-  for( std::set<Node*>::iterator iter = mImpl->connectedNodes.begin(); endIter != iter; ++iter )
+  Vector<Node*>::Iterator iter = mImpl->nodeRegistry.Begin();
+  Vector<Node*>::Iterator endIter = mImpl->nodeRegistry.End();
+  for(;iter != endIter; ++iter)
   {
     (*iter)->ResetToBaseValues( bufferIndex );
-  }
-
-  // If a Node is disconnected, it may still be "active" (requires a reset in next frame)
-  for( std::set<Node*>::iterator iter = mImpl->activeDisconnectedNodes.begin(); mImpl->activeDisconnectedNodes.end() != iter; iter = mImpl->activeDisconnectedNodes.begin() )
-  {
-    Node* node = *iter;
-    node->ResetToBaseValues( bufferIndex );
-    node->SetActive( false );
-
-    // Move everything from activeDisconnectedNodes to disconnectedNodes (no need to reset again)
-    mImpl->activeDisconnectedNodes.erase( iter );
-    mImpl->disconnectedNodes.insert( node );
   }
 
   // Reset system-level render-task list properties to base values
@@ -723,7 +694,6 @@ void UpdateManager::ResetProperties( BufferIndex bufferIndex )
   mImpl->materials.ResetToBaseValues( bufferIndex );
   mImpl->geometries.ResetToBaseValues( bufferIndex );
   mImpl->renderers.ResetToBaseValues( bufferIndex );
-
 
   // Reset animatable shader properties to base values
   for (ShaderIter iter = mImpl->shaders.Begin(); iter != mImpl->shaders.End(); ++iter)

dali/internal/update/nodes/node.h
@@ -291,10 +291,9 @@ public:
    * @pre childNode is a child of this Node.
    * @param[in] updateBufferIndex The current update buffer index.
    * @param[in] childNode The node to disconnect.
-   * @param[in] connectedNodes Disconnected Node attachments should be removed from here.
-   * @param[in] disconnectedNodes Disconnected Node attachments should be added here.
+   * @param[in] discardQueue Disconnected Node attachments should be added here.
    */
-  void DisconnectChild( BufferIndex updateBufferIndex, Node& childNode, std::set<Node*>& connectedNodes,  std::set<Node*>& disconnectedNodes );
+  void DisconnectChild( BufferIndex updateBufferIndex, Node& childNode, DiscardQueue& discardQueue );
 
   /**
    * Retrieve the children a Node.
@@ -1020,10 +1019,9 @@ private:
    * Recursive helper to disconnect a Node and its children.
    * Disconnected Nodes have no parent or children.
    * @param[in] updateBufferIndex The current update buffer index.
-   * @param[in] connectedNodes Disconnected Node attachments should be removed from here.
-   * @param[in] disconnectedNodes Disconnected Node attachments should be added here.
+   * @param[in] discardQueue Disconnected Node attachments should be added here.
    */
-  void RecursiveDisconnectFromSceneGraph( BufferIndex updateBufferIndex, std::set<Node*>& connectedNodes, std::set<Node*>& disconnectedNodes );
+  void RecursiveDisconnectFromSceneGraph( BufferIndex updateBufferIndex, DiscardQueue& discardQueue );
 
 public: // Default properties
 

dalihub/dali-core

dali/internal/update/manager/update-manager.cpp
@@ -103,25 +103,6 @@ namespace Internal
 namespace SceneGraph
 {
 
-namespace
-{
-
-void DestroyNodeSet( std::set<Node*>& nodeSet )
-{
-  for( std::set<Node*>::iterator iter = nodeSet.begin(); iter != nodeSet.end(); ++iter )
-  {
-    Node* node( *iter );
-
-    // Call Node::OnDestroy as each node is destroyed
-    node->OnDestroy();
-
-    delete node;
-  }
-  nodeSet.clear();
-}
-
-} //namespace
-
 typedef OwnerContainer< Shader* >              ShaderContainer;
 typedef ShaderContainer::Iterator              ShaderIter;
 typedef ShaderContainer::ConstIterator         ShaderConstIter;
@@ -185,6 +166,9 @@ struct UpdateManager::Impl
     renderers.SetSceneController( *sceneController );
     geometries.SetSceneController( *sceneController );
     materials.SetSceneController( *sceneController );
+
+    // create first 'dummy' node
+    nodes.PushBack(0u);
   }
 
   ~Impl()
@@ -203,9 +187,13 @@ struct UpdateManager::Impl
     }
 
     // UpdateManager owns the Nodes
-    DestroyNodeSet( activeDisconnectedNodes );
-    DestroyNodeSet( connectedNodes );
-    DestroyNodeSet( disconnectedNodes );
+    Vector<Node*>::Iterator iter = nodes.Begin()+1;
+    Vector<Node*>::Iterator endIter = nodes.End();
+    for(;iter!=endIter;++iter)
+    {
+      (*iter)->OnDestroy();
+      delete(*iter);
+    }
 
     // If there is root, reset it, otherwise do nothing as rendering was never started
     if( root )
@@ -250,9 +238,8 @@ struct UpdateManager::Impl
 
   Layer*                              root;                          ///< The root node (root is a layer)
   Layer*                              systemLevelRoot;               ///< A separate root-node for system-level content
-  std::set< Node* >                   activeDisconnectedNodes;       ///< A container of new or modified nodes (without parent) owned by UpdateManager
-  std::set< Node* >                   connectedNodes;                ///< A container of connected (with parent) nodes owned by UpdateManager
-  std::set< Node* >                   disconnectedNodes;             ///< A container of inactive disconnected nodes (without parent) owned by UpdateManager
+
+  Vector<Node*>                       nodes;                         ///< A container of all instantiated nodes
 
   SortedLayerPointers                 sortedLayers;                  ///< A container of Layer pointers sorted by depth
   SortedLayerPointers                 systemLevelSortedLayers;       ///< A separate container of system-level Layers
@@ -342,7 +329,16 @@ void UpdateManager::AddNode( Node* node )
   DALI_ASSERT_ALWAYS( NULL != node );
   DALI_ASSERT_ALWAYS( NULL == node->GetParent() ); // Should not have a parent yet
 
-  mImpl->activeDisconnectedNodes.insert( node ); // Takes ownership of node
+  // Nodes must be sorted by pointer
+  Vector<Node*>::Iterator begin = mImpl->nodes.Begin();
+  for(Vector<Node*>::Iterator iter = mImpl->nodes.End()-1; iter >= begin; --iter)
+  {
+    if(node > (*iter))
+    {
+      mImpl->nodes.Insert((iter+1), node);
+      break;
+    }
+  }
 }
 
 void UpdateManager::ConnectNode( Node* parent, Node* node )
@@ -350,17 +346,6 @@ void UpdateManager::ConnectNode( Node* parent, Node* node )
   DALI_ASSERT_ALWAYS( NULL != parent );
   DALI_ASSERT_ALWAYS( NULL != node );
   DALI_ASSERT_ALWAYS( NULL == node->GetParent() ); // Should not have a parent yet
-
-  // Move from active/disconnectedNodes to connectedNodes
-  std::set<Node*>::size_type removed = mImpl->activeDisconnectedNodes.erase( node );
-  if( !removed )
-  {
-    removed = mImpl->disconnectedNodes.erase( node );
-    DALI_ASSERT_ALWAYS( removed );
-  }
-  mImpl->connectedNodes.insert( node );
-
-  node->SetActive( true );
 
   parent->ConnectChild( node );
 }
@@ -371,21 +356,7 @@ void UpdateManager::DisconnectNode( Node* node )
   DALI_ASSERT_ALWAYS( NULL != parent );
   parent->SetDirtyFlag( ChildDeletedFlag ); // make parent dirty so that render items dont get reused
 
-  // Move from connectedNodes to activeDisconnectedNodes (reset properties next frame)
-  parent->DisconnectChild( mSceneGraphBuffers.GetUpdateBufferIndex(), *node, mImpl->connectedNodes, mImpl->activeDisconnectedNodes );
-}
-
-void UpdateManager::SetNodeActive( Node* node )
-{
-  DALI_ASSERT_ALWAYS( NULL != node );
-  DALI_ASSERT_ALWAYS( NULL == node->GetParent() ); // Should not have a parent yet
-
-  // Move from disconnectedNodes to activeDisconnectedNodes (reset properties next frame)
-  std::set<Node*>::size_type removed = mImpl->disconnectedNodes.erase( node );
-  DALI_ASSERT_ALWAYS( removed );
-  mImpl->activeDisconnectedNodes.insert( node );
-
-  node->SetActive( true );
+  parent->DisconnectChild( mSceneGraphBuffers.GetUpdateBufferIndex(), *node );
 }
 
 void UpdateManager::DestroyNode( Node* node )
@@ -393,14 +364,17 @@ void UpdateManager::DestroyNode( Node* node )
   DALI_ASSERT_ALWAYS( NULL != node );
   DALI_ASSERT_ALWAYS( NULL == node->GetParent() ); // Should have been disconnected
 
-  // Transfer ownership from new/disconnectedNodes to the discard queue
-  // This keeps the nodes alive, until the render-thread has finished with them
-  std::set<Node*>::size_type removed = mImpl->activeDisconnectedNodes.erase( node );
-  if( !removed )
+  Vector<Node*>::Iterator iter = mImpl->nodes.Begin()+1;
+  Vector<Node*>::Iterator endIter = mImpl->nodes.End();
+  for(;iter!=endIter;++iter)
   {
-    removed = mImpl->disconnectedNodes.erase( node );
-    DALI_ASSERT_ALWAYS( removed );
+    if((*iter) == node)
+    {
+      mImpl->nodes.Erase(iter);
+      break;
+    }
   }
+
   mImpl->discardQueue.Add( mSceneGraphBuffers.GetUpdateBufferIndex(), node );
 
   // Notify the Node about impending destruction
@@ -679,23 +653,12 @@ void UpdateManager::ResetProperties( BufferIndex bufferIndex )
     mImpl->systemLevelRoot->ResetToBaseValues( bufferIndex );
   }
 
-  // Reset the Connected Nodes
-  const std::set<Node*>::iterator endIter = mImpl->connectedNodes.end();
-  for( std::set<Node*>::iterator iter = mImpl->connectedNodes.begin(); endIter != iter; ++iter )
+  // Reset all the nodes
+  Vector<Node*>::Iterator iter = mImpl->nodes.Begin()+1;
+  Vector<Node*>::Iterator endIter = mImpl->nodes.End();
+  for(;iter != endIter; ++iter)
   {
     (*iter)->ResetToBaseValues( bufferIndex );
-  }
-
-  // If a Node is disconnected, it may still be "active" (requires a reset in next frame)
-  for( std::set<Node*>::iterator iter = mImpl->activeDisconnectedNodes.begin(); mImpl->activeDisconnectedNodes.end() != iter; iter = mImpl->activeDisconnectedNodes.begin() )
-  {
-    Node* node = *iter;
-    node->ResetToBaseValues( bufferIndex );
-    node->SetActive( false );
-
-    // Move everything from activeDisconnectedNodes to disconnectedNodes (no need to reset again)
-    mImpl->activeDisconnectedNodes.erase( iter );
-    mImpl->disconnectedNodes.insert( node );
   }
 
   // Reset system-level render-task list properties to base values
@@ -723,7 +686,6 @@ void UpdateManager::ResetProperties( BufferIndex bufferIndex )
   mImpl->materials.ResetToBaseValues( bufferIndex );
   mImpl->geometries.ResetToBaseValues( bufferIndex );
   mImpl->renderers.ResetToBaseValues( bufferIndex );
-
 
   // Reset animatable shader properties to base values
   for (ShaderIter iter = mImpl->shaders.Begin(); iter != mImpl->shaders.End(); ++iter)

dali/internal/update/nodes/node.h
@@ -118,25 +118,6 @@ public:
   void operator delete( void* ptr );
 
   /**
-   * When a Node is marked "active" it has been disconnected, but its properties have been modified.
-   * @note An inactive Node will be skipped during the UpdateManager ResetProperties stage.
-   * @param[in] isActive True if the Node is active.
-   */
-  void SetActive( bool isActive )
-  {
-    mIsActive = isActive;
-  }
-
-  /**
-   * Query whether the Node is active.
-   * @return True if the Node is active.
-   */
-  bool IsActive() const
-  {
-    return mIsActive;
-  }
-
-  /**
    * Called during UpdateManager::DestroyNode shortly before Node is destroyed.
    */
   void OnDestroy();
@@ -291,10 +272,8 @@ public:
    * @pre childNode is a child of this Node.
    * @param[in] updateBufferIndex The current update buffer index.
    * @param[in] childNode The node to disconnect.
-   * @param[in] connectedNodes Disconnected Node attachments should be removed from here.
-   * @param[in] disconnectedNodes Disconnected Node attachments should be added here.
    */
-  void DisconnectChild( BufferIndex updateBufferIndex, Node& childNode, std::set<Node*>& connectedNodes,  std::set<Node*>& disconnectedNodes );
+  void DisconnectChild( BufferIndex updateBufferIndex, Node& childNode );
 
   /**
    * Retrieve the children a Node.
@@ -1020,10 +999,8 @@ private:
    * Recursive helper to disconnect a Node and its children.
    * Disconnected Nodes have no parent or children.
    * @param[in] updateBufferIndex The current update buffer index.
-   * @param[in] connectedNodes Disconnected Node attachments should be removed from here.
-   * @param[in] disconnectedNodes Disconnected Node attachments should be added here.
    */
-  void RecursiveDisconnectFromSceneGraph( BufferIndex updateBufferIndex, std::set<Node*>& connectedNodes, std::set<Node*>& disconnectedNodes );
+  void RecursiveDisconnectFromSceneGraph( BufferIndex updateBufferIndex );
 
 public: // Default properties
 
@@ -1066,7 +1043,6 @@ protected:
   bool mIsRoot:1;                                    ///< True if the node cannot have a parent
   bool mInheritOrientation:1;                        ///< Whether the parent's orientation should be inherited.
   bool mInheritScale:1;                              ///< Whether the parent's scale should be inherited.
-  bool mIsActive:1;                                  ///< When a Node is marked "active" it has been disconnected, and its properties have not been modified
 
   DrawMode::Type          mDrawMode:2;               ///< How the Node and its children should be drawn
   PositionInheritanceMode mPositionInheritanceMode:2;///< Determines how position is inherited, 2 bits is enough