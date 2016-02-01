tizenorg/platform.core.uifw.dali-core

NativeImageSource with tbm_surface for tizen 3.0 wayland
Change-Id: I5c8cadd96f87d4d1db9e30101cd647c697e3c7ea

dali/public-api/images/native-image.cpp
@@ -64,4 +64,14 @@ NativeImage NativeImage::DownCast( BaseHandle handle )
   return NativeImage( dynamic_cast<Internal::NativeImage*>( handle.GetObjectPtr()) );
 }
 
+const char* NativeImage::GetCustomVertexShaderCode()
+{
+  return GetImplementation(*this).GetCustomVertexShaderCode();
+}
+
+const char* NativeImage::GetCustomFragmentShaderCode()
+{
+  return GetImplementation(*this).GetCustomFragmentShaderCode();
+}
+
 } // namespace Dali

dalihub/dali-core

 - NativeImageSource uses XPixmap or Ecore Pixmap type in x windows system.
 - Tizen 3.0 wayland doesn't support the pixmap type.
 -> Replaces pixmap to tbm_surface.
 -> Adds two public-APIs to get sampler type and prefix for tbm_surface rendering
 -> Adds NativeImageInterface::Extension header as devel-api

dali/public-api/images/native-image.cpp
@@ -64,4 +64,14 @@ NativeImage NativeImage::DownCast( BaseHandle handle )
   return NativeImage( dynamic_cast<Internal::NativeImage*>( handle.GetObjectPtr()) );
 }
 
+const char* NativeImage::GetCustomSamplerTypename()
+{
+  return GetImplementation(*this).GetCustomSamplerTypename();
+}
+
+const char* NativeImage::GetCustomFragmentPreFix()
+{
+  return GetImplementation(*this).GetCustomFragmentPreFix();
+}
+
 } // namespace Dali