d0c27d2c3b6f19191a1d99ec37ab32ec6160bf61
Kimmo Hoikka <kimmo.hoikka@samsung.com>
2016-05-13 14:25:36 +0100

    Removed incorrectly included vector-wrapper from property
    Change-Id: Ieefc93ca94ed08403e4133569bb497296768beb3

--- dali/public-api/events/touch-point.h
+++ dali/public-api/events/touch-point.h
@@ -2,7 +2,7 @@
 #define __DALI_TOUCH_POINT_H__
 
 /*
- * Copyright (c) 2015 Samsung Electronics Co., Ltd.
+ * Copyright (c) 2016 Samsung Electronics Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,6 +19,7 @@
  */
 
 // INTERNAL INCLUDES
+#include <dali/public-api/common/vector-wrapper.h>
 #include <dali/public-api/actors/actor.h>
 #include <dali/public-api/math/vector2.h>
 
