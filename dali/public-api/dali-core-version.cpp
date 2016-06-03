91450243dcb1484c1f21cb44027daa306e3fff09
Tom Robinson <tom.robinson@samsung.com>
2016-06-03 14:33:18 +0100

    DALi Version 1.1.37
    Change-Id: I7513dc130ff0974fb67aff77ffbcf4cd30b424a7

--- dali/public-api/dali-core-version.cpp
+++ dali/public-api/dali-core-version.cpp
@@ -28,7 +28,7 @@ namespace Dali
 
 const unsigned int CORE_MAJOR_VERSION = 1;
 const unsigned int CORE_MINOR_VERSION = 1;
-const unsigned int CORE_MICRO_VERSION = 36;
+const unsigned int CORE_MICRO_VERSION = 37;
 const char * const CORE_BUILD_DATE    = __DATE__ " " __TIME__;
 
 #ifndef EMSCRIPTEN
