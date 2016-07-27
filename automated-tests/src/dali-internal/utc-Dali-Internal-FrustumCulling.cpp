tizenorg/platform.core.uifw.dali-core

Wrapped ShaderHints enum inside a struct
Change-Id: Ib1e23fcc942a7df8a83b86599c17c517b0c1df91

automated-tests/src/dali-internal/utc-Dali-Internal-FrustumCulling.cpp
@@ -56,7 +56,7 @@ void main()
 }
 );
 
-Actor CreateMeshActorToStage( TestApplication& application, Vector3 parentOrigin = ParentOrigin::CENTER, Vector3 anchorPoint = AnchorPoint::CENTER, Shader::ShaderHints shaderHints = Shader::HINT_NONE )
+Actor CreateMeshActorToStage( TestApplication& application, Vector3 parentOrigin = ParentOrigin::CENTER, Vector3 anchorPoint = AnchorPoint::CENTER, Shader::Hints::Value shaderHints = Shader::Hints::NONE )
 {
   PixelBuffer* pixelBuffer = new PixelBuffer[ 4 ];
   BufferImage image = BufferImage::New( pixelBuffer, 1, 1 );
@@ -420,7 +420,7 @@ int UtcFrustumCullDisabledP(void)
   TraceCallStack& drawTrace = glAbstraction.GetDrawTrace();
   drawTrace.Enable( true );
 
-  CreateMeshActorToStage( application, Vector3( 7.0f, 0.5f, 0.5f ), AnchorPoint::CENTER, Shader::HINT_MODIFIES_GEOMETRY );
+  CreateMeshActorToStage( application, Vector3( 7.0f, 0.5f, 0.5f ), AnchorPoint::CENTER, Shader::Hints::MODIFIES_GEOMETRY );
 
   drawTrace.Reset();
   application.SendNotification();

automated-tests/src/dali/utc-Dali-Renderer.cpp
@@ -737,7 +737,7 @@ int UtcDaliRendererSetBlendMode06(void)
   tet_infoline("Test setting the blend mode to auto with an opaque color and an image without an alpha channel and a shader with the hint OUTPUT_IS_TRANSPARENT renders with blending enabled");
 
   Geometry geometry = CreateQuadGeometry();
-  Shader shader = Shader::New( "vertexSrc", "fragmentSrc", Shader::HINT_OUTPUT_IS_TRANSPARENT );
+  Shader shader = Shader::New( "vertexSrc", "fragmentSrc", Shader::Hints::OUTPUT_IS_TRANSPARENT );
 
   Renderer renderer = Renderer::New( geometry, shader );
 

dali/internal/common/shader-data.h
@@ -48,7 +48,7 @@ public:
    * @param[in] vertexSource   Source code for vertex program
    * @param[in] fragmentSource Source code for fragment program
    */
-  ShaderData(const std::string& vertexSource, const std::string& fragmentSource, const Dali::Shader::ShaderHints hints)
+  ShaderData(const std::string& vertexSource, const std::string& fragmentSource, const Dali::Shader::Hints::Value hints)
   : mShaderHash( -1 ),
     mVertexShader(vertexSource),
     mFragmentShader(fragmentSource),
@@ -106,7 +106,7 @@ public: // API
   /**
    * @return the hints
    */
-  Dali::Shader::ShaderHints GetHints() const
+  Dali::Shader::Hints::Value GetHints() const
   {
     return mHints;
   }
@@ -166,7 +166,7 @@ private: // Data
   size_t                      mShaderHash;     ///< hash key created with vertex and fragment shader code
   std::string                 mVertexShader;   ///< source code for vertex program
   std::string                 mFragmentShader; ///< source code for fragment program
-  Dali::Shader::ShaderHints   mHints;    ///< take a hint
+  Dali::Shader::Hints::Value  mHints;          ///< take a hint
   Dali::Vector<unsigned char> mBuffer;         ///< buffer containing compiled binary bytecode
 };
 

dali/internal/event/effects/shader-effect-impl.cpp
@@ -137,20 +137,20 @@ std::string GetStringProperty(const std::string& field, const Property::Value& p
   return retval;
 }
 
-Dali::Shader::ShaderHints ConvertHints( Dali::ShaderEffect::GeometryHints hints)
+Dali::Shader::Hints::Value ConvertHints( Dali::ShaderEffect::GeometryHints hints)
 {
-  int convertedHints = Dali::Shader::HINT_NONE;
+  int convertedHints = Dali::Shader::Hints::NONE;
 
   if( hints & Dali::ShaderEffect::HINT_BLENDING )
   {
-    convertedHints |= Dali::Shader::HINT_OUTPUT_IS_TRANSPARENT;
+    convertedHints |= Dali::Shader::Hints::OUTPUT_IS_TRANSPARENT;
   }
   if( !(hints & Dali::ShaderEffect::HINT_DOESNT_MODIFY_GEOMETRY) )
   {
-    convertedHints |= Dali::Shader::HINT_MODIFIES_GEOMETRY;
+    convertedHints |= Dali::Shader::Hints::MODIFIES_GEOMETRY;
   }
 
-  return Dali::Shader::ShaderHints( convertedHints );
+  return Dali::Shader::Hints::Value( convertedHints );
 }
 
 } // unnamed namespace

dali/internal/event/rendering/shader-impl.h
@@ -52,7 +52,7 @@ public:
    */
   static ShaderPtr New( const std::string& vertexShader,
                         const std::string& fragmentShader,
-                        Dali::Shader::ShaderHints hints );
+                        Dali::Shader::Hints::Value hints );
 
   /**
    * @brief Get the shader scene object
@@ -155,7 +155,7 @@ private: // implementation
   /**
    * Second stage initialization
    */
-  void Initialize( const std::string& vertexShader, const std::string& fragmentShader, Dali::Shader::ShaderHints hints );
+  void Initialize( const std::string& vertexShader, const std::string& fragmentShader, Dali::Shader::Hints::Value hints );
 
 protected:
   /**

dali/internal/update/manager/prepare-render-instructions.cpp
@@ -70,7 +70,7 @@ inline void AddRendererToRenderList( BufferIndex updateBufferIndex,
                                      bool cull )
 {
   bool inside( true );
-  if ( cull && !renderable.mRenderer->GetShader().HintEnabled( Dali::Shader::HINT_MODIFIES_GEOMETRY ) )
+  if ( cull && !renderable.mRenderer->GetShader().HintEnabled( Dali::Shader::Hints::MODIFIES_GEOMETRY ) )
   {
     const Vector4& boundingSphere = renderable.mNode->GetBoundingSphere();
     inside = (boundingSphere.w > Math::MACHINE_EPSILON_1000) &&

dali/internal/update/rendering/scene-graph-renderer.cpp
@@ -635,7 +635,7 @@ Renderer::Opacity Renderer::GetOpacity( BufferIndex updateBufferIndex, const Nod
     }
     case BlendMode::AUTO:
     {
-      bool shaderRequiresBlending( mShader->HintEnabled( Dali::Shader::HINT_OUTPUT_IS_TRANSPARENT ) );
+      bool shaderRequiresBlending( mShader->HintEnabled( Dali::Shader::Hints::OUTPUT_IS_TRANSPARENT ) );
       if( shaderRequiresBlending || ( mTextureSet && mTextureSet->HasAlpha() ) )
       {
         opacity = Renderer::TRANSLUCENT;

dali/public-api/rendering/shader.cpp
@@ -26,7 +26,7 @@ namespace Dali
 
 Shader Shader::New( const std::string& vertexShader,
                     const std::string& fragmentShader,
-                    ShaderHints hints )
+                    Hints::Value hints )
 {
   Internal::ShaderPtr renderer = Internal::Shader::New( vertexShader, fragmentShader, hints );
   return Shader( renderer.Get() );

dalihub/dali-core

automated-tests/src/dali-internal/utc-Dali-Internal-FrustumCulling.cpp
@@ -56,7 +56,7 @@ void main()
 }
 );
 
-Actor CreateMeshActorToStage( TestApplication& application, Vector3 parentOrigin = ParentOrigin::CENTER, Vector3 anchorPoint = AnchorPoint::CENTER, Shader::ShaderHints shaderHints = Shader::HINT_NONE )
+Actor CreateMeshActorToStage( TestApplication& application, Vector3 parentOrigin = ParentOrigin::CENTER, Vector3 anchorPoint = AnchorPoint::CENTER, Shader::Hint::Value shaderHints = Shader::Hint::NONE )
 {
   PixelBuffer* pixelBuffer = new PixelBuffer[ 4 ];
   BufferImage image = BufferImage::New( pixelBuffer, 1, 1 );
@@ -420,7 +420,7 @@ int UtcFrustumCullDisabledP(void)
   TraceCallStack& drawTrace = glAbstraction.GetDrawTrace();
   drawTrace.Enable( true );
 
-  CreateMeshActorToStage( application, Vector3( 7.0f, 0.5f, 0.5f ), AnchorPoint::CENTER, Shader::HINT_MODIFIES_GEOMETRY );
+  CreateMeshActorToStage( application, Vector3( 7.0f, 0.5f, 0.5f ), AnchorPoint::CENTER, Shader::Hint::MODIFIES_GEOMETRY );
 
   drawTrace.Reset();
   application.SendNotification();

automated-tests/src/dali/utc-Dali-Renderer.cpp
@@ -737,7 +737,7 @@ int UtcDaliRendererSetBlendMode06(void)
   tet_infoline("Test setting the blend mode to auto with an opaque color and an image without an alpha channel and a shader with the hint OUTPUT_IS_TRANSPARENT renders with blending enabled");
 
   Geometry geometry = CreateQuadGeometry();
-  Shader shader = Shader::New( "vertexSrc", "fragmentSrc", Shader::HINT_OUTPUT_IS_TRANSPARENT );
+  Shader shader = Shader::New( "vertexSrc", "fragmentSrc", Shader::Hint::OUTPUT_IS_TRANSPARENT );
 
   Renderer renderer = Renderer::New( geometry, shader );
 

dali/internal/common/shader-data.h
@@ -48,7 +48,7 @@ public:
    * @param[in] vertexSource   Source code for vertex program
    * @param[in] fragmentSource Source code for fragment program
    */
-  ShaderData(const std::string& vertexSource, const std::string& fragmentSource, const Dali::Shader::ShaderHints hints)
+  ShaderData(const std::string& vertexSource, const std::string& fragmentSource, const Dali::Shader::Hint::Value hints)
   : mShaderHash( -1 ),
     mVertexShader(vertexSource),
     mFragmentShader(fragmentSource),
@@ -106,7 +106,7 @@ public: // API
   /**
    * @return the hints
    */
-  Dali::Shader::ShaderHints GetHints() const
+  Dali::Shader::Hint::Value GetHints() const
   {
     return mHints;
   }
@@ -166,7 +166,7 @@ private: // Data
   size_t                      mShaderHash;     ///< hash key created with vertex and fragment shader code
   std::string                 mVertexShader;   ///< source code for vertex program
   std::string                 mFragmentShader; ///< source code for fragment program
-  Dali::Shader::ShaderHints   mHints;    ///< take a hint
+  Dali::Shader::Hint::Value  mHints;          ///< take a hint
   Dali::Vector<unsigned char> mBuffer;         ///< buffer containing compiled binary bytecode
 };
 

dali/internal/event/effects/shader-effect-impl.cpp
@@ -137,20 +137,20 @@ std::string GetStringProperty(const std::string& field, const Property::Value& p
   return retval;
 }
 
-Dali::Shader::ShaderHints ConvertHints( Dali::ShaderEffect::GeometryHints hints)
+Dali::Shader::Hint::Value ConvertHints( Dali::ShaderEffect::GeometryHints hints)
 {
-  int convertedHints = Dali::Shader::HINT_NONE;
+  int convertedHints = Dali::Shader::Hint::NONE;
 
   if( hints & Dali::ShaderEffect::HINT_BLENDING )
   {
-    convertedHints |= Dali::Shader::HINT_OUTPUT_IS_TRANSPARENT;
+    convertedHints |= Dali::Shader::Hint::OUTPUT_IS_TRANSPARENT;
   }
   if( !(hints & Dali::ShaderEffect::HINT_DOESNT_MODIFY_GEOMETRY) )
   {
-    convertedHints |= Dali::Shader::HINT_MODIFIES_GEOMETRY;
+    convertedHints |= Dali::Shader::Hint::MODIFIES_GEOMETRY;
   }
 
-  return Dali::Shader::ShaderHints( convertedHints );
+  return Dali::Shader::Hint::Value( convertedHints );
 }
 
 } // unnamed namespace

dali/internal/event/rendering/shader-impl.h
@@ -52,7 +52,7 @@ public:
    */
   static ShaderPtr New( const std::string& vertexShader,
                         const std::string& fragmentShader,
-                        Dali::Shader::ShaderHints hints );
+                        Dali::Shader::Hint::Value hints );
 
   /**
    * @brief Get the shader scene object
@@ -155,7 +155,7 @@ private: // implementation
   /**
    * Second stage initialization
    */
-  void Initialize( const std::string& vertexShader, const std::string& fragmentShader, Dali::Shader::ShaderHints hints );
+  void Initialize( const std::string& vertexShader, const std::string& fragmentShader, Dali::Shader::Hint::Value hints );
 
 protected:
   /**

dali/internal/update/manager/prepare-render-instructions.cpp
@@ -70,7 +70,7 @@ inline void AddRendererToRenderList( BufferIndex updateBufferIndex,
                                      bool cull )
 {
   bool inside( true );
-  if ( cull && !renderable.mRenderer->GetShader().HintEnabled( Dali::Shader::HINT_MODIFIES_GEOMETRY ) )
+  if ( cull && !renderable.mRenderer->GetShader().HintEnabled( Dali::Shader::Hint::MODIFIES_GEOMETRY ) )
   {
     const Vector4& boundingSphere = renderable.mNode->GetBoundingSphere();
     inside = (boundingSphere.w > Math::MACHINE_EPSILON_1000) &&

dali/internal/update/rendering/scene-graph-renderer.cpp
@@ -635,7 +635,7 @@ Renderer::Opacity Renderer::GetOpacity( BufferIndex updateBufferIndex, const Nod
     }
     case BlendMode::AUTO:
     {
-      bool shaderRequiresBlending( mShader->HintEnabled( Dali::Shader::HINT_OUTPUT_IS_TRANSPARENT ) );
+      bool shaderRequiresBlending( mShader->HintEnabled( Dali::Shader::Hint::OUTPUT_IS_TRANSPARENT ) );
       if( shaderRequiresBlending || ( mTextureSet && mTextureSet->HasAlpha() ) )
       {
         opacity = Renderer::TRANSLUCENT;

dali/public-api/rendering/shader.cpp
@@ -26,7 +26,7 @@ namespace Dali
 
 Shader Shader::New( const std::string& vertexShader,
                     const std::string& fragmentShader,
-                    ShaderHints hints )
+                    Hint::Value hints )
 {
   Internal::ShaderPtr renderer = Internal::Shader::New( vertexShader, fragmentShader, hints );
   return Shader( renderer.Get() );