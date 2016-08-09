c444a25e384fc570529b903478b4cb7404656988
taeyoon <taeyoon0.lee@samsung.com>
2016-08-09 17:02:53 +0900

    Revert "[3.0] Restore the code at random.h"
    This reverts commit 2a1d4cdb2dec66c1d66c75f9ac4ace6214c763f9.
    
    Change-Id: Ia88cfffd0b3662b283be8afcb69b037b1a80689c

--- dali/public-api/math/random.h
+++ dali/public-api/math/random.h
@@ -35,6 +35,11 @@ namespace Dali
 namespace Random
 {
 
+namespace
+{
+  __thread unsigned int seed;
+}
+
 /**
  * @brief Returns a random number between f0 and f1.
  *
@@ -46,9 +51,10 @@ namespace Random
  */
 inline float Range(float f0, float f1)
 {
+  seed = time(NULL);
   float min = std::min(f0, f1);
   float max = std::max(f0, f1);
-  return min + (rand() & 0xfff) * (max-min) * (1.0f/4095.0f);
+  return min + (rand_r(&seed) & 0xfff) * (max-min) * (1.0f/4095.0f);
 }
 
 /**
