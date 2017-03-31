d4a6276654c28c6acdc51d0d238feabed1da91d7
Richard Huang <r.huang@samsung.com>
2017-03-31 10:57:21 +0100

    DALi Version 1.2.33
    Change-Id: I5629a48c2021e06435d05c5dd8ac4a64c27b71bf

--- dali/public-api/dali-core-version.cpp
+++ dali/public-api/dali-core-version.cpp
@@ -28,7 +28,7 @@ namespace Dali
 
 const unsigned int CORE_MAJOR_VERSION = 1;
 const unsigned int CORE_MINOR_VERSION = 2;
-const unsigned int CORE_MICRO_VERSION = 32;
+const unsigned int CORE_MICRO_VERSION = 33;
 const char * const CORE_BUILD_DATE    = __DATE__ " " __TIME__;
 
 #ifndef EMSCRIPTEN
