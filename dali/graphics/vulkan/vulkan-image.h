271c4878085afe8f9be06b746468e4ef8ce8b472
adam.b <jsr184@gmail.com>
2017-06-15 18:01:17 +0100

    Simplified Vulkan backend [WIP]
    Designed for better integration with Dali adaptor.
    
    - Image, Buffer and DeviceMemory brought back.
    - Added ImageView
    - Added Resource user counting
    
    Change-Id: I7d35e8584f50d844ba76c25f37004daf0ef91304

---
+++ dali/graphics/vulkan/vulkan-image.h
@@ -0,0 +1,134 @@
+#ifndef DALI_GRAPHICS_VULKAN_IMAGE_H
+#define DALI_GRAPHICS_VULKAN_IMAGE_H
+
+/*
+ * Copyright (c) 2017 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include <dali/graphics/vulkan/vulkan-types.h>
+
+namespace Dali
+{
+namespace Graphics
+{
+namespace Vulkan
+{
+
+class ImageView;
+enum class ResourceOwnershipType
+{
+  OWNED,
+  EXTERNAL
+};
+
+class Image : public Resource
+{
+public:
+  /**
+   * Creates new VkImage with given specification, it doesn't
+   * bind the memory.
+   * @param graphics
+   * @param createInfo
+   */
+  Image(Graphics& graphics, const vk::ImageCreateInfo& createInfo);
+
+  /**
+   * Constructor creates wrapper on the VkImage coming from external
+   * source. It doesn't take over ownership so it's still owner's
+   * responsibility to destroy it and maintain the usage.
+   *
+   * @param graphics
+   * @param externalImage
+   */
+  Image(Graphics& graphics, vk::Image externalImage);
+
+  /**
+   * Destructor
+   */
+  virtual ~Image() = default;
+
+  /**
+   * Creates UNMANAGED VkImageView from the current image.
+   * Usage requires external lifecycle management and synchronization
+   * Memory MUST be bound for this function to work!
+   * @param info
+   * @return
+   */
+  vk::ImageView CreateUnmanagedView(const vk::ImageViewCreateInfo& info);
+
+  /**
+   * Creates MANAGED ImageView from the current image
+   * Memory MUST be bound for this function to work!
+   * @param info
+   * @return
+   */
+  UniqueImageView CreateView(const vk::ImageViewCreateInfo& info);
+
+  /**
+   * Returns underlying Vulkan object
+   * @return
+   */
+  vk::Image GetImage() const
+  {
+    return mImage;
+  }
+
+private:
+
+  Graphics& mGraphics;
+
+  vk::Image       mImage;
+  vk::ImageLayout mLayout;
+
+  ResourceOwnershipType mOwnershipType;
+};
+
+/*
+ * ImageView
+ * todo: move it to its own file
+ */
+class ImageView : public Resource
+{
+public:
+  ImageView(Graphics& graphics, Image& image);
+  ImageView(Graphics& graphics, Image& image, const VkImageViewCreateInfo& createInfo);
+
+  virtual ~ImageView() override;
+
+  const vk::ImageView& GetImageView() const
+  {
+    return mImageView;
+  }
+
+  Image& GetImage() const
+  {
+    return mImageRef.GetResource();
+  }
+
+private:
+  Graphics&             mGraphics;
+  ResourceRef<Image>    mImageRef;
+
+  vk::ImageView mImageView;
+};
+
+} // namespace Vulkan
+
+} // namespace Graphics
+
+} // namespace Dali
+
+#endif // DALI_GRAPHICS_VULKAN_IMAGE_H
---
+++ dali/graphics/vulkan/vulkan-queue.h
@@ -0,0 +1,95 @@
+#ifndef DALI_GRAPHICS_VULKAN_QUEUE_H
+#define DALI_GRAPHICS_VULKAN_QUEUE_H
+
+/*
+ * Copyright (c) 2017 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+// INTERNAL INCLUDES
+#include <dali/graphics/vulkan/vulkan-types.h>
+
+namespace Dali
+{
+namespace Graphics
+{
+namespace Vulkan
+{
+
+class Fence;
+class Submission
+{
+public:
+  Submission(Fence& fence);
+
+  bool WaitForFence(uint32_t timeout = 0u);
+
+private:
+  // todo: possibly resources locks stored here?
+  FenceRef mFences;
+};
+
+class Queue
+{
+public:
+  Queue() = delete;
+  Queue(Graphics& graphics, vk::Queue queue, uint32_t queueFamilyIndex, uint32_t queueIndex,
+        vk::QueueFlags queueFlags);
+  ~Queue(); // queues are non-destructible
+
+  /** Submits command buffers */
+  std::unique_ptr< Submission > Submit(const std::vector< CommandBufferRef >& commandBuffers,
+                                       Fence&                                 fence);
+
+  /** Helper function to submit single command buffer */
+  std::unique_ptr< Submission > Submit(CommandBuffer& commandBuffer, Fence& fence);
+
+  void WaitIdle() const;
+
+  /**
+   *
+   * @param presentInfo
+   * @return
+   */
+  vk::Result Present( const vk::PresentInfoKHR& presentInfo );
+
+  /**
+   *
+   * @param swapchain
+   * @return
+   */
+  vk::Result Present( vk::SwapchainKHR swapchain, uint32_t imageIndex );
+
+private:
+  /** Prepares command buffers for submission */
+  std::vector< vk::CommandBuffer > PrepareBuffers(
+      const std::vector< CommandBufferRef >& commandBuffers) const;
+
+
+
+private:
+  Graphics&                mGraphics;
+  vk::Queue                mQueue;
+  vk::QueueFlags           mFlags;
+  std::vector< vk::Fence > mFences;
+  uint32_t                 mQueueFamilyIndex;
+  uint32_t                 mQueueIndex;
+};
+
+} // namespace Vulkan
+} // namespace Graphics
+} // namespace Dali
+
+#endif // DALI_GRAPHICS_VULKAN_QUEUE_H
