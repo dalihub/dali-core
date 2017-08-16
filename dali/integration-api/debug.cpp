9b08c188557231dc3f3dc7bf5fb9b89241cf002f
Kimmo Hoikka <kimmo.hoikka@samsung.com>
2017-08-16 11:55:33 +0100

    Replace GCC specific __thread with C++ 11 keyword thread_local
    Change-Id: I8bfd5684a7c198a332c81362eb643851bf2d5e77

--- dali/integration-api/debug.cpp
+++ dali/integration-api/debug.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014 Samsung Electronics Co., Ltd.
+ * Copyright (c) 2017 Samsung Electronics Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -94,7 +94,7 @@ namespace Integration
 namespace Log
 {
 
-__thread LogFunction gthreadLocalLogFunction = NULL;
+thread_local LogFunction gthreadLocalLogFunction = NULL;
 
 /* Forward declarations */
 std::string FormatToString(const char *format, ...);
