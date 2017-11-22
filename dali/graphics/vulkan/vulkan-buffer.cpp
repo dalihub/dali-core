cb71c66d2efb500afd204aa04135c2347a840619
adam.b <jsr184@gmail.com>
2017-11-22 10:37:38 +0000

    vulkan backend: buffers ( WIP )

--- dali/graphics/vulkan/vulkan-buffer.cpp
+++ dali/graphics/vulkan/vulkan-buffer.cpp
@@ -1,5 +1,159 @@
-//
-// Created by adam.b on 06/06/17.
-//
+/*
+ * Copyright (c) 2017 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 
-#include "vulkan-buffer.h"
+#include <dali/graphics/vulkan/vulkan-buffer.h>
+#include <dali/graphics/vulkan/vulkan-graphics.h>
+namespace Dali
+{
+namespace Graphics
+{
+namespace Vulkan
+{
+namespace Impl
+{
+
+struct Buffer final
+{
+  Graphics&                             mGraphics;
+  vk::BufferCreateInfo                  mInfo;
+  vk::Buffer                            mBuffer;
+  bool                                  mHasDeviceMemory;
+
+  Buffer(Graphics& graphics, const vk::BufferCreateInfo& createInfo)
+  : mGraphics(graphics),
+    mHasDeviceMemory(false)
+  {
+    mBuffer = VkAssert(graphics.GetDevice().createBuffer( createInfo, graphics.GetAllocator()));
+    mInfo = createInfo;
+  }
+
+  ~Buffer()
+  {
+    mGraphics.GetDevice().destroyBuffer( mBuffer, mGraphics.GetAllocator() );
+  }
+
+  vk::BufferUsageFlags GetUsage() const
+  {
+    return mInfo.usage;
+  }
+
+  DeviceMemory& GetDeviceMemory() const
+  {
+
+  }
+
+  size_t GetSize() const
+  {
+    return static_cast<size_t>(mInfo.size);
+  }
+
+  bool HasDeviceMemory() const
+  {
+    return mHasDeviceMemory;
+  }
+
+  vk::Buffer GetVkObject() const
+  {
+    return mBuffer;
+  }
+
+};
+}
+
+/**
+ *
+ * @param graphics
+ * @param size
+ * @return
+ */
+std::unique_ptr<VertexBuffer> Buffer::NewVertexBuffer(Graphics& graphics, size_t size)
+{
+  vk::BufferCreateInfo info = {};
+  info.setSharingMode( vk::SharingMode::eExclusive );
+  info.setSize( size );
+  info.setUsage( vk::BufferUsageFlagBits::eVertexBuffer | vk::BufferUsageFlagBits::eTransferDst );
+  return MakeUnique<VertexBuffer>( graphics, info );
+}
+
+/**
+ *
+ * @param graphics
+ * @param size
+ * @return
+ */
+std::unique_ptr<UniformBuffer> Buffer::NewUniformBuffer(Graphics& graphics, size_t size)
+{
+  vk::BufferCreateInfo info = {};
+  info.setSharingMode( vk::SharingMode::eExclusive );
+  info.setSize( size );
+  info.setUsage( vk::BufferUsageFlagBits::eUniformBuffer | vk::BufferUsageFlagBits::eTransferDst );
+  return MakeUnique<VertexBuffer>( graphics, info );
+}
+
+/**
+ *
+ * @param graphics
+ * @param size
+ * @return
+ */
+std::unique_ptr<IndexBuffer> Buffer::NewIndexBuffer(Graphics& graphics, size_t size)
+{
+  vk::BufferCreateInfo info = {};
+  info.setSharingMode( vk::SharingMode::eExclusive );
+  info.setSize( size );
+  info.setUsage( vk::BufferUsageFlagBits::eIndexBuffer | vk::BufferUsageFlagBits::eTransferDst );
+  return MakeUnique<VertexBuffer>( graphics, info );
+}
+
+Buffer::Buffer(Graphics& graphics, const vk::BufferCreateInfo& createInfo)
+{
+  mImpl = MakeUnique<Impl::Buffer>(graphics, createInfo);
+}
+
+Buffer::~Buffer() = default;
+
+vk::BufferUsageFlags Buffer::GetUsage() const
+{
+  return mImpl->GetUsage();
+}
+
+DeviceMemory& Buffer::GetDeviceMemory() const
+{
+  return mImpl->GetDeviceMemory();
+}
+
+size_t Buffer::GetSize() const
+{
+  return mImpl->GetSize();
+}
+
+bool Buffer::HasDeviceMemory() const
+{
+  return mImpl->HasDeviceMemory();
+}
+
+vk::Buffer Buffer::GetVkObject() const
+{
+  return mImpl->GetVkObject();
+}
+
+
+}
+
+}
+
+}
--- dali/graphics/vulkan/vulkan-buffer.h
+++ dali/graphics/vulkan/vulkan-buffer.h
@@ -1,9 +1,22 @@
-//
-// Created by adam.b on 06/06/17.
-//
+#ifndef DALI_GRAPHICS_VULKAN_BUFFER_H
+#define DALI_GRAPHICS_VULKAN_BUFFER_H
 
-#ifndef VULKAN_PROJECT_BUFFER_H
-#define VULKAN_PROJECT_BUFFER_H
+/*
+ * Copyright (c) 2017 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 
 #include <dali/graphics/vulkan/vulkan-types.h>
 
@@ -13,11 +26,59 @@ namespace Graphics
 {
 namespace Vulkan
 {
+namespace Impl
+{
+struct Buffer;
+}
+
+class DeviceMemory;
+class Graphics;
+
+using VertexBuffer = Buffer;
+using UniformBuffer = Buffer;
+using IndexBuffer = Buffer;
 
 class Buffer
 {
+public:
+  /**
+   * Creates new VkBuffer with given specification, it doesn't
+   * bind the memory.
+   * @param graphics
+   * @param createInfo
+   */
+  Buffer(Graphics& graphics, const vk::BufferCreateInfo& createInfo);
+
+  /**
+   * Destructor
+   */
+  virtual ~Buffer();
+
+
+  // single purpose constructors helpers
+  static std::unique_ptr<VertexBuffer> NewVertexBuffer( Graphics& graphics, size_t size  );
+
+  static std::unique_ptr<UniformBuffer> NewUniformBuffer( Graphics& graphics, size_t size );
+
+  static std::unique_ptr<IndexBuffer> NewIndexBuffer( Graphics& graphics, size_t size );
+
+  vk::BufferUsageFlags GetUsage() const;
+
+  DeviceMemory& GetDeviceMemory() const;
+
+  size_t GetSize() const;
+
+  bool HasDeviceMemory() const;
+
+  vk::Buffer GetVkObject() const;
+
+private:
+
+  std::unique_ptr<Impl::Buffer> mImpl;
+
 
 };
+
 
 }
 }
