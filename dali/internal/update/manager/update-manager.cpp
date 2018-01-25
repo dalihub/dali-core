9ceab4a7e10d56bf1a2bed36fd96260e9f5073fa
Francisco Santos <f1.santos@samsung.com>
2018-01-25 14:08:51 +0000

    Don't set GL shaders.
    Change-Id: I2d1b04bfc5bc2489c0fac6a04ed9b5ba7a3354df

--- dali/internal/update/manager/update-manager.cpp
+++ dali/internal/update/manager/update-manager.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017 Samsung Electronics Co., Ltd.
+ * Copyright (c) 2018 Samsung Electronics Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -515,6 +515,7 @@ void UpdateManager::RemoveShader( Shader* shader )
 void UpdateManager::SetShaderProgram( Shader* shader,
                                       Internal::ShaderDataPtr shaderData, bool modifiesGeometry )
 {
+  return;
   if( shaderData )
   {
 
