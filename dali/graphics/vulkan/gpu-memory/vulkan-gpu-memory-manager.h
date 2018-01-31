feb339914085977fd1cdb416aee68d57a4fc7ed9
Francisco Santos <f1.santos@samsung.com>
2018-01-31 13:33:26 +0000

    return by value
    Change-Id: Ifb43ae460e667b3b9ce445fc08fa892329e9e88e

--- dali/graphics/vulkan/gpu-memory/vulkan-gpu-memory-manager.h
+++ dali/graphics/vulkan/gpu-memory/vulkan-gpu-memory-manager.h
@@ -2,7 +2,7 @@
 #define DALI_VULKAN_161117_GPU_MEMORY_MANAGER_H
 
 /*
- * Copyright (c) 2017 Samsung Electronics Co., Ltd.
+ * Copyright (c) 2018 Samsung Electronics Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -82,7 +82,7 @@ public:
   class Impl;
   GpuMemoryManager( Impl& impl );
 
-  static GpuMemoryManager&& GetInterface( Impl& impl );
+  static GpuMemoryManager GetInterface( Impl& impl );
 
 private:
 
