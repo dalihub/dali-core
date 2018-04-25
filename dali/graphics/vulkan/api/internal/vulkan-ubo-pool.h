565d8af0463dcbdca36b5e4e21a213c23273fb5f
adam.b <jsr184@gmail.com>
2018-04-25 18:49:19 +0100

    [Vulkan] graphics controller, multiple pipelines ( WIP )
    - Added UBO manager and pools
    
    - Graphics controller creates multiple pipelines depends on the state
    - Render command has been extended to pass the list of textures
    - Removed sampler-id hack from the code and the default shader
    - Textures passed via Accessor
    - Added buffer factory
    - Using SPIRV reflection in order to map vertex input attributes
    
    Limitation:
    
    - Single texture passed per render item
    
    Change-Id: Ib613d6cbc1ec481c2bd1fc7d504c58080c2f8cfa

---
+++ dali/graphics/vulkan/api/internal/vulkan-ubo-pool.h
@@ -0,0 +1,102 @@
+#ifndef DALI_VULKAN_INTERNAL_VULKAN_UBO_POOL_H
+#define DALI_VULKAN_INTERNAL_VULKAN_UBO_POOL_H
+
+/*
+ * Copyright (c) 2018 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include <cstdint>
+#include <memory>
+
+namespace Dali
+{
+namespace Graphics
+{
+namespace Vulkan
+{
+class Controller;
+}
+namespace VulkanAPI
+{
+using Controller = Vulkan::Controller;
+class Ubo;
+class UboPool
+{
+public:
+
+  explicit UboPool(Controller &controller, uint32_t blockSize, uint32_t initialCapacity );
+
+  ~UboPool();
+
+  /**
+   * Allocates memory within pool space for new UBO
+   * @return unique pointer to the UBO object
+   */
+  std::unique_ptr<Ubo> Allocate();
+
+  /**
+   * Releases allocated memory back to the pool
+   * @param ubo
+   */
+  void Release( Ubo& ubo );
+
+  /**
+   * Maps UBO buffer memory into the user space
+   * @param ubo
+   * @return
+   */
+  void* Map( Ubo& ubo );
+
+  /**
+   * Unmaps UBO memory
+   */
+  void Unmap( Ubo& ubo );
+
+private:
+
+  struct Impl;
+  std::unique_ptr<Impl> mImpl;
+
+};
+
+/**
+ * Ubo is an opaque object
+ */
+struct Ubo
+{
+  explicit Ubo( UboPool& pool, uint32_t heapIndex, uint32_t allocationIndex, uint32_t allocationSize );
+  ~Ubo();
+
+  /**
+ * Writes into UBO object memory. Maps and unmaps buffer. Not very efficient.
+ * @param data
+ * @param offset
+ * @param size
+ * @return
+ */
+  uint32_t Write( const void* data, uint32_t offset, uint32_t size );
+
+  struct Impl;
+  std::unique_ptr<Impl> mImpl;
+  Impl& GetImplementation();
+};
+
+
+}
+}
+}
+
+#endif //DALI_VULKAN_INTERNAL_VULKAN_UBO_POOL_H
--- dali/graphics/vulkan/api/vulkan-api-shader.cpp
+++ dali/graphics/vulkan/api/vulkan-api-shader.cpp
@@ -111,18 +111,238 @@ bool Shader::AddShaderModule( Dali::Graphics::API::ShaderDetails::PipelineStage 
   if(pipelineStage == ShaderPipelineStage::VERTEX )
   {
     mVertexShader = shaderRef;
-    return true;
+    mVertexShader->SetExplicitShaderStage( vk::ShaderStageFlagBits::eVertex );
+    // update input attributes
+    mVertexShader->GetSPIRVReflection().GetVertexInputAttributes( mVertexInputAttributes );
   }
   else if(pipelineStage == ShaderPipelineStage::FRAGMENT )
   {
     mFragmentShader = shaderRef;
-    return true;
+    mVertexShader->SetExplicitShaderStage( vk::ShaderStageFlagBits::eFragment );
   }
 
-  // possibly unsupported pipeline stage
+  mUniformBlocks.clear();
+  mUniformOpaques.clear();
+
+  // recreate reflection
+  if( mVertexShader )
+  {
+    auto& ubo = mVertexShader->GetSPIRVReflection().GetUniformBlocks();
+    mUniformBlocks.insert(mUniformBlocks.end(), ubo.begin(), ubo.end() );
+
+    auto& uniforms = mVertexShader->GetSPIRVReflection().GetOpaqueUniforms();
+    mUniformOpaques.insert(mUniformOpaques.end(), uniforms.begin(), uniforms.end() );
+  }
+  if( mFragmentShader )
+  {
+    auto& ubo = mFragmentShader->GetSPIRVReflection().GetUniformBlocks();
+    mUniformBlocks.insert(mUniformBlocks.end(), ubo.begin(), ubo.end() );
+
+    auto& uniforms = mFragmentShader->GetSPIRVReflection().GetOpaqueUniforms();
+    mUniformOpaques.insert(mUniformOpaques.end(), uniforms.begin(), uniforms.end() );
+  }
+
+  // verify bindings and descriptor sets ( must be no overlaps, or if there are, only one binding will be used )
+  return true;
+}
+
+bool Shader::IsReflectionSupported() const
+{
+  return true;
+}
+
+uint32_t Shader::GetVertexAttributeLocation(const std::string& name) const
+{
+  if( !mVertexShader )
+  {
+    return API::ShaderDetails::ERROR_VERTEX_INPUT_ATTRIBUTE_NOT_FOUND;
+  }
+
+  for( auto&& attr : mVertexInputAttributes )
+  {
+    if( attr.name == name )
+    {
+      return attr.location;
+    }
+  }
+  return API::ShaderDetails::ERROR_VERTEX_INPUT_ATTRIBUTE_NOT_FOUND;
+}
+
+API::ShaderDetails::VertexInputAttributeFormat Shader::GetVertexAttributeFormat( uint32_t location ) const
+{
+  if( !mVertexShader || mVertexInputAttributes.size() <= location )
+  {
+    return API::ShaderDetails::VertexInputAttributeFormat::UNDEFINED;
+  }
+
+  const auto& attr = mVertexInputAttributes[location];
+
+  if( attr.format == vk::Format::eR32Sfloat )
+  {
+    return API::ShaderDetails::VertexInputAttributeFormat::FLOAT;
+  }
+  if( attr.format == vk::Format::eR32Sint )
+  {
+    return API::ShaderDetails::VertexInputAttributeFormat::INTEGER;
+  }
+  if( attr.format == vk::Format::eR32G32Sfloat )
+  {
+    return API::ShaderDetails::VertexInputAttributeFormat::VEC2;
+  }
+  if( attr.format == vk::Format::eR32G32B32Sfloat )
+  {
+    return API::ShaderDetails::VertexInputAttributeFormat::VEC3;
+  }
+  if( attr.format == vk::Format::eR32G32B32A32Sfloat )
+  {
+    return API::ShaderDetails::VertexInputAttributeFormat::VEC4;
+  }
+  return API::ShaderDetails::VertexInputAttributeFormat::UNDEFINED;
+}
+
+std::string Shader::GetVertexAttributeName(uint32_t location) const
+{
+  if( !mVertexShader || mVertexInputAttributes.size() <= location )
+  {
+    return "";
+  }
+
+  return mVertexInputAttributes[location].name;
+}
+
+std::vector<uint32_t> Shader::GetVertexAttributeLocations() const
+{
+  std::vector<uint32_t> locations;
+  for( auto&& attr : mVertexInputAttributes )
+  {
+    if( attr.format != vk::Format::eUndefined )
+    {
+      locations.push_back( attr.location );
+    }
+  }
+
+  return locations;
+}
+
+std::vector<uint32_t> Shader::GetUniformBlockLocations() const
+{
+  std::vector<uint32_t> retval{};
+  for( auto&& ubo : mUniformBlocks )
+  {
+    retval.emplace_back( ubo.binding );
+  }
+  return retval;
+}
+
+std::string Shader::GetUniformBlockName( uint32_t blockIndex ) const
+{
+  return mUniformBlocks[blockIndex].name;
+}
+
+uint32_t Shader::GetUniformBlockMemberCount( uint32_t blockIndex ) const
+{
+  return U32(mUniformBlocks[blockIndex].members.size());
+}
+
+std::string Shader::GetUniformBlockMemberName( uint32_t blockIndex, uint32_t memberLocation ) const
+{
+  return mUniformBlocks[blockIndex].members[memberLocation].name;
+}
+
+uint32_t Shader::GetUniformBlockMemberOffset( uint32_t blockIndex, uint32_t memberLocation ) const
+{
+  return mUniformBlocks[blockIndex].members[memberLocation].offset;
+}
+
+bool Shader::GetNamedUniform( const std::string& name, API::ShaderDetails::UniformInfo& out ) const
+{
+  // check uniform blocks first
+  auto index = 0u;
+  for( auto&& ubo : mUniformBlocks )
+  {
+    for( auto&& member : ubo.members )
+    {
+      if( name == member.name || name == (ubo.name + "." + member.name) )
+      {
+        out.name = name;
+        out.location = member.location;
+        out.binding = ubo.binding;
+        out.bufferIndex = index;
+        out.offset = member.offset;
+        out.uniformClass = ShaderDetails::UniformClass::UNIFORM_BUFFER;
+        return true;
+      }
+    }
+    index++;
+  }
+
+  // check samplers
+  for( auto&& uniform : mUniformOpaques )
+  {
+    if( uniform.name == name )
+    {
+      out.uniformClass = ShaderDetails::UniformClass::COMBINED_IMAGE_SAMPLER;
+      out.binding = uniform.binding;
+      out.name = name;
+      out.offset = 0;
+      out.location = 0;
+      return true;
+    }
+  }
   return false;
 }
 
+std::vector<API::ShaderDetails::UniformInfo> Shader::GetSamplers() const
+{
+  std::vector<API::ShaderDetails::UniformInfo> retval;
+  for( auto&& uniform : mUniformOpaques )
+  {
+    if(uniform.type == vk::DescriptorType::eCombinedImageSampler )
+    {
+      API::ShaderDetails::UniformInfo info;
+      info.location = 0u;
+      info.binding = uniform.binding;
+      info.offset = 0;
+      info.name = uniform.name;
+      info.uniformClass = API::ShaderDetails::UniformClass::COMBINED_IMAGE_SAMPLER;
+      retval.emplace_back( info );
+    }
+  }
+  return retval;
+}
+
+uint32_t Shader::GetUniformBlockCount() const
+{
+  return U32(mUniformBlocks.size());
+}
+
+bool Shader::GetUniformBlock( uint32_t index, API::ShaderDetails::UniformBlockInfo& out ) const
+{
+  if( index >= mUniformBlocks.size() )
+  {
+    return false;
+  }
+
+  const auto& block = mUniformBlocks[index];
+
+  out.name = block.name;
+  out.binding = block.binding;
+  out.descriptorSet = block.descriptorSet;
+  auto membersSize = block.members.size();
+  out.members.resize( membersSize );
+  out.size = block.size;
+  for( auto i = 0u; i < out.members.size(); ++i )
+  {
+    const auto& memberUniform = block.members[i];
+    out.members[i].name = memberUniform.name;
+    out.members[i].binding = block.binding;
+    out.members[i].uniformClass = API::ShaderDetails::UniformClass::UNIFORM_BUFFER;
+    out.members[i].offset = memberUniform.offset;
+    out.members[i].location = memberUniform.location;
+  }
+
+  return true;
+}
 
 
 } // namespace VulkanAPI
