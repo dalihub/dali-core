f7cb49853958b752776bc06153ab15628f386dfa
adam.b <jsr184@gmail.com>
2018-05-18 13:05:31 +0100

    [Vulkan] Added UBO manager
    Simple uniform buffer management
    
    Change-Id: I17b9edaf3834a9766e70d582f96e8076140713ee

--- dali/graphics/vulkan/api/internal/vulkan-ubo-pool.h
+++ dali/graphics/vulkan/api/internal/vulkan-ubo-pool.h
@@ -26,13 +26,9 @@ namespace Dali
 {
 namespace Graphics
 {
-namespace Vulkan
-{
-class Controller;
-}
 namespace VulkanAPI
 {
-using Controller = Vulkan::Controller;
+class Controller;
 class Ubo;
 class UboPool
 {
