dfcfce6c9d593bbac0ace9ed9350d02f4ad3c879
Angelos Gkountis <a.gkountis@samsung.com>
2018-05-31 10:56:10 +0100

    Refactored the Vulkan::Sampler class
    > Removed the PIMPL pattern
    > Sampler can now only be immutable
    > Samplers should be unique and be created at initialization
    > Sampler creation logic moved to the Vulkan::Graphics class
    > Added the method AddSampler to the Vulkan::Graphics class
    > Sampler lifetime is now managed by the Vulkan::Graphics class
    
    Change-Id: Ifa4a9364e3dc2cf483e694648e4b03f35b689398

--- dali/graphics/vulkan/vulkan-sampler.cpp
+++ dali/graphics/vulkan/vulkan-sampler.cpp
@@ -26,58 +26,6 @@ namespace Graphics
 namespace Vulkan
 {
 
-struct Sampler::Impl
-{
-  Impl( Sampler& owner, Graphics& graphics, vk::SamplerCreateInfo info, bool immutable )
-  : mOwner( owner ), mGraphics( graphics ), mCreateInfo( info ), mImmutable( immutable ), mModified( true )
-  {
-
-  }
-
-  ~Impl()
-  {
-    DestroySafe();
-  }
-
-  bool CreateSampler()
-  {
-    if( !mModified )
-    {
-      return true;
-    }
-
-    if( mVkSampler )
-    {
-      DestroySafe();
-    }
-    mVkSampler = VkAssert( mGraphics.GetDevice().createSampler( mCreateInfo, mGraphics.GetAllocator()));
-
-    // push to the resource list in graphics
-
-    mModified = false;
-
-    return true;
-  }
-
-  void DestroySafe()
-  {
-    // fixme: push sampler to the destroy queue, will be destroyed with the end of the frame
-    mGraphics.GetDevice().destroySampler( mVkSampler );
-  }
-
-  Impl( const Impl& ) = delete;
-  Impl& operator=( const Impl& ) = delete;
-
-  Sampler&              mOwner;
-  Graphics&             mGraphics;
-  vk::SamplerCreateInfo mCreateInfo;
-  vk::Sampler           mVkSampler;
-  bool                  mImmutable;
-  bool                  mModified;
-};
-
-
-
 /**
  * Sampler
  */
@@ -87,118 +35,48 @@ struct Sampler::Impl
  * @param graphics
  * @return
  */
-RefCountedSampler Sampler::New( Graphics& graphics )
+RefCountedSampler Sampler::New( Graphics& graphics, const vk::SamplerCreateInfo& createInfo )
 {
-  return RefCountedSampler(new Sampler(graphics, vk::SamplerCreateInfo{}, false));
+  return RefCountedSampler( new Sampler( graphics, createInfo ) );
 }
 
-/**
- * Creates new immutable sampler
- * @param graphics
- * @param createInfo
- * @return
- */
-RefCountedSampler Sampler::NewImmutable( Graphics& graphics, vk::SamplerCreateInfo createInfo )
+Sampler::Sampler( Graphics& graphics, const vk::SamplerCreateInfo& createInfo )
+        : mGraphics(&graphics),
+          mCreateInfo(createInfo)
 {
-  auto retval = RefCountedSampler(new Sampler(graphics, createInfo, true));
-  if( retval->mImpl->CreateSampler() )
-  {
-    return retval;
-  }
-  return RefCountedSampler();
-}
-
-Sampler::Sampler( Graphics& graphics, vk::SamplerCreateInfo createInfo, bool immutable )
-{
-  mImpl.reset(new Impl( *this, graphics, createInfo, immutable ));
 }
 
 Sampler::~Sampler() = default;
 
-/**
- * Sets minification and magnifcation filters
- * @param minFilter
- * @param magFilter
- */
-void Sampler::SetFilter( vk::Filter minFilter, vk::Filter magFilter )
-{
-  mImpl->mCreateInfo.setMinFilter( minFilter );
-  mImpl->mCreateInfo.setMagFilter( magFilter );
-  mImpl->mModified = true;
-}
-
-/**
- * Sets UVW address mode
- * @param addressModeU
- * @param addressModeV
- * @param addressModeW
- */
-void Sampler::SetAddressMode( vk::SamplerAddressMode addressModeU,
-                     vk::SamplerAddressMode addressModeV,
-                     vk::SamplerAddressMode addressModeW )
-{
-  mImpl->mCreateInfo.setAddressModeU( addressModeU );
-  mImpl->mCreateInfo.setAddressModeV( addressModeV );
-  mImpl->mCreateInfo.setAddressModeW( addressModeW );
-  mImpl->mModified = true;
-}
-
-void Sampler::SetLod( float minLod, float maxLod )
-{
-  mImpl->mCreateInfo.setMinLod( minLod ).setMaxLod( maxLod );
-  mImpl->mModified = true;
-}
-
-void Sampler::EnableAnisotropy( bool enabled )
-{
-  mImpl->mCreateInfo.setAnisotropyEnable( enabled );
-  mImpl->mModified = true;
-}
-
-void Sampler::SetMipmapMode( vk::SamplerMipmapMode mode )
-{
-  mImpl->mCreateInfo.setMipmapMode( mode );
-  mImpl->mModified = true;
-}
-
-void Sampler::SetMaxAnisotropy( float maxAnisotropy )
-{
-  mImpl->mCreateInfo.setMaxAnisotropy( maxAnisotropy );
-  mImpl->mModified = true;
-}
-
-void Sampler::EnableCompare( bool enabled )
-{
-  mImpl->mCreateInfo.setCompareEnable( enabled );
-  mImpl->mModified = true;
-}
-
-void Sampler::SetBorderColor( vk::BorderColor color )
-{
-  mImpl->mCreateInfo.setBorderColor( color );
-  mImpl->mModified = true;
-}
-
-void Sampler::UseUnnormalizedCoordinates( bool enabled )
-{
-  mImpl->mCreateInfo.setUnnormalizedCoordinates( enabled );
-  mImpl->mModified = true;
-}
-
-void Sampler::SetCompareOp( vk::CompareOp compareOp )
-{
-  mImpl->mCreateInfo.setCompareOp( compareOp );
-  mImpl->mModified = true;
-}
-
 vk::Sampler Sampler::GetVkHandle() const
 {
-  // if no sampler yet, create it now
-  if( mImpl->mModified )
-  {
-    mImpl->CreateSampler();
-  }
-  return mImpl->mVkSampler;
+  return mSampler;
+}
+
+const Sampler& Sampler::ConstRef()
+{
+  return *this;
+}
+
+Sampler& Sampler::Ref()
+{
+  return *this;
+}
+
+Sampler::operator vk::Sampler*()
+{
+  return &mSampler;
+}
+
+bool Sampler::OnDestroy()
+{
+  mGraphics->RemoveSampler( *this );
+
+  mGraphics->DiscardResource( [this]() {
+    mGraphics->GetDevice().destroySampler( mSampler, mGraphics->GetAllocator() );
+  } );
+
+  return false;
 }
 
 } // namespace Vulkan
