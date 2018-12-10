e522b45346361c717e3350ee069a534351a066aa
David Steele <david.steele@samsung.com>
2018-12-10 20:13:12 +0900

    Changed node / renderer aging of uniform maps
    Change-Id: Ie921fe077359b8887c6bc4bd33789ccc6459fbab
    Signed-off-by: David Steele <david.steele@samsung.com>

--- dali/internal/update/manager/update-manager.cpp
+++ dali/internal/update/manager/update-manager.cpp
@@ -735,6 +735,17 @@ void UpdateManager::UpdateRenderers( BufferIndex bufferIndex )
   }
 }
 
+void UpdateManager::PrepareNodes( BufferIndex updateBufferIndex )
+{
+  for( auto node : mImpl->nodes )
+  {
+    if(node)
+    {
+      node->PrepareRender( updateBufferIndex );
+    }
+  }
+}
+
 void UpdateManager::PrepareRenderers( BufferIndex bufferIndex )
 {
   // Prepare renderers. Each render item maps to a render
@@ -898,6 +909,7 @@ uint32_t UpdateManager::Update( float elapsedSeconds,
       }
 
       // generate graphics objects
+      PrepareNodes( bufferIndex );
       PrepareRenderers( bufferIndex );
       mImpl->graphicsAlgorithms.SubmitRenderInstructions( mImpl->graphics.GetController(), mImpl->renderInstructions, bufferIndex );
     }
