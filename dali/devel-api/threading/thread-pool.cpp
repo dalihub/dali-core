0792802244bb5f66588f5a07ef2aa2531f99c286
adam.b <jsr184@gmail.com>
2019-03-01 13:44:38 +0000

    Thread pool moved to DALi devel-api
    Change-Id: Ic8b6b59fe58b3678693cc84414b43d596c7debd5

---
+++ dali/devel-api/threading/thread-pool.cpp
@@ -0,0 +1,243 @@
+/*
+ * Copyright (c) 2018 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include "thread-pool.h"
+#include <cmath>
+
+namespace Dali
+{
+
+void WorkerThread::WaitAndExecute()
+{
+  while( true )
+  {
+    Task task;
+
+    {
+      std::unique_lock< std::mutex > lock{ mTaskQueueMutex };
+
+      mConditionVariable.wait( lock, [ this ]() -> bool {
+        return !mTaskQueue.empty() || mTerminating;
+      } );
+
+      if( mTerminating )
+      {
+        break;
+      }
+
+      task = mTaskQueue.front();
+    }
+
+    task( mIndex );
+
+    {
+      std::lock_guard< std::mutex > lock{ mTaskQueueMutex };
+
+      mTaskQueue.pop();
+
+      mConditionVariable.notify_one();
+    }
+  }
+}
+
+WorkerThread::WorkerThread(uint32_t index) : mIndex( index )
+{
+  // Have to pass "this" as an argument because WaitAndExecute is a member function.
+  mWorker = std::thread{ &WorkerThread::WaitAndExecute, this };
+}
+
+WorkerThread::~WorkerThread()
+{
+  if( mWorker.joinable() )
+  {
+    Notify();
+    Wait();
+
+    {
+      std::lock_guard< std::mutex > lock{ mTaskQueueMutex };
+      mTerminating = true;
+      mConditionVariable.notify_one();
+    }
+
+    mWorker.join();
+  }
+}
+
+void WorkerThread::AddTask( Task task )
+{
+  std::lock_guard< std::mutex > lock{ mTaskQueueMutex };
+  mTaskQueue.push( std::move( task ) );
+  mConditionVariable.notify_one();
+}
+
+void WorkerThread::AddTask( Task task, bool doNotify )
+{
+  std::lock_guard< std::mutex > lock{ mTaskQueueMutex };
+  mTaskQueue.push( std::move( task ) );
+  if( doNotify )
+  {
+    mConditionVariable.notify_one();
+  }
+}
+
+void WorkerThread::Notify()
+{
+  std::lock_guard< std::mutex > lock{ mTaskQueueMutex };
+  mConditionVariable.notify_one();
+}
+
+void WorkerThread::Wait()
+{
+  std::unique_lock< std::mutex > lock{ mTaskQueueMutex };
+  mConditionVariable.wait( lock, [ this ]() -> bool {
+    return mTaskQueue.empty();
+  } );
+}
+
+// ThreadPool -----------------------------------------------------------------------------------------------
+
+bool ThreadPool::Initialize( uint32_t threadCount )
+{
+//  LOG( "Initializing thread pool..." );
+
+  /**
+   * Get the system's supported thread count.
+   */
+  auto thread_count = threadCount + 1;
+  if( !threadCount )
+  {
+    thread_count = std::thread::hardware_concurrency();
+    if( !thread_count )
+    {
+      return false;
+    }
+  }
+
+  /**
+   * Spawn the worker threads.
+   */
+  for( auto i = 0u; i < thread_count - 1; i++ )
+  {
+    /**
+    * The workers will execute an infinite loop function
+    * and will wait for a job to enter the job queue. Once a job is in the the queue
+    * the threads will wake up to acquire and execute it.
+    */
+    mWorkers.push_back( std::unique_ptr< WorkerThread >( new WorkerThread( i ) ) );
+  }
+
+  return true;
+}
+
+
+void ThreadPool::Wait()
+{
+  for( auto& worker : mWorkers )
+  {
+    worker->Wait();
+  }
+}
+
+std::shared_ptr< Future< void > > ThreadPool::SubmitTask( uint32_t workerIndex, const Task& task )
+{
+  auto future = std::shared_ptr< Future< void > >( new Future< void > );
+  mWorkers[workerIndex]->AddTask( [task, future]( uint32_t index )
+                                  {
+                                    task( index );
+
+                                    future->mPromise.set_value();
+                                  });
+
+  return future;
+}
+
+std::shared_ptr< Future< void > > ThreadPool::SubmitTasks( const std::vector< Task >& tasks )
+{
+  auto future = std::shared_ptr< Future< void > >( new Future< void > );
+
+  mWorkers[ mWorkerIndex++ % static_cast< uint32_t >( mWorkers.size() )]->AddTask(
+    [ future, tasks ]( uint32_t index ) {
+      for( auto& task : tasks )
+      {
+        task( index );
+      }
+
+      future->mPromise.set_value();
+
+    } );
+
+  return future;
+}
+
+std::unique_ptr<FutureGroup<void>> ThreadPool::SubmitTasks( const std::vector< Task >& tasks, uint32_t threadMask )
+{
+  std::unique_ptr<FutureGroup<void>> retval = std::make_unique<FutureGroup<void>>();
+
+  /**
+   * Use square root of number of sumbitted tasks to estimate optimal number of threads
+   * used to execute jobs
+   */
+  auto threads = uint32_t(std::log2(float(tasks.size())));
+
+  if( threadMask != 0 )
+  {
+    threads = threadMask;
+  }
+
+  if( threads > mWorkers.size() )
+  {
+    threads = uint32_t(mWorkers.size());
+  }
+  else if( !threads )
+  {
+    threads = 1;
+  }
+
+  auto payloadPerThread = uint32_t(tasks.size() / threads);
+  auto remaining = uint32_t(tasks.size() % threads);
+
+  uint32_t taskIndex = 0;
+  uint32_t taskSize = uint32_t(remaining + payloadPerThread); // add 'remaining' tasks to the very first job list
+
+  for( auto wt = 0u; wt < threads; ++wt )
+  {
+    auto future = std::shared_ptr< Future< void > >( new Future< void > );
+    retval->mFutures.emplace_back( future );
+    mWorkers[ mWorkerIndex++ % static_cast< uint32_t >( mWorkers.size() )]->AddTask(
+      [ future, tasks, taskIndex, taskSize ]( uint32_t index ) {
+        auto begin = tasks.begin() + int(taskIndex);
+        auto end = begin + int(taskSize);
+        for( auto it = begin; it < end; ++it )
+        {
+          (*it)( index );
+        }
+        future->mPromise.set_value();
+      } );
+
+    taskIndex += taskSize;
+    taskSize = payloadPerThread;
+  }
+
+  return retval;
+}
+
+size_t ThreadPool::GetWorkerCount() const
+{
+  return mWorkers.size();
+}
+
+} //namespace Dali
---
+++ dali/devel-api/threading/thread-pool.h
@@ -0,0 +1,316 @@
+#ifndef DALI_THREAD_POOL_H
+#define DALI_THREAD_POOL_H
+
+/*
+ * Copyright (c) 2019 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include <thread>
+#include <functional>
+#include <mutex>
+#include <queue>
+#include <condition_variable>
+#include <future>
+#include <algorithm>
+#include <iostream>
+
+namespace Dali
+{
+using Task = std::function< void(uint32_t) >;
+
+using TaskQueue = std::queue< Task >;
+
+template< typename T >
+class Future final
+{
+  friend class ThreadPool;
+
+private:
+  std::promise< T > mPromise{};
+  std::future< T > mFuture{};
+
+public:
+
+  Future()
+  {
+    mFuture = mPromise.get_future();
+  }
+
+  ~Future()
+  {
+    Wait();
+  }
+
+  T Get() const
+  {
+    return mFuture.get();
+  }
+
+  void Wait() const
+  {
+    if( IsValid() )
+    {
+      mFuture.wait();
+    }
+  }
+
+  bool IsValid() const
+  {
+    return mFuture.valid();
+  }
+
+  void Reset()
+  {
+    mPromise = std::promise< T >();
+    mFuture = mPromise.get_future();
+  }
+};
+
+using SharedFuture = std::shared_ptr< Future< void > >;
+
+template< typename T >
+class FutureGroup final
+{
+  friend class ThreadPool;
+
+private:
+  std::vector< std::shared_ptr< Future< T > > > mFutures;
+
+public:
+
+  void Wait()
+  {
+    for( auto& future : mFutures )
+    {
+      future->Wait();
+    }
+  }
+
+};
+
+class WorkerThread
+{
+private:
+  std::thread mWorker;
+
+  uint32_t mIndex;
+
+  TaskQueue mTaskQueue;
+
+  std::mutex mTaskQueueMutex;
+
+  std::condition_variable mConditionVariable;
+
+  bool mTerminating{ false };
+
+  void WaitAndExecute();
+
+public:
+  WorkerThread(uint32_t index);
+
+  WorkerThread( const WorkerThread& other ) = delete;
+
+  WorkerThread& operator=( const WorkerThread& other ) = delete;
+
+  ~WorkerThread();
+
+  void AddTask( Task task );
+
+  void AddTask( Task task, bool doNotify );
+
+  void Notify();
+
+  void Wait();
+};
+
+class ThreadPool
+{
+private:
+  std::vector< std::unique_ptr< WorkerThread>> mWorkers;
+
+  uint32_t mWorkerIndex {0u};
+
+public:
+  bool Initialize( uint32_t threadCount = 0u );
+
+  void Wait();
+
+  template< typename ReturnT >
+  std::shared_ptr< Future< ReturnT > > SubmitTask( uint32_t workerIndex, std::function< ReturnT(uint32_t) > task )
+  {
+    auto future = std::shared_ptr< Future< ReturnT > >( new Future< ReturnT > );
+    mWorkers[workerIndex]->AddTask( [ task, future ](uint32_t index) {
+      auto res = task( index );
+      future->mPromise.set_value( res );
+    } );
+
+    return future;
+  }
+
+
+  std::shared_ptr< Future< void > > SubmitTask( uint32_t workerIndex, const Task& task );
+
+  template< typename ReturnT >
+  std::shared_ptr< Future< void > > SubmitTask( std::function< ReturnT(uint32_t) > task )
+  {
+    return SubmitTask( mWorkerIndex++ % static_cast< uint32_t >( mWorkers.size() ), std::move( task ) );
+  }
+
+  std::shared_ptr< Future< void > > SubmitTasks( const std::vector< Task >& tasks );
+
+  std::unique_ptr<FutureGroup<void>> SubmitTasks( const std::vector< Task >& tasks, uint32_t threadMask );
+
+  template< typename T, typename Predicate >
+  std::shared_ptr< FutureGroup< void > >
+  ParallelProcess( std::vector< T >& data, Predicate predicate, bool useMainThread )
+  {
+    auto workerCount = useMainThread ? mWorkers.size() + 1 : mWorkers.size();
+    auto tasksPerThread = data.size() / workerCount;
+
+    if (tasksPerThread == 0 ) tasksPerThread = data.size();
+
+    auto batches = useMainThread ? (data.size() / tasksPerThread) - 1 : data.size() / tasksPerThread;
+
+    auto start = 0ul;
+    auto end = 0ul;
+
+    auto futureGroup = std::shared_ptr< FutureGroup< void > >( new FutureGroup< void > );
+
+    for( auto i = 0u; i < batches; ++i )
+    {
+      futureGroup->mFutures.emplace_back( new Future< void >() );
+    }
+
+    for( auto i = 0u; i < batches; ++i )
+    {
+      if ( end == data.size() )
+      {
+        break;
+      }
+
+      if( !useMainThread )
+      {
+        end = ( i == batches - 1 ) ? start + tasksPerThread + ( data.size() - batches * tasksPerThread ) :
+              start + tasksPerThread;
+      }
+      else
+      {
+        end = start + tasksPerThread;
+      }
+
+      auto task = Task(
+        [ futureGroup, i, start, end, predicate, &data ](uint32_t index)
+        {
+          auto sIt = data.begin() + static_cast<int>(start);
+          auto eIt = data.begin() + static_cast<int>(end);
+          std::for_each( sIt, eIt, predicate );
+
+          futureGroup->mFutures[i]->mPromise.set_value();
+        } );
+
+      mWorkers[mWorkerIndex++ % static_cast< uint32_t >( mWorkers.size() )]->AddTask( task );
+
+      start = end;
+    }
+
+    if( useMainThread )
+    {
+      end = start + tasksPerThread + ( data.size() - (++batches) * tasksPerThread );
+
+      auto sIt = data.begin() + static_cast<int>(start);
+      auto eIt = data.begin() + static_cast<int>(end);
+      std::for_each( sIt, eIt, predicate );
+    }
+
+    return futureGroup;
+  }
+
+  template< typename T, typename Predicate >
+  std::shared_ptr< FutureGroup< void > >
+  IndexedParallelProcess( std::vector< T >& data, Predicate predicate, bool useMainThread )
+  {
+    auto workerCount = useMainThread ? mWorkers.size() + 1 : mWorkers.size();
+    auto tasksPerThread = data.size() / workerCount;
+
+    if (tasksPerThread == 0 )
+    {
+      tasksPerThread = data.size();
+    }
+
+    auto batches = useMainThread ? (data.size() / tasksPerThread) - 1 : data.size() / tasksPerThread;
+
+    auto start = 0ul;
+    auto end = 0ul;
+
+    auto futureGroup = std::shared_ptr< FutureGroup< void > >( new FutureGroup< void > );
+
+    for( auto i = 0u; i < batches; ++i )
+    {
+      futureGroup->mFutures.emplace_back( new Future< void >() );
+    }
+
+    for( auto i = 0u; i < batches; ++i )
+    {
+      if ( end == data.size() )
+      {
+        break;
+      }
+
+      if( !useMainThread )
+      {
+        end = ( i == batches - 1 ) ? start + tasksPerThread + ( data.size() - batches * tasksPerThread ) :
+              start + tasksPerThread;
+      }
+      else
+      {
+        end = start + tasksPerThread;
+      }
+
+      auto task = Task(
+        [ futureGroup, i, start, end, predicate, &data ](uint32_t index)
+        {
+          auto sIt = data.begin() + static_cast<int>(start);
+          auto eIt = data.begin() + static_cast<int>(end);
+          std::for_each( sIt, eIt, std::bind( predicate, std::placeholders::_1, index ) );
+
+          futureGroup->mFutures[i]->mPromise.set_value();
+        } );
+
+      mWorkers[mWorkerIndex++ % static_cast< uint32_t >( mWorkers.size() )]->AddTask( task );
+
+      start = end;
+    }
+
+    if( useMainThread )
+    {
+      end = start + tasksPerThread + ( data.size() - (++batches) * tasksPerThread );
+
+      auto sIt = data.begin() + static_cast<int>(start);
+      auto eIt = data.begin() + static_cast<int>(end);
+      std::for_each( sIt, eIt, std::bind( predicate,
+                                          std::placeholders::_1,
+                                          mWorkers.size() /* the main thread will be the last index */ ) );
+    }
+
+    return futureGroup;
+  }
+
+  size_t GetWorkerCount() const;
+};
+
+} //namespace Dali
+
+#endif // DALI_THREAD_POOL_H
