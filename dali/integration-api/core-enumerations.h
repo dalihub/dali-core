35ca427163218663a73550e7c24285ae128fd7cf
Joogab Yun <joogab.yun@samsung.com>
2019-12-27 17:21:19 +0900

    [Tizen] Implement partial update
    This reverts commit fa00680bb4d86d2b301e82864bc81246579d9922.
    
    Change-Id: I88fd2711d25cd65144053df082ea6a2baebd0a05

--- dali/integration-api/core-enumerations.h
+++ dali/integration-api/core-enumerations.h
@@ -56,6 +56,15 @@ enum class StencilBufferAvailable
   TRUE
 };
 
+/**
+ * @brief Enumerations to specify whether the partial update is available.
+ */
+enum class PartialUpdateAvailable
+{
+  FALSE = 0,
+  TRUE
+};
+
 } // namespace Integration
 
 } // namespace Dali
