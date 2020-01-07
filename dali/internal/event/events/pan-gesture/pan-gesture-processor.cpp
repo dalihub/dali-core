377c1ca97120a89e029655c6443372c846f3f378
Adeel Kazmi <adeel.kazmi@samsung.com>
2020-01-07 16:15:45 +0000

    GCC 9.2 Support
    Change-Id: I762e8e95654724ff648b93172788cdcaf0a0def3

--- dali/internal/event/events/pan-gesture/pan-gesture-processor.cpp
+++ dali/internal/event/events/pan-gesture/pan-gesture-processor.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 Samsung Electronics Co., Ltd.
+ * Copyright (c) 2020 Samsung Electronics Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -210,8 +210,10 @@ void PanGestureProcessor::Process( Scene& scene, const PanGestureEvent& panEvent
     {
       // Requires a core update
       mNeedsUpdate = true;
+
+      DALI_FALLTHROUGH;
     }
-    // No break, Fallthrough
+
     case Gesture::Finished:
     case Gesture::Cancelled:
     {
