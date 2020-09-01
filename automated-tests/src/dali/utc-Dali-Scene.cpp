f33b548fc3bd1aa5d106bab2da00edd41279f2de
Joogab Yun <joogab.yun@samsung.com>
2020-09-01 16:26:43 +0900

    [Tizen] Add screen and client rotation itself function
    This reverts commit 3091ff3b58fda4659c0b1674cebbd27d2670ee72.
    
    Change-Id: I2675405444f74d55efad849f1559910a3584d26f

--- automated-tests/src/dali/utc-Dali-Scene.cpp
+++ automated-tests/src/dali/utc-Dali-Scene.cpp
@@ -873,7 +873,8 @@ int UtcDaliSceneSurfaceResizedDefaultScene(void)
   // Resize the scene
   Vector2 newSize( 1000.0f, 2000.0f );
   DALI_TEST_CHECK( stage.GetSize() != newSize );
-  defaultScene.SurfaceResized( newSize.width, newSize.height );
+
+  defaultScene.SurfaceResized( newSize.width, newSize.height, 0, false );
 
   DALI_TEST_EQUALS( stage.GetSize(), newSize, TEST_LOCATION );
   DALI_TEST_EQUALS( defaultScene.GetSize(), newSize, TEST_LOCATION );
@@ -916,7 +917,7 @@ int UtcDaliSceneSurfaceResizedDefaultSceneViewport(void)
   Vector2 newSize( 1000.0f, 2000.0f );
   std::string viewportParams( "0, 0, 1000, 2000" ); // to match newSize
   DALI_TEST_CHECK( stage.GetSize() != newSize );
-  defaultScene.SurfaceResized( newSize.width, newSize.height );
+  defaultScene.SurfaceResized( newSize.width, newSize.height, 0, false );
 
   DALI_TEST_EQUALS( stage.GetSize(), newSize, TEST_LOCATION );
   DALI_TEST_EQUALS( defaultScene.GetSize(), newSize, TEST_LOCATION );
@@ -988,7 +989,7 @@ int UtcDaliSceneSurfaceResizedMultipleRenderTasks(void)
   Vector2 newSize( 1000.0f, 2000.0f );
   std::string viewportParams( "0, 0, 1000, 2000" ); // to match newSize
   DALI_TEST_CHECK( stage.GetSize() != newSize );
-  defaultScene.SurfaceResized( newSize.width, newSize.height );
+  defaultScene.SurfaceResized( newSize.width, newSize.height, 0, false );
 
   DALI_TEST_EQUALS( stage.GetSize(), newSize, TEST_LOCATION );
   DALI_TEST_EQUALS( defaultScene.GetSize(), newSize, TEST_LOCATION );
@@ -1028,7 +1029,8 @@ int UtcDaliSceneSurfaceResizedAdditionalScene(void)
   // Resize the surface and inform the scene accordingly
   Vector2 newSize( 1000.0f, 2000.0f );
   DALI_TEST_CHECK( stage.GetSize() != newSize );
-  scene.SurfaceResized( newSize.width, newSize.height );
+
+  scene.SurfaceResized( newSize.width, newSize.height, 0, false );
 
   // Ensure the stage hasn't been resized
   DALI_TEST_EQUALS( stage.GetSize(), stageSize, TEST_LOCATION );
