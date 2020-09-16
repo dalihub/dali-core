7ea1ac68a0262257ff8b4a822601b60fdc00d6d4
Seoyeon Kim <seoyeon2.kim@samsung.com>
2020-09-16 11:29:04 +0900

    Revert "[Tizen] Add Integration API to Create public event type"
    This reverts commit 4ffe40a1c9ec1384e9cbf156ed8cfca09f1f6198.

--- dali/integration-api/events/touch-integ.cpp
+++ dali/integration-api/events/touch-integ.cpp
@@ -22,16 +22,11 @@ namespace Dali
 namespace Integration
 {
 
-Dali::TouchEvent NewTouchEvent(uint32_t timeStamp, const TouchPoint& point)
+Dali::TouchEvent NewTouchEvent(uint32_t timestamp, const TouchPoint& point)
 {
-  return NewTouchEvent( timeStamp, Integration::Point( point ) );
-}
-
-Dali::TouchEvent NewTouchEvent( uint32_t timeStamp, const Dali::Integration::Point& point )
-{
-  Internal::TouchEventPtr touchEventImpl( new Internal::TouchEvent( timeStamp ) );
-  touchEventImpl->AddPoint( point );
-  Dali::TouchEvent handle( touchEventImpl.Get() );
+  Internal::TouchEventPtr touchEventImpl( new Internal::TouchEvent(timestamp) );
+  touchEventImpl->AddPoint(Integration::Point(point));
+  Dali::TouchEvent handle(touchEventImpl.Get());
   return handle;
 }
 
