70e6b040872c1f78727f97dd587ba20385e21d10
David Steele <david.steele@samsung.com>
2020-10-30 14:33:16 +0000

    DALi Version 1.9.36
    Change-Id: Ie87e252297e782248c1d5222a3cabc58e2c8f76a

--- dali/public-api/dali-core-version.cpp
+++ dali/public-api/dali-core-version.cpp
@@ -27,7 +27,7 @@ namespace Dali
 {
 const uint32_t    CORE_MAJOR_VERSION = 1;
 const uint32_t    CORE_MINOR_VERSION = 9;
-const uint32_t    CORE_MICRO_VERSION = 35;
+const uint32_t    CORE_MICRO_VERSION = 36;
 const char* const CORE_BUILD_DATE    = __DATE__ " " __TIME__;
 
 #ifdef DEBUG_ENABLED
--- packaging/dali.spec
+++ packaging/dali.spec
@@ -1,6 +1,6 @@
 Name:       dali2
 Summary:    DALi 3D Engine
-Version:    1.9.35
+Version:    1.9.36
 Release:    1
 Group:      System/Libraries
 License:    Apache-2.0 and BSD-3-Clause and MIT
