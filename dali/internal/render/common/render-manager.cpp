5b10a481d33f3667b2366be033aed1a8baa8228c
Joogab Yun <joogab.yun@samsung.com>
2020-11-12 15:07:03 +0900

    [Tizen] Partial rendering rotation does not work
    Change-Id: If62c8667519ad3c375f29a7095482a6535fc9332

--- dali/internal/render/common/render-manager.cpp
+++ dali/internal/render/common/render-manager.cpp
@@ -566,6 +566,15 @@ void RenderManager::PreRender( Integration::Scene& scene, std::vector<Rect<int>>
     return;
   }
 
+  Internal::Scene& sceneInternal = GetImplementation(scene);
+  SceneGraph::Scene* sceneObject = sceneInternal.GetSceneObject();
+
+  // @TODO We need to do partial rendering rotation.
+  if( sceneObject && sceneObject->GetSurfaceOrientation() != 0 )
+  {
+    return;
+  }
+
   class DamagedRectsCleaner
   {
   public:
@@ -597,10 +606,6 @@ void RenderManager::PreRender( Integration::Scene& scene, std::vector<Rect<int>>
 
   // Clean collected dirty/damaged rects on exit if 3d layer or 3d node or other conditions.
   DamagedRectsCleaner damagedRectCleaner(damagedRects);
-
-
-  Internal::Scene& sceneInternal = GetImplementation(scene);
-  SceneGraph::Scene* sceneObject = sceneInternal.GetSceneObject();
 
   // Mark previous dirty rects in the sorted array. The array is already sorted by node and renderer, frame number.
   // so you don't need to sort: std::stable_sort(itemsDirtyRects.begin(), itemsDirtyRects.end());
