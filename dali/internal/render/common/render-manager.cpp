6bfe656ae942470e3768d2a143a536b92a24d1ea
Heeyong Song <heeyong.song@samsung.com>
2020-11-19 11:57:20 +0900

    Revert "[Tizen] Add screen and client rotation itself function"
    This reverts commit 54e09df4cbea2e5d857a86d6f3bf7ffd7bb9f07a.
    
    Change-Id: I515ca38b474c874d3c9aee7cd799095617a4a374

--- dali/internal/render/common/render-manager.cpp
+++ dali/internal/render/common/render-manager.cpp
@@ -77,8 +77,7 @@ struct RenderManager::Impl
     programController( glAbstraction ),
     depthBufferAvailable( depthBufferAvailableParam ),
     stencilBufferAvailable( stencilBufferAvailableParam ),
-    partialUpdateAvailable( partialUpdateAvailableParam ),
-    defaultSurfaceOrientation( 0 )
+    partialUpdateAvailable( partialUpdateAvailableParam )
   {
      // Create thread pool with just one thread ( there may be a need to create more threads in the future ).
     threadPool = std::unique_ptr<Dali::ThreadPool>( new Dali::ThreadPool() );
@@ -175,7 +174,6 @@ struct RenderManager::Impl
   std::unique_ptr<Dali::ThreadPool>         threadPool;               ///< The thread pool
   Vector<GLuint>                            boundTextures;            ///< The textures bound for rendering
   Vector<GLuint>                            textureDependencyList;    ///< The dependency list of binded textures
-  int                                       defaultSurfaceOrientation; ///< defaultSurfaceOrientation for the default surface we are rendering to
 };
 
 RenderManager* RenderManager::New( Integration::GlAbstraction& glAbstraction,
@@ -257,11 +255,6 @@ void RenderManager::SetShaderSaver( ShaderSaver& upstream )
 void RenderManager::SetDefaultSurfaceRect(const Rect<int32_t>& rect)
 {
   mImpl->defaultSurfaceRect = rect;
-}
-
-void RenderManager::SetDefaultSurfaceOrientation( int orientation )
-{
-  mImpl->defaultSurfaceOrientation = orientation;
 }
 
 void RenderManager::AddRenderer( OwnerPointer< Render::Renderer >& renderer )
@@ -829,7 +822,6 @@ void RenderManager::RenderScene( Integration::RenderStatus& status, Integration:
     Rect<int32_t> surfaceRect = mImpl->defaultSurfaceRect;
     Integration::DepthBufferAvailable depthBufferAvailable = mImpl->depthBufferAvailable;
     Integration::StencilBufferAvailable stencilBufferAvailable = mImpl->stencilBufferAvailable;
-    int surfaceOrientation = mImpl->defaultSurfaceOrientation;
 
     if ( instruction.mFrameBuffer )
     {
@@ -929,7 +921,6 @@ void RenderManager::RenderScene( Integration::RenderStatus& status, Integration:
       {
         viewportRect.Set( 0, 0, instruction.mFrameBuffer->GetWidth(), instruction.mFrameBuffer->GetHeight() );
       }
-      surfaceOrientation = 0;
     }
     else // No Offscreen frame buffer rendering
     {
@@ -944,13 +935,6 @@ void RenderManager::RenderScene( Integration::RenderStatus& status, Integration:
       {
         viewportRect = surfaceRect;
       }
-    }
-
-    if( surfaceOrientation == 90 || surfaceOrientation == 270 )
-    {
-      int temp = viewportRect.width;
-      viewportRect.width = viewportRect.height;
-      viewportRect.height = temp;
     }
 
     bool clearFullFrameRect = true;
@@ -1016,8 +1000,7 @@ void RenderManager::RenderScene( Integration::RenderStatus& status, Integration:
         depthBufferAvailable,
         stencilBufferAvailable,
         mImpl->boundTextures,
-        clippingRect,
-        surfaceOrientation );
+        clippingRect );
 
     // Synchronise the FBO/Texture access when there are multiple contexts
     if ( mImpl->currentContext->IsSurfacelessContextSupported() )
