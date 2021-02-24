30d8d280b1c5d4a247d7e3701b5ec41ec95227c8
hosangKim <hosang12.kim@samsung.com>
2021-02-24 15:55:27 +0900

    Revert "[Tizen] Partial rendering rotation does not work"
    This reverts commit 32ce774cae806d4cd8b24f98fbe8af6174566659.

--- dali/internal/render/common/render-manager.cpp
+++ dali/internal/render/common/render-manager.cpp
@@ -559,12 +559,6 @@ void RenderManager::PreRender(Integration::Scene& scene, std::vector<Rect<int>>&
     return;
   }
 
-  // @TODO We need to do partial rendering rotation.
-  if( sceneObject && sceneObject->GetSurfaceOrientation() != 0 )
-  {
-    return;
-  }
-
   class DamagedRectsCleaner
   {
   public:
