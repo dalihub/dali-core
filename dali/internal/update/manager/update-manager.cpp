b55e83d3509f60de5ee9215a1b62c521308a3e67
Heeyong Song <heeyong.song@samsung.com>
2021-05-25 14:23:36 +0900

    Fix node tree logging
    Change-Id: I7d01d54fee9dae84f2e2ae9364b4e0a8066104ac

--- dali/internal/update/manager/update-manager.cpp
+++ dali/internal/update/manager/update-manager.cpp
@@ -44,20 +44,20 @@
 //#define NODE_TREE_LOGGING 1
 
 #if(defined(DEBUG_ENABLED) && defined(NODE_TREE_LOGGING))
-#define SNAPSHOT_NODE_LOGGING                                                       \
-  const uint32_t FRAME_COUNT_TRIGGER = 16;                                          \
-  if(mImpl->frameCounter >= FRAME_COUNT_TRIGGER)                                    \
-  {                                                                                 \
-    for(auto&& scene : mImpl->scenes)                                               \
-    {                                                                               \
-      if(scene && scene->root)                                                      \
-      {                                                                             \
-        mImpl->frameCounter = 0;                                                    \
-        PrintNodeTree(*scene->root, mSceneGraphBuffers.GetUpdateBufferIndex(), ""); \
-      }                                                                             \
-    }                                                                               \
-  }
-mImpl->frameCounter++;
+#define SNAPSHOT_NODE_LOGGING                                                   \
+  const uint32_t FRAME_COUNT_TRIGGER = 16;                                      \
+  if(mImpl->frameCounter >= FRAME_COUNT_TRIGGER)                                \
+  {                                                                             \
+    for(auto&& scene : mImpl->scenes)                                           \
+    {                                                                           \
+      if(scene && scene->root)                                                  \
+      {                                                                         \
+        mImpl->frameCounter = 0;                                                \
+        PrintNodes(*scene->root, mSceneGraphBuffers.GetUpdateBufferIndex(), 0); \
+      }                                                                         \
+    }                                                                           \
+  }                                                                             \
+  mImpl->frameCounter++;
 #else
 #define SNAPSHOT_NODE_LOGGING
 #endif
