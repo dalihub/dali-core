53a534bebeb0f32519aa030f65d037f858b974e7
Heeyong Song <heeyong.song@samsung.com>
2022-02-03 14:18:10 +0900

    Add stride to PixelData
    Change-Id: Ida564386962e1afed91d3f6805b5a14f446a6937

--- dali/internal/render/renderers/render-texture.cpp
+++ dali/internal/render/renderers/render-texture.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Samsung Electronics Co., Ltd.
+ * Copyright (c) 2022 Samsung Electronics Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -289,6 +289,7 @@ void Texture::Upload(PixelDataPtr pixelData, const Internal::Texture::UploadPara
   info.srcExtent2D  = {params.width, params.height};
   info.srcOffset    = 0;
   info.srcSize      = pixelData->GetBufferSize();
+  info.srcStride    = pixelData->GetStride();
   info.srcFormat    = ConvertPixelFormat(pixelData->GetPixelFormat());
 
   Graphics::TextureUpdateSourceInfo updateSourceInfo{};
