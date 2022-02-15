538d808523cf7cdb8dc37e10d60aa4fce636fb24
Eunki, Hong <eunkiki.hong@samsung.com>
2022-02-15 15:48:17 +0900

    Level up Dali::Internal::FreeList as Dali::FreeList
    Open devel Dali::Internal::FreeList as Dali::FreeList
    so we can use this container in another packages i.e. dali-toolkit's texture-manager
    
    Change-Id: I719e960ce1ecf8b61a980e49cba976fe1ab57200
    Signed-off-by: Eunki, Hong <eunkiki.hong@samsung.com>

--- dali/internal/update/manager/transform-manager.h
+++ dali/internal/update/manager/transform-manager.h
@@ -2,7 +2,7 @@
 #define DALI_INTERNAL_TRANSFORM_MANAGER_H
 
 /*
- * Copyright (c) 2021 Samsung Electronics Co., Ltd.
+ * Copyright (c) 2022 Samsung Electronics Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,7 +19,7 @@
  */
 
 // INTERNAL INCLUDES
-#include <dali/internal/update/manager/free-list.h>
+#include <dali/devel-api/common/free-list.h>
 #include <dali/public-api/common/constants.h>
 #include <dali/public-api/common/dali-vector.h>
 #include <dali/public-api/math/matrix.h>
