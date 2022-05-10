c4fdd06b5c40cf636afb26435ca3e9c3b54c0565
EverLEEst(SangHyeon Lee) <sh10233.lee@samsung.com>
2022-05-10 14:07:17 +0900

    [Tizen] Print backtrace when an exception occurs
    This reverts commit a111f7ee1ce464184ddd1765fb2ba0477a2f2f4b.

--- dali/public-api/common/dali-common.cpp
+++ dali/public-api/common/dali-common.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020 Samsung Electronics Co., Ltd.
+ * Copyright (c) 2021 Samsung Electronics Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,6 +22,8 @@
 #include <stdlib.h>
 #include <cstdio>
 #include <string>
+
+#define BACKTRACE_ENABLED
 
 #if defined(BACKTRACE_ENABLED)
 #if defined(__GLIBC__)
