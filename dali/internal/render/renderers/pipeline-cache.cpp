132cce497783a42c4035ef34888abb4c3fff8bdf
Eunki, Hong <eunkiki.hong@samsung.com>
2022-11-15 15:52:20 +0900

    Fix typo error
    Maybe is this typo error?
    
    Change-Id: I450bd5ec74252f0c59616110aeb3f9b55b05d0e9
    Signed-off-by: Eunki, Hong <eunkiki.hong@samsung.com>

--- dali/internal/render/renderers/pipeline-cache.cpp
+++ dali/internal/render/renderers/pipeline-cache.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Samsung Electronics Co., Ltd.
+ * Copyright (c) 2022 Samsung Electronics Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -364,7 +364,7 @@ PipelineCacheL2* PipelineCacheL1::GetPipelineCacheL2(bool blend, bool premul, Bl
     auto& colorBlendState = l2.colorBlendState;
     colorBlendState.SetBlendEnable(true);
     Graphics::BlendOp rgbOp   = ConvertBlendEquation(blendingOptions.GetBlendEquationRgb());
-    Graphics::BlendOp alphaOp = ConvertBlendEquation(blendingOptions.GetBlendEquationRgb());
+    Graphics::BlendOp alphaOp = ConvertBlendEquation(blendingOptions.GetBlendEquationAlpha());
     if(blendingOptions.IsAdvancedBlendEquationApplied() && premul)
     {
       if(rgbOp != alphaOp)
