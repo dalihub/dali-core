1c99aaf6496ef6810b23a1ff400e2f6abf927dc3
David Steele <david.steele@samsung.com>
2023-03-06 18:54:41 +0000

    [Tizen] Add BuildPickingRay to devel api
    Change-Id: I93874e9b546c9f56f90a0cf11e8c4dbc3b7e7b6a
    Signed-off-by: huiyu.eun <huiyu.eun@samsung.com>

--- dali/devel-api/events/hit-test-algorithm.cpp
+++ dali/devel-api/events/hit-test-algorithm.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020 Samsung Electronics Co., Ltd.
+ * Copyright (c) 2023 Samsung Electronics Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,6 +19,7 @@
 #include <dali/devel-api/events/hit-test-algorithm.h>
 
 // INTERNAL INCLUDES
+#include <dali/internal/event/actors/camera-actor-impl.h>
 #include <dali/internal/event/common/scene-impl.h>
 #include <dali/internal/event/common/stage-impl.h>
 #include <dali/internal/event/events/hit-test-algorithm-impl.h>
@@ -32,6 +33,30 @@ bool HitTest(Stage stage, const Vector2& screenCoordinates, Results& results, Hi
 {
   Internal::Stage& stageImpl = GetImplementation(stage);
   return Internal::HitTestAlgorithm::HitTest(stageImpl.GetSize(), stageImpl.GetRenderTaskList(), stageImpl.GetLayerList(), screenCoordinates, results, func);
+}
+
+bool BuildPickingRay(RenderTask renderTask, const Vector2& screenCoordinates, Vector3& origin, Vector3& direction)
+{
+  Viewport viewport = renderTask.GetViewport();
+  if(screenCoordinates.x < static_cast<float>(viewport.x) ||
+     screenCoordinates.x > static_cast<float>(viewport.x + viewport.width) ||
+     screenCoordinates.y < static_cast<float>(viewport.y) ||
+     screenCoordinates.y > static_cast<float>(viewport.y + viewport.height))
+  {
+    // The screen coordinate is outside the viewport of render task. The viewport clips all layers.
+    return false;
+  }
+  CameraActor            cameraActor     = renderTask.GetCameraActor();
+  Internal::CameraActor& cameraActorImpl = GetImplementation(cameraActor);
+  Vector4                rayOrigin;
+  Vector4                rayDirection;
+  bool                   success = cameraActorImpl.BuildPickingRay(screenCoordinates, viewport, rayOrigin, rayDirection);
+  if(success)
+  {
+    origin    = Vector3(rayOrigin.x, rayOrigin.y, rayOrigin.z);
+    direction = Vector3(rayDirection.x, rayDirection.y, rayDirection.z);
+  }
+  return success;
 }
 
 } // namespace HitTestAlgorithm
