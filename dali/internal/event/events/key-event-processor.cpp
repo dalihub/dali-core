5bcc1aa5c324cc09ab19dcfb05a7d9247b46d3ab
Eunki, Hong <eunkiki.hong@samsung.com>
2023-05-24 09:32:05 +0900

    [Tizen] Appendix log for ttrace + Print keycode and timestamp
    Since we need to know the state of key event and timestamp, let we print it
    only if performance check state enabled
    
    And also, we need to print the additional informations in same line.
    (Since third-party performance profile tools requirements)
    
    Change-Id: I9b1b176fcba76e5997b09136b2d634ad493dcf5f
    Signed-off-by: Eunki, Hong <eunkiki.hong@samsung.com>

--- dali/internal/event/events/key-event-processor.cpp
+++ dali/internal/event/events/key-event-processor.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Samsung Electronics Co., Ltd.
+ * Copyright (c) 2023 Samsung Electronics Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,6 +18,11 @@
 // CLASS HEADER
 #include <dali/internal/event/events/key-event-processor.h>
 
+// EXTERNAL INCLUDES
+#ifdef TRACE_ENABLED
+#include <sstream> ///< for std::ostringstream
+#endif
+
 // INTERNAL INCLUDES
 #include <dali/integration-api/events/key-event-integ.h>
 #include <dali/integration-api/trace.h>
@@ -32,6 +37,14 @@ namespace Internal
 namespace
 {
 DALI_INIT_TRACE_FILTER(gTraceFilter, DALI_TRACE_PERFORMANCE_MARKER, false);
+
+#ifdef TRACE_ENABLED
+const char* KEY_EVENT_STATES[Dali::KeyEvent::State::UP + 1] =
+  {
+    "DOWN",
+    "UP",
+};
+#endif
 } // namespace
 
 KeyEventProcessor::KeyEventProcessor(Scene& scene)
@@ -46,7 +59,14 @@ void KeyEventProcessor::ProcessKeyEvent(const Integration::KeyEvent& event)
   KeyEventPtr    keyEvent(new KeyEvent(event.keyName, event.logicalKey, event.keyString, event.keyCode, event.keyModifier, event.time, static_cast<Dali::KeyEvent::State>(event.state), event.compose, event.deviceName, event.deviceClass, event.deviceSubclass));
   Dali::KeyEvent keyEventHandle(keyEvent.Get());
 
-  DALI_TRACE_SCOPE(gTraceFilter, "DALI_PROCESS_KEY_EVENT");
+#ifdef TRACE_ENABLED
+  if(gTraceFilter && gTraceFilter->IsTraceEnabled())
+  {
+    std::ostringstream stream;
+    stream << "[name:" << event.keyName << ", code:" << event.keyCode << ", state:" << KEY_EVENT_STATES[event.state] << ", time:" << event.time << "]";
+    DALI_TRACE_BEGIN_WITH_MESSAGE(gTraceFilter, "DALI_PROCESS_KEY_EVENT", stream.str().c_str());
+  }
+#endif
 
   // Emit the key event signal from the scene.
   bool consumed = mScene.EmitInterceptKeyEventSignal(keyEventHandle);
@@ -58,6 +78,12 @@ void KeyEventProcessor::ProcessKeyEvent(const Integration::KeyEvent& event)
   {
     mScene.EmitKeyEventSignal(keyEventHandle);
   }
+#ifdef TRACE_ENABLED
+  if(gTraceFilter && gTraceFilter->IsTraceEnabled())
+  {
+    DALI_TRACE_END(gTraceFilter, "DALI_PROCESS_KEY_EVENT");
+  }
+#endif
 }
 
 } // namespace Internal
