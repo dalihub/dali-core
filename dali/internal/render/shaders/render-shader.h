fed8708e2bae7b55bc711546fc0bb9c3a8cdf24b
Heeyong Song <heeyong.song@samsung.com>
2023-06-16 11:47:09 +0900

    Remove unsued member variable
    Change-Id: If4125c2e39bd97890a470ea6b1a6056e70f835d2

--- dali/internal/render/shaders/render-shader.h
+++ dali/internal/render/shaders/render-shader.h
@@ -2,7 +2,7 @@
 #define DALI_INTERNAL_RENDER_SHADER_H
 
 /*
- * Copyright (c) 2021 Samsung Electronics Co., Ltd.
+ * Copyright (c) 2023 Samsung Electronics Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -70,8 +70,6 @@ public:
   [[nodiscard]] ShaderDataPtr GetShaderData(uint32_t renderPass) const;
 
 private: // Data
-  Dali::Shader::Hint::Value mHints;
-
   std::vector<ShaderDataPtr> mShaderDataList;
 };
 
