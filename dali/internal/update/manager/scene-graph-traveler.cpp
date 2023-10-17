4a1fa3ed1d45b11427803e16ae3b2f0a3af768f3
Eunki, Hong <eunkiki.hong@samsung.com>
2023-10-17 17:50:53 +0900

    Reduce update proxy performance
    Since we try to travel whole scene tree whenever
    we fail to found node id, It might make some performance drop.
    
    To avoid these kind of performance issue, let we cache
    whole traversal scene tree only one times until scene tree
    updated.
    
    Change-Id: Iab4379c9421a18e5fa9ebcb373c7ef1de91022a5
    Signed-off-by: Eunki, Hong <eunkiki.hong@samsung.com>

---
+++ dali/internal/update/manager/scene-graph-traveler.cpp
@@ -0,0 +1,123 @@
+/*
+ * Copyright (c) 2023 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+// CLASS HEADER
+#include <dali/internal/update/manager/scene-graph-traveler.h>
+
+// INTERNAL INCLUDES
+#include <dali/internal/update/nodes/node.h>
+
+namespace Dali
+{
+namespace Internal
+{
+SceneGraphTraveler::SceneGraphTraveler(SceneGraph::Node& rootNode)
+: mRootNode(rootNode),
+  mNodeStack{},
+  mInvalidated{false}
+{
+  mRootNode.AddObserver(*this);
+  Clear();
+}
+
+SceneGraphTraveler::~SceneGraphTraveler()
+{
+  if(!mInvalidated)
+  {
+    mRootNode.RemoveObserver(*this);
+  }
+}
+
+SceneGraph::Node* SceneGraphTraveler::FindNode(uint32_t id)
+{
+  SceneGraph::Node* node = nullptr;
+
+  // Find node in cached map
+  auto iter = mTravledNodeMap.find(id);
+  if(iter != mTravledNodeMap.end())
+  {
+    node = iter->second;
+  }
+  else
+  {
+    while(!FullSearched())
+    {
+      SceneGraph::Node& currentNode = GetCurrentNode();
+      IterateNextNode();
+
+      // Cache traveled node and id pair.
+      mTravledNodeMap.insert({currentNode.mId, &currentNode});
+
+      if(currentNode.mId == id)
+      {
+        node = &currentNode;
+        break;
+      }
+    }
+  }
+
+  return node;
+}
+
+void SceneGraphTraveler::Clear()
+{
+  mTravledNodeMap.clear();
+  mTravledNodeMap.rehash(0u); ///< Note : We have to reduce capacity of hash map. Without this line, clear() API will be slow downed.
+  mNodeStack.clear();
+  if(!mInvalidated)
+  {
+    mNodeStack.emplace_back(&mRootNode, 0u);
+  }
+}
+
+bool SceneGraphTraveler::FullSearched() const
+{
+  return mNodeStack.empty();
+}
+
+SceneGraph::Node& SceneGraphTraveler::GetCurrentNode()
+{
+  DALI_ASSERT_DEBUG(!FullSearched());
+
+  return *(mNodeStack.back().first);
+}
+
+void SceneGraphTraveler::IterateNextNode()
+{
+  while(!mNodeStack.empty())
+  {
+    auto&    currentNode       = *(mNodeStack.back().first);
+    uint32_t currentChildIndex = mNodeStack.back().second;
+
+    if(currentNode.GetChildren().Count() <= currentChildIndex)
+    {
+      mNodeStack.pop_back();
+      continue;
+    }
+    else
+    {
+      // Stack current child, and increase index
+      ++mNodeStack.back().second;
+      mNodeStack.emplace_back(currentNode.GetChildren()[currentChildIndex], 0u);
+      break;
+    }
+  }
+}
+
+} // namespace Internal
+
+} // namespace Dali
