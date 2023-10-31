eb4f6b96c3821b3e4df4fb3af8e70e57a0ffe892
Eunki Hong <eunkiki.hong@samsung.com>
2023-10-31 23:16:54 +0900

    [Tizen] Fix svace/coverity issue (key events constructor)
    This reverts commit 8989306b3ad8a8702955b0bbfc38b8fb2c9aff96.
    
    Change-Id: I170518cb3a9c95dc5f2ee751d442d0fba905b500

--- dali/internal/event/events/key-event-impl.cpp
+++ dali/internal/event/events/key-event-impl.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Samsung Electronics Co., Ltd.
+ * Copyright (c) 2023 Samsung Electronics Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -44,7 +44,8 @@ KeyEvent::KeyEvent()
   mCompose(""),
   mDeviceName(""),
   mDeviceClass(Device::Class::NONE),
-  mDeviceSubclass(Device::Subclass::NONE)
+  mDeviceSubclass(Device::Subclass::NONE),
+  mIsRepeat(false)
 {
 }
 
