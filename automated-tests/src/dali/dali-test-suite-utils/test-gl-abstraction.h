65668fbc0d583862e0627da35fb65ec05530c555
sunghyun kim <scholb.kim@samsung.com>
2023-11-22 13:59:11 +0900

    [Tizen] Revert "Moved shader cache to graphics backend"
    This reverts commit 5402aeac11d588e5d906f8e9f87076584b181339.
    
    Change-Id: Ic37d2430b50c51fd133a2292795cf6cd4f9e927c

--- automated-tests/src/dali/dali-test-suite-utils/test-gl-abstraction.h
+++ automated-tests/src/dali/dali-test-suite-utils/test-gl-abstraction.h
@@ -1041,32 +1041,7 @@ public:
 
     for(const auto& uniform : mActiveUniforms)
     {
-      std::string name = uniform.name;
-      if(uniform.size <= 1)
-      {
-        GetUniformLocation(program, name.c_str());
-      }
-      else
-      {
-        // Convert single active uniform from "uBlah[0]" or "uStruct[0].element" to N versions of the same
-        std::string suffix;
-        auto        iter = name.find("["); // Search for index operator
-        if(iter != std::string::npos)
-        {
-          name = uniform.name.substr(0, iter); // Strip off index operator
-          iter = uniform.name.find("]");
-          if(iter != std::string::npos && iter + 1 != uniform.name.length())
-          {
-            suffix = uniform.name.substr(iter + 1);
-          }
-        }
-        for(int i = 0; i < uniform.size; ++i)
-        {
-          std::stringstream nss;
-          nss << name << "[" << i << "]" << suffix;
-          GetUniformLocation(program, nss.str().c_str()); // Generate N uniforms in the uniform map
-        }
-      }
+      GetUniformLocation(program, uniform.name.c_str());
     }
 
     for(const auto& uniform : mCustomUniformData)
