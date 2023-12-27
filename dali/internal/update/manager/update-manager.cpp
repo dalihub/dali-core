0b59584bff261382facf73515387a32d832993fd
Eunki, Hong <eunkiki.hong@samsung.com>
2023-12-27 09:37:43 +0900

    [Tizen] Print logs if dali skip rendering
    Change-Id: Id41faabf812d99009e4a7dbf01941f8657833db3
    Signed-off-by: Eunki, Hong <eunkiki.hong@samsung.com>

--- dali/internal/update/manager/update-manager.cpp
+++ dali/internal/update/manager/update-manager.cpp
@@ -1141,6 +1141,10 @@ uint32_t UpdateManager::Update(float    elapsedSeconds,
           }
           else
           {
+            if(uploadOnly)
+            {
+              DALI_LOG_RELEASE_INFO("Scene skip rendering set as true, during 'uploadOnly' was true.\n");
+            }
             scene->scene->SetSkipRendering(true);
           }
 
