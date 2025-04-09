5b1e1997812d9227005062557c3c1870d60ee0c4
Eunki, Hong <eunkiki.hong@samsung.com>
2025-04-09 09:52:55 +0900

    Fix svace - Assert if RenderItem.mNode is nullptr + Print log if sceneobject is nullptr
    Change-Id: Ib85427f2605a6685e4a0c37142195ab82361c399
    Signed-off-by: Eunki, Hong <eunkiki.hong@samsung.com>

--- dali/internal/render/common/render-manager.cpp
+++ dali/internal/render/common/render-manager.cpp
@@ -705,6 +705,7 @@ void RenderManager::PreRenderScene(Integration::Scene& scene, Integration::Scene
   if(!sceneObject)
   {
     // May not be a scene object if the window is being removed.
+    DALI_LOG_ERROR("Scene was empty handle. Skip PreRenderScene\n");
     return;
   }
 
@@ -729,17 +730,10 @@ void RenderManager::PreRenderScene(Integration::Scene& scene, Integration::Scene
     return;
   }
 
-  if(!sceneObject || sceneObject->IsRenderingSkipped())
+  if(sceneObject->IsRenderingSkipped())
   {
     // We don't need to calculate dirty rects
-    if(!sceneObject)
-    {
-      DALI_LOG_ERROR("Scene was empty handle. Skip pre-rendering\n");
-    }
-    else
-    {
-      DALI_LOG_RELEASE_INFO("RenderingSkipped was set true. Skip pre-rendering\n");
-    }
+    DALI_LOG_RELEASE_INFO("RenderingSkipped was set true. Skip pre-rendering\n");
     return;
   }
 
@@ -866,6 +860,9 @@ void RenderManager::PreRenderScene(Integration::Scene& scene, Integration::Scene
             {
               RenderItem& item = renderList->GetItem(listIndex);
 
+              // For now, we don't allow to rendering nodeless renderer.
+              DALI_ASSERT_DEBUG(item.mNode && "RenderItem should have node!");
+
               // Get NodeInformation as const l-value, to reduce memory access operations.
               const SceneGraph::PartialRenderingData::NodeInfomations& nodeInfo = item.GetPartialRenderingDataNodeInfomations();
 
@@ -895,8 +892,7 @@ void RenderManager::PreRenderScene(Integration::Scene& scene, Integration::Scene
               DirtyRectKey dirtyRectKey(item.mNode, item.mRenderer);
               // If the item refers to updated node or renderer.
               if(item.mIsUpdated ||
-                 (item.mNode &&
-                  (item.mNode->Updated() || (item.mRenderer && item.mRenderer->Updated()))))
+                 (item.mNode->Updated() || (item.mRenderer && item.mRenderer->Updated())))
               {
                 item.mIsUpdated = false; /// DevNote : Reset flag here, since RenderItem could be reused by renderList.ReuseCachedItems().
 
@@ -1437,6 +1433,7 @@ void RenderManager::ClearScene(Integration::Scene scene)
   SceneGraph::Scene* sceneObject   = sceneInternal.GetSceneObject();
   if(!sceneObject)
   {
+    DALI_LOG_ERROR("Scene was empty handle. Skip ClearScene\n");
     return;
   }
 
