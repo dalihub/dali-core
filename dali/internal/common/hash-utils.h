8fdf5cf4031651d7c34a2df79d0d2ce518fcc171
Eunki, Hong <eunkiki.hong@samsung.com>
2025-05-08 12:19:25 +0900

    Make HashUtil function as inline
    Let we make hashutil functions as inline instead of cpp.
    
    Seperate it as cpp if we prepare to implement as NEON.
    
    Change-Id: I6c6c8eae8e3b7d0407bceef5b675c5410b33768f
    Signed-off-by: Eunki, Hong <eunkiki.hong@samsung.com>

--- dali/internal/common/hash-utils.h
+++ dali/internal/common/hash-utils.h
@@ -70,7 +70,75 @@ constexpr HashType INITIAL_HASH_VALUE = 5381;
 }
 
 // Hash functions with specified length, which we could optimize for
-[[maybe_unused]] HashType HashRawByteBufferMultipleComponent(const uint8_t* __restrict__ buffer, std::size_t bufferSize, HashType& hash);
+[[maybe_unused]] inline HashType HashRawByteBufferMultipleComponent(const uint8_t* __restrict__ buffer, std::size_t bufferSize, HashType& hash)
+{
+  // TODO : use ARM_NEON here in future
+  static constexpr HashType SCALE_LEVEL_1 = Dali::Power<33, 1>::value;
+  static constexpr HashType SCALE_LEVEL_2 = Dali::Power<33, 2>::value;
+  static constexpr HashType SCALE_LEVEL_3 = Dali::Power<33, 3>::value;
+  static constexpr HashType SCALE_LEVEL_4 = Dali::Power<33, 4>::value;
+  static constexpr HashType SCALE_LEVEL_5 = Dali::Power<33, 5>::value;
+  static constexpr HashType SCALE_LEVEL_6 = Dali::Power<33, 6>::value;
+  static constexpr HashType SCALE_LEVEL_7 = Dali::Power<33, 7>::value;
+  static constexpr HashType SCALE_LEVEL_8 = Dali::Power<33, 8>::value;
+
+  while(bufferSize & (~0x7)) // bufferSize >= 8
+  {
+    // clang-format off
+    hash = hash * SCALE_LEVEL_8 +
+           *(buffer + 0) * SCALE_LEVEL_7 +
+           *(buffer + 1) * SCALE_LEVEL_6 +
+           *(buffer + 2) * SCALE_LEVEL_5 +
+           *(buffer + 3) * SCALE_LEVEL_4 +
+           *(buffer + 4) * SCALE_LEVEL_3 +
+           *(buffer + 5) * SCALE_LEVEL_2 +
+           *(buffer + 6) * SCALE_LEVEL_1 +
+           *(buffer + 7);
+    // clang-format on
+    buffer += 8;
+    bufferSize -= 8;
+  }
+
+  if(bufferSize & 0x4) // bufferSize >= 4. Note that 0 <= bufferSize < 8 in here.
+  {
+    // clang-format off
+    hash = hash * SCALE_LEVEL_4 +
+           *(buffer + 0) * SCALE_LEVEL_3 +
+           *(buffer + 1) * SCALE_LEVEL_2 +
+           *(buffer + 2) * SCALE_LEVEL_1 +
+           *(buffer + 3);
+    // clang-format on
+    buffer += 4;
+    bufferSize &= 0x3;
+  }
+
+  // Now bufferSize is 0, 1, 2, 3. We can optimize here by switch-case
+  switch(bufferSize)
+  {
+    case 3:
+    {
+      hash = hash * 33 + *(buffer++);
+      DALI_FALLTHROUGH;
+    }
+    case 2:
+    {
+      hash = hash * 33 + *(buffer++);
+      DALI_FALLTHROUGH;
+    }
+    case 1:
+    {
+      hash = hash * 33 + *(buffer++);
+      DALI_FALLTHROUGH;
+    }
+    case 0:
+    default:
+    {
+      break;
+    }
+  }
+
+  return hash;
+}
 
 [[maybe_unused]] inline HashType HashRawByteBuffer(const uint8_t* __restrict__ buffer, std::size_t bufferSize, HashType& hash)
 {
