413d2d1ea54fa87b29cd6d3200532d9cf19effd4
Seungho Baek <sbsh.baek@samsung.com>
2025-05-29 21:42:24 +0900

    [Tizen] Use BoundingBox for 3D model hit test
    Change-Id: I2403a03598628025d03130d0e75417af614fc52b
    Signed-off-by: Seungho Baek <sbsh.baek@samsung.com>

--- dali/internal/event/events/hit-test-algorithm-impl.cpp
+++ dali/internal/event/events/hit-test-algorithm-impl.cpp
@@ -175,7 +175,7 @@ void HitTestActor(const RenderTask&         renderTask,
                   const float&              nearClippingPlane,
                   const float&              farClippingPlane,
                   HitTestInterface&         hitCheck,
-                  const RayTest&            rayTest,
+                  RayTest&                  rayTest,
                   const Integration::Point& point,
                   const uint32_t            eventTime,
                   bool                      clippingActor,
@@ -195,8 +195,21 @@ void HitTestActor(const RenderTask&         renderTask,
       Vector2 hitPointLocal;
       float   distance;
 
+      Vector3 hitPointLocal3D;
+      Dali::Layer::Behavior layerBehavior = actor.GetLayer().GetProperty<Dali::Layer::Behavior>(Dali::Layer::Property::BEHAVIOR);
+
+      bool isHitted = false;
+      if(layerBehavior == Dali::Layer::Behavior::LAYER_3D)
+      {
+        isHitted = rayTest.ActorBoundingBoxTest(actor, rayOrigin, rayDir, hitPointLocal3D, distance);
+      }
+      else
+      {
+        isHitted = rayTest.ActorTest(actor, rayOrigin, rayDir, hitPointLocal, distance);
+      }
+
       // Finally, perform a more accurate ray test to see if our ray actually hits the actor.
-      if(rayTest.ActorTest(actor, rayOrigin, rayDir, hitPointLocal, distance))
+      if(isHitted)
       {
         // Calculate z coordinate value in Camera Space.
         const Matrix&  viewMatrix          = renderTask.GetCameraActor()->GetViewMatrix();
@@ -296,7 +309,7 @@ HitActor HitTestWithinLayer(Actor&                                     actor,
                             const bool&                                overlayed,
                             bool&                                      overlayHit,
                             bool                                       layerIs3d,
-                            const RayTest&                             rayTest,
+                            RayTest&                                   rayTest,
                             const Integration::Point&                  point,
                             const uint32_t                             eventTime)
 {
@@ -459,7 +472,7 @@ bool HitTestRenderTask(const RenderTaskList::ExclusivesContainer& exclusives,
                        Vector2                                    screenCoordinates,
                        Results&                                   results,
                        HitTestInterface&                          hitCheck,
-                       const RayTest&                             rayTest)
+                       RayTest&                                   rayTest)
 {
   if(renderTask.IsHittable(screenCoordinates))
   {
