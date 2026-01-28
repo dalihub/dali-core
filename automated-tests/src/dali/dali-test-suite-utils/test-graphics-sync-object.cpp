ab3b82fd8041ddf7fbdf746675e26f048a4ee1cb
Heeyong Song <heeyong.song@samsung.com>
2026-01-28 14:16:42 +0900

    Add SyncType to SyncObject
    Change-Id: Iff86e3a07a89f5e55938f96610d2d3955d861fa4

--- automated-tests/src/dali/dali-test-suite-utils/test-graphics-sync-object.cpp
+++ automated-tests/src/dali/dali-test-suite-utils/test-graphics-sync-object.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Samsung Electronics Co., Ltd.
+ * Copyright (c) 2026 Samsung Electronics Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -32,7 +32,7 @@ TestGraphicsSyncObject::~TestGraphicsSyncObject()
 
 void TestGraphicsSyncObject::InitializeResource()
 {
-  mSyncObject = static_cast<TestSyncObject*>(mSyncImplementation.CreateSyncObject());
+  mSyncObject = static_cast<TestSyncObject*>(mSyncImplementation.CreateSyncObject(Integration::GraphicsSyncAbstraction::SyncObject::SyncType::FENCE_SYNC));
 }
 
 bool TestGraphicsSyncObject::IsSynced()
