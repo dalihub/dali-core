c218790939ff0f3cc6a7198ce8aec8c00732f0d4
Eunki Hong <eunkiki.hong@samsung.com>
2026-03-27 10:04:39 +0900

    (PanGestureDetector) Use Dali::Pair for AngleThresholdPair
    This reverts commit 4a3bcdd2f3922cc6f78cec9d744fe63eb66d3d24.
    
    Change-Id: I8ef058dc969f88787427ced0f4de818b99d8aa4c

--- dali/public-api/events/pan-gesture-detector.h
+++ dali/public-api/events/pan-gesture-detector.h
@@ -22,6 +22,7 @@
 #include <cstdint> // uint32_t
 
 // INTERNAL INCLUDES
+#include <dali/public-api/common/dali-pair.h>
 #include <dali/public-api/events/gesture-detector.h>
 #include <dali/public-api/events/pan-gesture.h>
 #include <dali/public-api/object/property-index-ranges.h>
@@ -89,50 +90,12 @@ public:
     };
   };
 
-  /**
-   * @brief Range of angles for a direction.
-   * @SINCE_1_0.0
-   */
-  struct AngleThresholdPair
-  {
-  public:
-    /**
-     * @brief Default constructor.
-     * @SINCE_2_5.8
-     */
-    AngleThresholdPair()
-    : AngleThresholdPair(Radian(0), Radian(0))
-    {
-    }
-
-    /**
-     * @brief Constructor with angle and threshold.
-     * @SINCE_2_5.8
-     * @param[in] angleValue The angle value
-     * @param[in] thresholdValue The threshold value
-     */
-    AngleThresholdPair(Radian angleValue, Radian thresholdValue)
-    : angle(angleValue),
-      threshold(thresholdValue)
-    {
-    }
-
-  public:
-    // Use default copy & move operation
-    AngleThresholdPair(const AngleThresholdPair&)                = default;
-    AngleThresholdPair(AngleThresholdPair&&) noexcept            = default;
-    AngleThresholdPair& operator=(const AngleThresholdPair&)     = default;
-    AngleThresholdPair& operator=(AngleThresholdPair&&) noexcept = default;
-
-  public:
-    Radian angle;     ///< The angle that pan should be allowed. @SINCE_2_5.8
-    Radian threshold; ///< The threshold around that angle. @SINCE_2_5.8
-  };
-
   // Typedefs
   using DetectedSignalType = Signal<void(Actor, const PanGesture&)>; ///< Pan gesture detected signal type @SINCE_1_0.0
 
   // Directional Pan
+  using AngleThresholdPair = Dali::Pair<Radian, Radian>; ///< Range of angles for a direction @SINCE_1_0.0
+
   static const Radian DIRECTION_LEFT;       ///< For a left pan (-PI Radians).
   static const Radian DIRECTION_RIGHT;      ///< For a right pan (0 Radians).
   static const Radian DIRECTION_UP;         ///< For an up pan (-0.5 * PI Radians).
