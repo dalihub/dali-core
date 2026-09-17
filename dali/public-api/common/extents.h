8228720460a4910151f4eb4ad36976816b13a102
dongsug.song <dongsug.song@samsung.com>
2026-09-17 10:01:15 +0900

    [Tizen] Add public extents compatibility header temporarilly
    Change-Id: I49ab0cfbffc046d82034f81f14b2efd8ce8e3c03

---
+++ dali/public-api/common/extents.h
@@ -0,0 +1,6 @@
+#ifndef DALI_PUBLIC_API_COMMON_EXTENTS_COMPAT_H
+#define DALI_PUBLIC_API_COMMON_EXTENTS_COMPAT_H
+
+#include <dali/devel-api/common/extents.h>
+
+#endif // DALI_PUBLIC_API_COMMON_EXTENTS_COMPAT_H
